04ca216bb81deee20e7a4fd561e6c6cbdf51ef5f
Big Boss <bigboss@metalrodeo.xyz>
2023-12-08 13:00:45 -0600

    wip passkey parsing

--- packages/app/utils/passkeys.test.ts
+++ packages/app/utils/passkeys.test.ts
@@ -1,18 +1,24 @@
 import { describe, it } from '@jest/globals'
 import { base64 } from '@scure/base'
 import { bytesToHex } from 'viem'
-import { parseAndNormalizeSig } from './passkeys'
+import { parseAndNormalizeSig, parseAuthDataFromAttestationObject } from './passkeys'
 import { CreateResult, SignResult } from '@daimo/expo-passkeys'
 
 const mockAttestations: CreateResult[] = [
+  // {
+  //   // first bytes are the ASN.1 header (3059301306072a8648ce3d020106082a8648ce3d03010703420004)
+  //   // last bytes are the x and y coordinates of the public key
+  //   // public key '3059301306072a8648ce3d020106082a8648ce3d030107034200049e0ec64e75d6687d07a8060db040ac6bc2419b20c4e6b70ea8ac7737a93ebec513455f7d82c915b55eb76eb10f637387a38a3d6ed9536dd045ce0d70ca2998af'
+  //   rawClientDataJSONB64:
+  //     'eyJ0eXBlIjoid2ViYXV0aG4uY3JlYXRlIiwiY2hhbGxlbmdlIjoiYzI5dFpTQmphR0ZzYkdWdVoyVSIsIm9yaWdpbiI6Imh0dHBzOi8vc2VuZGFwcC5sb2NhbGhvc3QifQ==',
+  //   rawAttestationObjectB64:
+  //     'o2NmbXRkbm9uZWdhdHRTdG10oGhhdXRoRGF0YViUVVJTlMb5JhbV7rFoaFKDcCvBmzc1HOD7tW1Q7aed139dAAAAALraVWanqkAfvZZFYZpVEg0AEBH8VIF2M9LFEWC+4zQYvOOlAQIDJiABIVggng7GTnXWaH0HqAYNsECsa8JBmyDE5rcOqKx3N6k+vsUiWCATRV99gskVtV63brEPY3OHo4o9btlTbdBFzg1wyimYrw==',
+  // },
   {
-    // first bytes are the ASN.1 header (3059301306072a8648ce3d020106082a8648ce3d03010703420004)
-    // last bytes are the x and y coordinates of the public key
-    // public key '3059301306072a8648ce3d020106082a8648ce3d030107034200049e0ec64e75d6687d07a8060db040ac6bc2419b20c4e6b70ea8ac7737a93ebec513455f7d82c915b55eb76eb10f637387a38a3d6ed9536dd045ce0d70ca2998af'
     rawClientDataJSONB64:
       'eyJ0eXBlIjoid2ViYXV0aG4uY3JlYXRlIiwiY2hhbGxlbmdlIjoiYzI5dFpTQmphR0ZzYkdWdVoyVSIsIm9yaWdpbiI6Imh0dHBzOi8vc2VuZGFwcC5sb2NhbGhvc3QifQ==',
     rawAttestationObjectB64:
-      'o2NmbXRkbm9uZWdhdHRTdG10oGhhdXRoRGF0YViUVVJTlMb5JhbV7rFoaFKDcCvBmzc1HOD7tW1Q7aed139dAAAAALraVWanqkAfvZZFYZpVEg0AEBH8VIF2M9LFEWC+4zQYvOOlAQIDJiABIVggng7GTnXWaH0HqAYNsECsa8JBmyDE5rcOqKx3N6k+vsUiWCATRV99gskVtV63brEPY3OHo4o9btlTbdBFzg1wyimYrw==',
+      'o2NmbXRkbm9uZWdhdHRTdG10oGhhdXRoRGF0YViUVVJTlMb5JhbV7rFoaFKDcCvBmzc1HOD7tW1Q7aed139dAAAAALraVWanqkAfvZZFYZpVEg0AELrCIUXbFEmBuRuNcWACmbylAQIDJiABIVggyaWUrCX3btzZi3qE0axKQNbuazi4Lc7S146OjuD97IQiWCAm+K/wA2sPR57JgLIkd/PZDoVRz9RTCxoXESdVwZiHnA==',
   },
 ]
 
@@ -27,8 +33,41 @@ const mockAssertions: SignResult[] = [
   },
 ]
 
+describe('parseAttestationObject', () => {
+  it('can parse attestation objects', () => {
+    for (const attestation of mockAttestations) {
+      const parsed = parseAuthDataFromAttestationObject(
+        base64.decode(attestation.rawAttestationObjectB64)
+      )
+      expect(parsed).toBeTruthy()
+    }
+  })
+})
+
 describe('parseAndNormalizeSig', () => {
   // TODO: can parse attestation signatures
+
+  it('can parse attestation signatures', () => {
+    for (const attestation of mockAttestations) {
+      const sig = parseAndNormalizeSig(base64.decode(attestation.signatureB64))
+      const rHex = sig.r.toString(16)
+      const sHex = sig.s.toString(16)
+      const expected = {
+        r: '9e0ec64e75d6687d07a8060db040ac6bc2419b20c4e6b70ea8ac7737a93ebec5',
+        s: '13455f7d82c915b55eb76eb10f637387a38a3d6ed9536dd045ce0d70ca2998af',
+      }
+      // console.log('sig', {
+      //   r: sig.r.toString(16),
+      //   s: sig.s.toString(16),
+      // })
+      // console.log('expected', expected)
+      expect({
+        r: rHex,
+        s: sHex,
+      }).toStrictEqual(expected)
+    }
+  })
+
   it('can parse assertion signatures', () => {
     for (const assertion of mockAssertions) {
       const sig = parseAndNormalizeSig(bytesToHex(base64.decode(assertion.signatureB64)))
--- packages/app/utils/passkeys.ts
+++ packages/app/utils/passkeys.ts
@@ -1,6 +1,8 @@
 import { p256 } from '@noble/curves/p256'
 import { Hex, bytesToBigInt, hexToBytes, bytesToHex } from 'viem'
 import { base64, base16 } from '@scure/base'
+import cbor from 'cbor'
+import { CreateResult, SignResult } from '@daimo/expo-passkeys'
 
 const derPrefix = '0x3059301306072a8648ce3d020106082a8648ce3d03010703420004'
 
@@ -46,4 +48,99 @@ export function parseAndNormalizeSig(derSig: Hex): { r: bigint; s: bigint } {
     s = n - s
   }
   return { r, s }
+}
+
+export function parseAuthDataFromAttestationObject(attestationObject: Uint8Array) {
+  const attestation = cbor.decodeAllSync(attestationObject)[0] as {
+    fmt: string
+    attStmt: {
+      alg: number
+      sig: Buffer
+    }
+    authData: Buffer
+  }
+  console.log('attestation', attestation)
+  assert(!!attestation, 'Invalid attestation object')
+  assert(attestation.authData.length >= 37, 'Invalid authData length')
+  return attestation
+}
+
+// Parses authenticatorData buffer to struct
+// https://www.w3.org/TR/webauthn-2/#sctn-authenticator-data
+function parseMakeCredAuthData(buffer: Uint8Array) {
+  const rpIdHash = buffer.slice(0, 32)
+  buffer = buffer.slice(32)
+  const flagsBuf = buffer.slice(0, 1)
+  buffer = buffer.slice(1)
+  const flags = flagsBuf[0]
+  const counterBuf = buffer.slice(0, 4)
+  buffer = buffer.slice(4)
+  const counter = Buffer.from(counterBuf).readUInt32BE(0)
+  const aaguid = buffer.slice(0, 16)
+  buffer = buffer.slice(16)
+  const credIDLenBuf = buffer.slice(0, 2)
+  buffer = buffer.slice(2)
+  const credIDLen = Buffer.from(credIDLenBuf).readUInt16BE(0)
+  const credID = buffer.slice(0, credIDLen)
+  buffer = buffer.slice(credIDLen)
+  const COSEPublicKey = buffer
+
+  return {
+    rpIdHash,
+    flagsBuf,
+    flags,
+    counter,
+    counterBuf,
+    aaguid,
+    credID,
+    COSEPublicKey,
+  }
+}
+
+// Takes COSE encoded public key and converts it to DER keys
+// https://www.rfc-editor.org/rfc/rfc8152.html#section-13.1
+function COSEECDHAtoDER(COSEPublicKey: Uint8Array): Hex {
+  const coseStruct = cbor.decodeAllSync(COSEPublicKey)[0]
+  const x = coseStruct.get(-2)
+  const y = coseStruct.get(-3)
+
+  return contractFriendlyKeyToDER([
+    `0x${Buffer.from(x).toString('hex')}`,
+    `0x${Buffer.from(y).toString('hex')}`,
+  ])
+}
+
+// Parses Webauthn MakeCredential response
+// https://www.w3.org/TR/webauthn-2/#sctn-op-make-cred
+export function parseCreateResponse(result: CreateResult) {
+  const rawAttestationObject = base64.decode(result.rawAttestationObjectB64)
+  const attestationObject = cbor.decode(rawAttestationObject)
+  const authData = parseMakeCredAuthData(attestationObject.authData)
+  const pubKey = COSEECDHAtoDER(authData.COSEPublicKey)
+  return pubKey
+}
+
+// Parses Webauthn GetAssertion response
+// https://www.w3.org/TR/webauthn-2/#sctn-op-get-assertion
+export function parseSignResponse(result: SignResult) {
+  const derSig = base64.decode(result.signatureB64)
+  const rawAuthenticatorData = base64.decode(result.rawAuthenticatorDataB64)
+  const passkeyName = result.passkeyName
+  const [accountName, keySlotStr] = passkeyName.split('.') // Assumes account name does not have periods (.) in it.
+  const keySlot = parseInt(keySlotStr, 10)
+
+  const clientDataJSON = Buffer.from(base64.decode(result.rawClientDataJSONB64)).toString('utf-8')
+
+  const challengeLocation = BigInt(clientDataJSON.indexOf('"challenge":"'))
+  const responseTypeLocation = BigInt(clientDataJSON.indexOf('"type":"'))
+
+  return {
+    derSig: bytesToHex(derSig),
+    rawAuthenticatorData,
+    accountName,
+    keySlot,
+    clientDataJSON,
+    challengeLocation,
+    responseTypeLocation,
+  }
 }
