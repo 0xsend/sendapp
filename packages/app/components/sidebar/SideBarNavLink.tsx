9cd63cc0891879754cf281b84ac2a28fe91b0955
youngkidwarrior <victor@she.energy>
2023-12-22 01:46:54 -0800

    SideBar App Component

--- packages/app/components/sidebar/SideBarNavLink.tsx
+++ packages/app/components/sidebar/SideBarNavLink.tsx
@@ -1,16 +1,30 @@
-import { Button, ButtonIcon, ButtonText, Link, type LinkProps } from "@my/ui";
-import { type ReactElement } from "react";
+import { Button, ButtonIcon, ButtonText, Link, type LinkProps } from '@my/ui'
+import { type ReactElement } from 'react'
 
-export function SideBarNavLink({ icon, text, isActive, ...props }: { icon?: ReactElement, text: string, isActive: boolean } & Omit<LinkProps, "children">): ReactElement {
+export function SideBarNavLink({
+  icon,
+  text,
+  location,
+  ...props
+}: { icon?: ReactElement; text: string; location: string } & Omit<
+  LinkProps,
+  'children'
+>): ReactElement {
+  const isActiveRoute = location === props.href
 
   return (
-    <Link {...props} href={props.disabled ? "" : props.href} >
-      <Button disabled={props.disabled} color={isActive ? "$white" : "$gold10"} bg={isActive ? "$gold7" : "transparent"} hoverStyle={{ scale: "105%" }} fontSize={"$4"} fontFamily={"$heading"}>
-        <ButtonIcon>
-          {icon}
-        </ButtonIcon>
-        <ButtonText fontWeight={isActive ? "bold" : "normal"}>{text}</ButtonText>
+    <Link {...props} href={props.disabled ? '' : props.href}>
+      <Button
+        disabled={props.disabled}
+        color={isActiveRoute ? '$white' : '$gold10'}
+        bg={isActiveRoute ? '$gold7' : 'transparent'}
+        hoverStyle={{ scale: '105%' }}
+        fontSize={'$4'}
+        fontFamily={'$heading'}
+      >
+        <ButtonIcon>{icon}</ButtonIcon>
+        <ButtonText fontWeight={isActiveRoute ? 'bold' : 'normal'}>{text}</ButtonText>
       </Button>
-    </Link >
+    </Link>
   )
 }
