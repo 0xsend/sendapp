f09046b0f8ac1d8f0666e88c74e726237715081d
youngkidwarrior <victor@she.energy>
2023-12-22 01:46:54 -0800

    SideBar App Component

---
+++ packages/app/components/sidebar/SideBarNavLink.tsx
@@ -0,0 +1,17 @@
+import { Button, ButtonIcon, ButtonText, Link, type LinkProps } from "@my/ui";
+import { type ReactElement } from "react";
+
+export function SideBarNavLink({ icon, text, location, ...props }: { icon?: ReactElement, text: string, location: string } & Omit<LinkProps, "children">): ReactElement {
+  const isActiveRoute = location === props.href
+
+  return (
+    <Link {...props} href={props.disabled ? "" : props.href} >
+      <Button disabled={props.disabled} color={isActiveRoute ? "$white" : "$gold10"} bg={isActiveRoute ? "$gold7" : "transparent"} hoverStyle={{ scale: "105%" }} fontSize={"$4"} fontFamily={"$heading"}>
+        <ButtonIcon>
+          {icon}
+        </ButtonIcon>
+        <ButtonText fontWeight={isActiveRoute ? "bold" : "normal"}>{text}</ButtonText>
+      </Button>
+    </Link >
+  )
+}
