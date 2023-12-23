6914eabe9b7bf87a2baf0be0012a09a234c1a059
youngkidwarrior <victor@she.energy>
2023-12-22 17:54:28 -0800

    Fix Icon Names and add missing Icons

--- packages/app/components/sidebar/index.tsx
+++ packages/app/components/sidebar/index.tsx
@@ -1,34 +1,67 @@
-
-import { SideBar as SideBarUI, Button, ButtonIcon, XStack, YStack, Nav } from "@my/ui";
-import { Link } from "@my/ui";
-import { IconSendLogo, IconSendLogoSmall, IconTelegramLogo, IconTwitterLogo } from "../icons";
-import { SideBarNavLink } from "./SideBarNavLink";
-import { SideBarFooterLink } from "./SideBarFooterLink";
-import { twitter as twitterSocial, telegram as telegramSocial } from '../../data/socialLinks'
+import { Button, ButtonIcon, Nav, SideBar as SideBarUI, XStack, YStack } from '@my/ui'
+import { Link } from '@my/ui'
+import {
+  IconDashboard,
+  IconDistributions,
+  IconSLogo,
+  IconSendLogo,
+  IconTelegramLogo,
+  IconXLogo,
+} from 'app/components/icons'
+import { telegram as telegramSocial, twitter as twitterSocial } from '../../data/socialLinks'
+import { SideBarFooterLink } from './SideBarFooterLink'
+import { SideBarNavLink } from './SideBarNavLink'
 
 export const SideBar = ({ location }: { location: string }) => (
   <SideBarUI>
-    <Link href={"/"} marginTop={"$10"}>
-      <Button borderRadius={9999} w={"$11"} h={"$11"} bg={"transparent"}>
+    <Link href={'/'} marginTop={'$10'}>
+      <Button borderRadius={9999} w={'$11'} h={'$11'} bg={'transparent'}>
         {/* TODO: Implement Radial Gradient UI Element. Curently not in TamaGUI */}
         <ButtonIcon>
-          <IconSendLogoSmall size={"$10"} />
+          <IconSLogo size={'$10'} />
         </ButtonIcon>
-      </Button  >
+      </Button>
     </Link>
-    <Nav display="flex" flex={2} justifyContent={"center"} alignItems="center">
-      <YStack gap={"$4"} alignItems='flex-start' justifyContent='center'>
-        <SideBarNavLink location={location} icon={<IconSendLogoSmall />} text={"Dashboard"} href={"/"} />
-        <SideBarNavLink location={location} icon={<IconSendLogoSmall />} text={"Distributions"} href={"/distributions"} disabled={true} hoverStyle={{ cursor: "not-allowed" }} />
-        <SideBarNavLink location={location} icon={<IconSendLogoSmall />} text={"Leaderboard"} href={"/leaderboard"} disabled={true} hoverStyle={{ cursor: "not-allowed" }} />
+    <Nav display="flex" flex={2} justifyContent={'center'} alignItems="center">
+      <YStack gap={'$4'} alignItems="flex-start" justifyContent="center">
+        <SideBarNavLink
+          icon={<IconDashboard size={'$2'} />}
+          text={'Dashboard'}
+          href={'/'}
+          isActive={location === '/'}
+        />
+        <SideBarNavLink
+          icon={<IconDistributions size={'$2'} />}
+          text={'Distributions'}
+          href={'/distributions'}
+          isActive={location.includes('/distributions')}
+        />
+        <SideBarNavLink
+          icon={<IconSLogo size={'$2'} />}
+          text={'Leaderboard'}
+          href={'/leaderboard'}
+          isActive={location.includes('/leaderboard')}
+          disabled={true}
+          hoverStyle={{ cursor: 'not-allowed' }}
+        />
       </YStack>
     </Nav>
-    <YStack gap="$4" alignItems='center'>
-      <IconSendLogo color={"$gold10"} />
+    <YStack gap="$4" alignItems="center">
+      <IconSendLogo />
       <XStack gap="$2">
-        <SideBarFooterLink icon={<IconTwitterLogo />} href={twitterSocial} target='_blank' borderRadius={9999} />
-        <SideBarFooterLink icon={<IconTelegramLogo />} href={telegramSocial} target='_blank' borderRadius={9999} />
+        <SideBarFooterLink
+          icon={<IconXLogo />}
+          href={twitterSocial}
+          target="_blank"
+          borderRadius={9999}
+        />
+        <SideBarFooterLink
+          icon={<IconTelegramLogo />}
+          href={telegramSocial}
+          target="_blank"
+          borderRadius={9999}
+        />
       </XStack>
     </YStack>
-  </SideBarUI >
+  </SideBarUI>
 )
