3faddb2e717b0d3e705fd7deaf04a9d8db972a31
youngkidwarrior <victor@she.energy>
2023-12-22 17:56:27 -0800

    Better Abstraction Hierarchy with SideBarWrapper Component

--- packages/ui/src/components/SideBar/index.tsx
+++ packages/ui/src/components/SideBar/index.tsx
@@ -1,10 +1,13 @@
-import { YStack } from "@my/ui";
+import { YStack, YStackProps } from "@my/ui";
 
 
-export const SideBar = ({ children }: { children?: React.ReactNode }) => {
+
+export const SideBar = ({ children, ...props }: { children?: React.ReactNode } & YStackProps) => {
   return (
-    <YStack $md={{ display: "none" }} height={'100svh'} width={"20%"} py="$6" zIndex={1} justifyContent="space-around" alignItems="center" backgroundColor={"$backgroundHover"}>
+    <YStack position="unset" $md={{ display: "none" }} height={'100svh'} $ width={"20%"} py="$6" zIndex={1} justifyContent="space-around" alignItems="center" {...props}>
       {children}
     </YStack>
   )
 }
+
+export * from "./Wrapper"
