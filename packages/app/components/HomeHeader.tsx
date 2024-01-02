e9543b5747f14c8c9f3d646404a4035785d4b99a
youngkidwarrior <victor@she.energy>
2024-01-02 14:07:18 -0800

    Add Points Count and size header items to parent height

--- packages/app/components/HomeHeader.tsx
+++ packages/app/components/HomeHeader.tsx
@@ -4,6 +4,7 @@ import { Copy, X } from "@tamagui/lucide-icons";
 import { getReferralHref } from "app/utils/getReferralLink";
 import { IconClose, IconSettings } from "./icons";
 import { useAccount, useConnect, useDisconnect } from "wagmi";
+import { IconStar } from "./icons/IconStar";
 
 
 
@@ -12,13 +13,14 @@ export function HomeHeader({ children }: { children: string }) {
     <Header w="100%" miw={804}>
       <XStack jc="space-between" fd="column" ai="center" $gtSm={{ fd: 'row' }}>
         <H1 fontWeight={"100"}>{children}</H1>
-        <XStack $sm={{ display: "none" }} ai="center" space="$2" $gtSm={{ fd: 'row' }}>
+        <XStack $sm={{ display: "none" }} ai="center" space="$2" height="$4" $gtSm={{ fd: 'row' }}>
           <ReferralCodeCard />
+          <PointsCount />
           <SettingsButton />
           <WagmiAccountInfo />
         </XStack>
       </XStack>
-    </Header>)
+    </Header >)
 }
 
 
@@ -32,49 +34,64 @@ function ReferralCodeCard() {
   }
 
   return (
-
-    <Card size="$1" ai="center" f={1} br={"$4"}>
-      <Card.Header>
-        <XStack ai="center" theme="alt2" space="$2" px={"$2"} py={"$1"} >
-          <Anchor href={referralHref} textDecorationLine="none">
-            <XStack ai="center" space="$2" jc="space-around" >
-              <Text color="$gray11" fontSize={"$3"} >
-                Referral Link
-              </Text>
-              <Text theme={"gold"} color="$gold11" fontWeight={"600"}>send.it/{user?.profile?.referral_code}</Text>
-            </XStack>
-          </Anchor>
-          <Button
-            theme="alt2"
-            size="$2"
-            icon={<Copy />}
-            // @ts-expect-error tamagui doesn't support this yet
-            type="button"
-            onPress={() => {
-              if (user?.profile?.referral_code) {
-                try {
-                  // write the referral link to clipboard
-                  navigator.clipboard.writeText(referralHref)
-                } catch (e) {
-                  console.warn(e)
-                  prompt('Copy to clipboard: Ctrl+C, Enter', referralHref)
-                }
-                toast.show('Copied your referral link to clipboard')
-              }
-            }}
-          />
+    <XStack ai="center" space="$2" bg="$background" h="100%" f={1} px={"$3"} br="$4" >
+      <Anchor href={referralHref} textDecorationLine="none">
+        <XStack ai="center" space="$2" jc="space-around" >
+          <Text color="$gray11" fontSize={"$2"} >
+            Referral Link
+          </Text>
+          <Text theme={"gold"} color="$gold11" fontWeight={"600"}>send.it/{user?.profile?.referral_code}</Text>
         </XStack>
-      </Card.Header>
-    </Card>
+      </Anchor>
+      <Button
+
+        size="$2"
+        icon={<Copy />}
+        // @ts-expect-error tamagui doesn't support this yet
+        type="button"
+        onPress={() => {
+          if (user?.profile?.referral_code) {
+            try {
+              // write the referral link to clipboard
+              navigator.clipboard.writeText(referralHref)
+            } catch (e) {
+              console.warn(e)
+              prompt('Copy to clipboard: Ctrl+C, Enter', referralHref)
+            }
+            toast.show('Copied your referral link to clipboard')
+          }
+        }}
+      />
+    </XStack>
+
 
   )
 }
 
+function PointsCount() {
+  // const user = useUser()
+  // const points = user?.profile?.points ?? 0
+  const points = 0
+
+  return (
+
+    <XStack ai="center" space="$2" $gtSm={{ fd: 'row' }} cursor="not-allowed" bg="$background" h="100%" borderWidth={2} boc={"$goldVibrant"} f={1} px={"$3"} br="$4" >
+      <IconStar size={"$2"} color={"$goldVibrant"} />
+      <Text color={"$goldVibrant"} fontSize={"$1"} fontWeight={"600"}>{points}</Text>
+    </XStack >
+
+
+  )
+}
+
+
 function SettingsButton() {
   return (
-    <Link href="" disabled={true} cursor="not-allowed">
-      <Button disabled={true} size="$3" icon={<IconSettings size={"$1"} />} />
-    </Link>
+    <XStack ai="center" space="$2" $gtSm={{ fd: 'row' }} cursor="not-allowed" bg="$background" h="100%" f={1} px={"$3"} br="$4" >
+      <Link href="" cursor="not-allowed" alignSelf="stretch">
+        <IconSettings size={"$2"} />
+      </Link>
+    </XStack>
   )
 }
 
@@ -84,12 +101,18 @@ function WagmiAccountInfo() {
   const { disconnect } = useDisconnect()
 
   if (!isConnected || !address) {
-    return <Button size={"$3"} cursor="not-allowed" disabled={true} onPress={() => connectAsync()}>Connect Wallet</Button>
+    return (
+      <XStack ai="center" space="$2" $gtSm={{ fd: 'row' }} cursor="not-allowed" bg="$background" h="100%" f={1} px={"$3"} br="$4" >
+        <Button bg="transparent" disabled={true} onPress={() => connectAsync()}>Connect Wallet</Button>
+      </XStack>)
+
   }
   return (
-    <XStack h="$3" ai="center" space="$2" $gtSm={{ fd: 'row' }} cursor="not-allowed">
+    <XStack ai="center" space="$2" $gtSm={{ fd: 'row' }} cursor="not-allowed" bg="$background" h="100%" f={1} px={"$3"} br="$4" >
       <Text fontWeight={"100"}>{address}</Text>
       <Button disabled={true} icon={<IconClose />} onPress={() => disconnect()} cursor="not-allowed" />
     </XStack>
+
+
   )
 }
