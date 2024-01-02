9287b3471ac665f62802790502f9c5bedca0e7d8
youngkidwarrior <victor@she.energy>
2024-01-02 14:07:18 -0800

    Add Points Count and size header items to parent height

--- packages/app/features/home/screen.tsx
+++ packages/app/features/home/screen.tsx
@@ -4,43 +4,42 @@ import { HomeHeader } from 'app/components/HomeHeader'
 
 export function HomeScreen() {
   return (
-    <Container>
-      <ScrollView f={3} fb={0} p="$12" >
-        <YStack gap="$6" pt="$5" pb="$8">
-          <YStack space="$4">
-            <HomeHeader>Dashboard</HomeHeader>
-            <Button
-              onPress={async () => {
-                // const result = await createPasskey({
-                //   domain: 'sendapp.localhost',
-                //   challengeB64: window.btoa('some challenge'),
-                //   passkeyName: 'sendappuser',
-                //   passkeyDisplayTitle: 'SendAppUser',
-                // })
-                // // @ts-expect-error - for debugging
-                // window.passkey = result
-                // console.log(result)
-              }}
-            >
-              Create
-            </Button>
-            <Button
-              onPress={async () => {
-                // const sign = await signWithPasskey({
-                //   domain: 'sendapp.localhost',
-                //   challengeB64: window.btoa('another challenge'),
-                // })
-                // // @ts-expect-error - for debugging
-                // window.sign = sign
-                // console.log(sign)
-              }}
-            >
-              Sign
-            </Button>
-          </YStack>
+
+    <ScrollView f={3} fb={0} p="$12" bg="#F0EEE9">
+      <YStack gap="$6" pt="$5" pb="$8">
+        <YStack space="$4">
+          <HomeHeader>Dashboard</HomeHeader>
+          <Button
+            onPress={async () => {
+              // const result = await createPasskey({
+              //   domain: 'sendapp.localhost',
+              //   challengeB64: window.btoa('some challenge'),
+              //   passkeyName: 'sendappuser',
+              //   passkeyDisplayTitle: 'SendAppUser',
+              // })
+              // // @ts-expect-error - for debugging
+              // window.passkey = result
+              // console.log(result)
+            }}
+          >
+            Create
+          </Button>
+          <Button
+            onPress={async () => {
+              // const sign = await signWithPasskey({
+              //   domain: 'sendapp.localhost',
+              //   challengeB64: window.btoa('another challenge'),
+              // })
+              // // @ts-expect-error - for debugging
+              // window.sign = sign
+              // console.log(sign)
+            }}
+          >
+            Sign
+          </Button>
         </YStack>
-      </ScrollView >
-    </Container>
+      </YStack>
+    </ScrollView >
 
   )
 }
