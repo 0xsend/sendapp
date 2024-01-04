b2fec952665123c56b8b463a279103160745fedb
youngkidwarrior <victor@she.energy>
2024-01-03 16:47:43 -0800

    Add BottomSheet Nav for small screen sizes

--- packages/ui/src/components/SideBar/index.tsx
+++ packages/ui/src/components/SideBar/index.tsx
@@ -1,7 +1,5 @@
 import { YStack, YStackProps } from "@my/ui";
 
-
-
 export const SideBar = ({ children, ...props }: { children?: React.ReactNode } & YStackProps) => {
   return (
     <YStack $md={{ display: "none" }} height={'100svh'} width={"20%"} py="$6" zIndex={1} justifyContent="space-around" alignItems="center" {...props}>
