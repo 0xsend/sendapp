bad6f0071c18f6fd4a0078d7940d1ee82458b7b9
youngkidwarrior <victor@she.energy>
2024-01-11 11:29:40 -0800

    Add min height to sidebare component

--- packages/ui/src/components/SideBar/index.tsx
+++ packages/ui/src/components/SideBar/index.tsx
@@ -1,11 +1,21 @@
-import { YStack, YStackProps } from "@my/ui";
+import { YStack, YStackProps } from '@my/ui'
 
 export const SideBar = ({ children, ...props }: { children?: React.ReactNode } & YStackProps) => {
   return (
-    <YStack $md={{ display: "none" }} height={'100svh'} width={"20%"} py="$6" zIndex={1} justifyContent="space-around" alignItems="center" {...props}>
+    <YStack
+      $md={{ display: 'none' }}
+      height={'100svh'}
+      width={'20%'}
+      py="$6"
+      zIndex={1}
+      justifyContent="space-around"
+      alignItems="center"
+      mih={'524px'}
+      {...props}
+    >
       {children}
     </YStack>
   )
 }
 
-export * from "./Wrapper"
+export * from './Wrapper'
