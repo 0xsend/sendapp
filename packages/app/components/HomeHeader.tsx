48fe6039dee94050b7808ac2a821c5eccc72e63a
youngkidwarrior <victor@she.energy>
2024-01-11 11:48:38 -0800

    Remove referrals code for now

--- packages/app/components/HomeHeader.tsx
+++ packages/app/components/HomeHeader.tsx
@@ -1,58 +1,62 @@
-import { Anchor, Button, H1, Header, Link, XStack, useToastController, Text } from "@my/ui";
-import { useUser } from "app/utils/useUser";
-import { Copy } from "@tamagui/lucide-icons";
-import { getReferralHref } from "app/utils/getReferralLink";
-import { IconClose, IconHamburger, IconSettings } from "./icons";
-import { useAccount, useConnect, useDisconnect } from "wagmi";
-import { IconStar } from "./icons/IconStar";
-import { useNav } from "app/routers/params";
+import { Anchor, Button, H1, Header, Link, Text, XStack, useToastController } from '@my/ui'
+import { Copy } from '@tamagui/lucide-icons'
+import { useNav } from 'app/routers/params'
+import { getReferralHref } from 'app/utils/getReferralLink'
+import { useUser } from 'app/utils/useUser'
+import { useAccount, useConnect, useDisconnect } from 'wagmi'
+import { IconClose, IconHamburger, IconSettings } from './icons'
+import { IconStar } from './icons/IconStar'
 
 export function HomeHeader({ children }: { children: string }) {
   const [nav, setNavParam] = useNav()
   const handleHomeBottomSheet = () => {
-    setNavParam(nav ? undefined : "home", { webBehavior: "replace" })
+    setNavParam(nav ? undefined : 'home', { webBehavior: 'replace' })
   }
-
 
   return (
     <Header w="100%" $gtSm={{ miw: 804 }}>
       <XStack jc="space-between" fd="row" ai="center">
-        <H1 fontWeight={"100"}>{children}</H1>
-        <XStack $lg={{ display: "none" }} ai="center" space="$2" height="$4" $gtSm={{ fd: 'row' }}>
+        <H1 fontWeight={'100'}>{children}</H1>
+        <XStack $lg={{ display: 'none' }} ai="center" space="$2" height="$4" $gtSm={{ fd: 'row' }}>
           <ReferralCodeCard />
           <ReferralsCount />
           <SettingsButton />
           <WagmiAccountInfo />
         </XStack>
-        <XStack $gtLg={{ display: "none" }} ai="center" space="$2" height="$4">
-          <Button onPress={handleHomeBottomSheet} bg="transparent" icon={<IconHamburger size={"$3"} />} />
+        <XStack $gtLg={{ display: 'none' }} ai="center" space="$2" height="$4">
+          <Button
+            onPress={handleHomeBottomSheet}
+            bg="transparent"
+            icon={<IconHamburger size={'$3'} />}
+          />
         </XStack>
       </XStack>
-    </Header >)
+    </Header>
+  )
 }
-
 
 function ReferralCodeCard() {
   const user = useUser()
   const toast = useToastController()
-  const referralHref = getReferralHref(user?.profile?.referral_code ?? '')!
+  const referralHref = getReferralHref(user?.profile?.referral_code ?? '')
 
   if (!user?.profile?.referral_code) {
     return null
   }
 
   return (
-    <XStack ai="center" space="$2" bg="$background" h="100%" f={1} px={"$3"} br="$4" >
+    <XStack ai="center" space="$2" bg="$background" h="100%" f={1} px={'$3'} br="$4">
       <Anchor href={referralHref} textDecorationLine="none">
-        <XStack ai="center" space="$2" jc="space-around" >
-          <Text color="$gray11" fontSize={"$2"} >
+        <XStack ai="center" space="$2" jc="space-around">
+          <Text color="$gray11" fontSize={'$2'}>
             Referral Link
           </Text>
-          <Text theme={"gold"} color="$gold11" fontWeight={"600"}>send.it/{user?.profile?.referral_code}</Text>
+          <Text theme={'gold'} color="$gold11" fontWeight={'600'}>
+            send.it/{user?.profile?.referral_code}
+          </Text>
         </XStack>
       </Anchor>
       <Button
-
         size="$2"
         icon={<Copy />}
         // @ts-expect-error tamagui doesn't support this yet
@@ -61,6 +65,7 @@ function ReferralCodeCard() {
           if (user?.profile?.referral_code) {
             try {
               // write the referral link to clipboard
+              // @TODO: implement a native clipboard solution
               navigator.clipboard.writeText(referralHref)
             } catch (e) {
               console.warn(e)
@@ -71,31 +76,50 @@ function ReferralCodeCard() {
         }}
       />
     </XStack>
-
-
   )
 }
 
 function ReferralsCount() {
   const user = useUser()
-  const referrals = user?.referrals ? user.referrals.length : 0
+  const referrals = 0
 
   return (
-    <XStack ai="center" space="$2" $gtSm={{ fd: 'row' }} cursor="not-allowed" bg="$background" h="100%" borderWidth={2} boc={"$goldVibrant"} f={1} px={"$3"} br="$4" >
-      <IconStar size={"$2"} color={"$goldVibrant"} />
-      <Text color={"$goldVibrant"} fontSize={"$1"} fontWeight={"600"}>{referrals}</Text>
-    </XStack >
-
-
+    <XStack
+      ai="center"
+      space="$2"
+      $gtSm={{ fd: 'row' }}
+      cursor="not-allowed"
+      bg="$background"
+      h="100%"
+      borderWidth={2}
+      boc={'$goldVibrant'}
+      f={1}
+      px={'$3'}
+      br="$4"
+    >
+      <IconStar size={'$2'} color={'$goldVibrant'} />
+      <Text color={'$goldVibrant'} fontSize={'$1'} fontWeight={'600'}>
+        {referrals}
+      </Text>
+    </XStack>
   )
 }
 
-
 function SettingsButton() {
   return (
-    <XStack ai="center" space="$2" $gtSm={{ fd: 'row' }} cursor="not-allowed" bg="$background" h="100%" f={1} px={"$3"} br="$4" >
+    <XStack
+      ai="center"
+      space="$2"
+      $gtSm={{ fd: 'row' }}
+      cursor="not-allowed"
+      bg="$background"
+      h="100%"
+      f={1}
+      px={'$3'}
+      br="$4"
+    >
       <Link href="" cursor="not-allowed" alignSelf="stretch">
-        <IconSettings size={"$2"} />
+        <IconSettings size={'$2'} />
       </Link>
     </XStack>
   )
@@ -108,17 +132,42 @@ function WagmiAccountInfo() {
 
   if (!isConnected || !address) {
     return (
-      <XStack ai="center" space="$2" $gtSm={{ fd: 'row' }} cursor="not-allowed" bg="$background" h="100%" f={1} px={"$3"} br="$4" >
-        <Button bg="transparent" disabled={true} onPress={() => connectAsync()}>Connect Wallet</Button>
-      </XStack>)
-
+      <XStack
+        ai="center"
+        space="$2"
+        $gtSm={{ fd: 'row' }}
+        cursor="not-allowed"
+        bg="$background"
+        h="100%"
+        f={1}
+        px={'$3'}
+        br="$4"
+      >
+        <Button bg="transparent" disabled={true} onPress={() => connectAsync()}>
+          Connect Wallet
+        </Button>
+      </XStack>
+    )
   }
   return (
-    <XStack ai="center" space="$2" $gtSm={{ fd: 'row' }} cursor="not-allowed" bg="$background" h="100%" f={1} px={"$3"} br="$4" >
-      <Text fontWeight={"100"}>{address}</Text>
-      <Button disabled={true} icon={<IconClose />} onPress={() => disconnect()} cursor="not-allowed" />
+    <XStack
+      ai="center"
+      space="$2"
+      $gtSm={{ fd: 'row' }}
+      cursor="not-allowed"
+      bg="$background"
+      h="100%"
+      f={1}
+      px={'$3'}
+      br="$4"
+    >
+      <Text fontWeight={'100'}>{address}</Text>
+      <Button
+        disabled={true}
+        icon={<IconClose />}
+        onPress={() => disconnect()}
+        cursor="not-allowed"
+      />
     </XStack>
-
-
   )
 }
--- packages/app/components/sidebar/HomeSideBar.tsx
+++ packages/app/components/sidebar/HomeSideBar.tsx
@@ -1,76 +1,147 @@
+import {
+  BottomSheet,
+  Button,
+  ButtonIcon,
+  Nav,
+  SheetProps,
+  SideBar,
+  SideBarWrapper,
+  XStack,
+  YStack,
+  YStackProps,
+  useMedia,
+} from '@my/ui'
+import { Link } from '@my/ui'
+import {
+  IconDashboard,
+  IconDistributions,
+  IconSLogo,
+  IconSendLogo,
+  IconTelegramLogo,
+  IconXLogo,
+} from 'app/components/icons'
+import { SideBarFooterLink } from 'app/components/sidebar/SideBarFooterLink'
+import { SideBarNavLink } from 'app/components/sidebar/SideBarNavLink'
+import { telegram as telegramSocial, twitter as twitterSocial } from 'app/data/socialLinks'
+import { usePathname } from 'app/utils/usePathname'
 
-import { SideBarWrapper, Button, ButtonIcon, XStack, YStack, Nav, SideBar, YStackProps, BottomSheet, useMedia, SheetProps } from "@my/ui";
-import { Link } from "@my/ui";
-import { IconSendLogo, IconSLogo, IconTelegramLogo, IconXLogo, IconDistributions, IconDashboard } from "app/components/icons";
-import { SideBarNavLink } from "app/components/sidebar/SideBarNavLink";
-import { SideBarFooterLink } from "app/components/sidebar/SideBarFooterLink";
-import { twitter as twitterSocial, telegram as telegramSocial } from 'app/data/socialLinks'
-import { usePathname } from "app/utils/usePathname";
-
-import { useNav } from "app/routers/params";
-
-
+import { useNav } from 'app/routers/params'
 
 const HomeSideBar = ({ ...props }: YStackProps) => {
   const pathName = usePathname()
   return (
     <SideBar {...props}>
-      <Link href={"/"} marginTop={"$10"}>
-        <Button borderRadius={9999} w={"$11"} h={"$11"} bg={"transparent"}>
+      <Link href={'/'} marginTop={'$10'}>
+        <Button borderRadius={9999} w={'$11'} h={'$11'} bg={'transparent'}>
           {/* TODO: Implement Radial Gradient UI Element. Curently not in TamaGUI */}
           <ButtonIcon>
-            <IconSLogo size={"$10"} />
+            <IconSLogo size={'$10'} />
           </ButtonIcon>
-        </Button  >
+        </Button>
       </Link>
-      <Nav display="flex" flex={2} justifyContent={"center"} alignItems="center">
-        <YStack gap={"$4"} alignItems='flex-start' justifyContent='center'>
-          <SideBarNavLink icon={<IconDashboard size={"$2"} />} text={"Dashboard"} href={"/"} isActive={pathName === "/"} />
-          <SideBarNavLink icon={<IconDistributions size={"$2"} />} text={"Distributions"} href={"/distributions"} isActive={pathName.includes("/distributions")} />
-          <SideBarNavLink icon={<IconSLogo size={"$2"} />} text={"Leaderboard"} href={"/leaderboard"} isActive={pathName.includes("/leaderboard")} disabled={true} hoverStyle={{ cursor: "not-allowed" }} />
+      <Nav display="flex" flex={2} justifyContent={'center'} alignItems="center">
+        <YStack gap={'$4'} alignItems="flex-start" justifyContent="center">
+          <SideBarNavLink
+            icon={<IconDashboard size={'$2'} />}
+            text={'Dashboard'}
+            href={'/'}
+            isActive={pathName === '/'}
+          />
+          <SideBarNavLink
+            icon={<IconDistributions size={'$2'} />}
+            text={'Distributions'}
+            href={'/distributions'}
+            isActive={pathName.includes('/distributions')}
+          />
+          <SideBarNavLink
+            icon={<IconSLogo size={'$2'} />}
+            text={'Leaderboard'}
+            href={'/leaderboard'}
+            isActive={pathName.includes('/leaderboard')}
+            disabled={true}
+            hoverStyle={{ cursor: 'not-allowed' }}
+          />
         </YStack>
       </Nav>
-      <YStack gap="$4" alignItems='center'>
+      <YStack gap="$4" alignItems="center">
         <IconSendLogo />
         <XStack gap="$2">
-          <SideBarFooterLink icon={<IconXLogo />} href={twitterSocial} target='_blank' borderRadius={9999} />
-          <SideBarFooterLink icon={<IconTelegramLogo />} href={telegramSocial} target='_blank' borderRadius={9999} />
+          <SideBarFooterLink
+            icon={<IconXLogo />}
+            href={twitterSocial}
+            target="_blank"
+            borderRadius={9999}
+          />
+          <SideBarFooterLink
+            icon={<IconTelegramLogo />}
+            href={telegramSocial}
+            target="_blank"
+            borderRadius={9999}
+          />
         </XStack>
       </YStack>
-    </SideBar >)
+    </SideBar>
+  )
 }
 
 const HomeBottomSheet = ({ open }: SheetProps) => {
   const [nav, setNavParam] = useNav()
 
   const onOpenChange = () => {
-    if (open) setNavParam("home", { webBehavior: "replace" })
-    else setNavParam(undefined, { webBehavior: "replace" })
+    if (open) setNavParam('home', { webBehavior: 'replace' })
+    else setNavParam(undefined, { webBehavior: 'replace' })
   }
 
   const pathName = usePathname()
   return (
-    <BottomSheet open={nav === "home"} onOpenChange={onOpenChange}>
-      <Link href={"/"} marginTop={"$4"}>
-        <Button borderRadius={9999} w={"$11"} h={"$11"} bg={"transparent"}>
+    <BottomSheet open={nav === 'home'} onOpenChange={onOpenChange}>
+      <Link href={'/'} marginTop={'$4'}>
+        <Button borderRadius={9999} w={'$11'} h={'$11'} bg={'transparent'}>
           {/* TODO: Implement Radial Gradient UI Element. Curently not in TamaGUI */}
           <ButtonIcon>
-            <IconSLogo size={"$10"} />
+            <IconSLogo size={'$10'} />
           </ButtonIcon>
-        </Button  >
+        </Button>
       </Link>
-      <Nav display="flex" flex={2} justifyContent={"center"} alignItems="center">
-        <YStack gap={"$4"} alignItems='flex-start' justifyContent='center'>
-          <SideBarNavLink icon={<IconDashboard size={"$2"} />} text={"Dashboard"} href={"/"} isActive={pathName === "/"} />
-          <SideBarNavLink icon={<IconDistributions size={"$2"} />} text={"Distributions"} href={"/distributions"} isActive={pathName.includes("/distributions")} />
-          <SideBarNavLink icon={<IconSLogo size={"$2"} />} text={"Leaderboard"} href={"/leaderboard"} isActive={pathName.includes("/leaderboard")} disabled={true} hoverStyle={{ cursor: "not-allowed" }} />
+      <Nav display="flex" flex={2} justifyContent={'center'} alignItems="center">
+        <YStack gap={'$4'} alignItems="flex-start" justifyContent="center">
+          <SideBarNavLink
+            icon={<IconDashboard size={'$2'} />}
+            text={'Dashboard'}
+            href={'/'}
+            isActive={pathName === '/'}
+          />
+          <SideBarNavLink
+            icon={<IconDistributions size={'$2'} />}
+            text={'Distributions'}
+            href={'/distributions'}
+            isActive={pathName.includes('/distributions')}
+          />
+          <SideBarNavLink
+            icon={<IconSLogo size={'$2'} />}
+            text={'Leaderboard'}
+            href={'/leaderboard'}
+            isActive={pathName.includes('/leaderboard')}
+            disabled={true}
+            hoverStyle={{ cursor: 'not-allowed' }}
+          />
         </YStack>
       </Nav>
-      <YStack gap="$4" py="$4" alignItems='center'>
+      <YStack gap="$4" py="$4" alignItems="center">
         <IconSendLogo />
         <XStack gap="$2">
-          <SideBarFooterLink icon={<IconXLogo />} href={twitterSocial} target='_blank' borderRadius={9999} />
-          <SideBarFooterLink icon={<IconTelegramLogo />} href={telegramSocial} target='_blank' borderRadius={9999} />
+          <SideBarFooterLink
+            icon={<IconXLogo />}
+            href={twitterSocial}
+            target="_blank"
+            borderRadius={9999}
+          />
+          <SideBarFooterLink
+            icon={<IconTelegramLogo />}
+            href={telegramSocial}
+            target="_blank"
+            borderRadius={9999}
+          />
         </XStack>
       </YStack>
     </BottomSheet>
@@ -82,17 +153,14 @@ export const HomeSideBarWrapper = ({ children }: { children?: React.ReactNode })
 
   if (media.gtLg)
     return (
-      <SideBarWrapper sidebar={< HomeSideBar backgroundColor={"$backgroundStrong"} />}>
+      <SideBarWrapper sidebar={<HomeSideBar backgroundColor={'$backgroundStrong'} />}>
         {children}
-      </SideBarWrapper >
-
+      </SideBarWrapper>
     )
-  else return (
+  return (
     <>
       <HomeBottomSheet />
       {children}
     </>
-
   )
 }
-
