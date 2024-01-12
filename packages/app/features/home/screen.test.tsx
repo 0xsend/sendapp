76234ae1efbdd27dc39d76f263ecaed29d253e60
youngkidwarrior <victor@she.energy>
2024-01-12 14:16:36 -0800

    Mock HomeHeader data dependencies & new snapshot

--- packages/app/features/home/screen.test.tsx
+++ packages/app/features/home/screen.test.tsx
@@ -1,7 +1,41 @@
 import { expect, test } from '@jest/globals'
+import { TamaguiProvider, config } from '@my/ui'
 import { render } from '@testing-library/react-native'
 import { HomeScreen } from './screen'
-import { TamaguiProvider, config } from '@my/ui'
+
+jest.mock('app/utils/useUser', () => ({
+  useUser: jest.fn().mockReturnValue({
+    user: {
+      id: '123',
+      profile: { referral_code: '123' },
+    },
+  }),
+}))
+
+jest.mock('app/routers/params', () => ({
+  useNav: jest.fn().mockReturnValue([undefined, jest.fn()]),
+}))
+
+jest.mock('wagmi', () => ({
+  useAccount: jest.fn().mockReturnValue({
+    address: '0x123',
+    isConnected: false,
+  }),
+  useConnect: jest.fn().mockReturnValue({
+    connectAsync: jest.fn(),
+  }),
+  useDisconnect: jest.fn().mockReturnValue({
+    disconnect: jest.fn(),
+  }),
+}))
+
+jest.mock('app/utils/getReferralLink', () => ({
+  getReferralHref: jest.fn().mockReturnValue('https://send.it/123'),
+}))
+
+jest.mock('solito/link', () => ({
+  Link: jest.fn(),
+}))
 
 test('HomeScreen', () => {
   const tree = render(
