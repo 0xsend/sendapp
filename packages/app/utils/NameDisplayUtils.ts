57bc3800420dcde248dd3091a26a7ec42633f0e2
youngkidwarrior <victor@she.energy>
2024-01-19 16:45:49 -0800

    Display Name Utils

---
+++ packages/app/utils/NameDisplayUtils.ts
@@ -0,0 +1,23 @@
+import { Address, isAddress } from 'viem'
+
+export const veryShortTag = (tag: string) => {
+  if (tag.length < 7) {
+    return tag
+  }
+  return tag.substring(0, 6).concat('...')
+}
+export const veryShortAddress = (address: Address) =>
+  [address.substring(0, 3), address.substring(address.length - 1, address.length)].join('...')
+
+export const shortTag = (tag: string) => {
+  if (tag.length < 15) {
+    return tag
+  }
+  return tag.substring(0, 15).concat('...')
+}
+
+export const useShortAddress = (address?: Address) => {
+  address && isAddress(address)
+    ? [address.substring(0, 4), address.substring(38, 42)].join('...')
+    : undefined
+}
