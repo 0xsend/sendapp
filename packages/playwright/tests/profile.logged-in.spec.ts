85017cfc336823740aeedb8e6b6d3918fe06b459
Big Boss <bigboss@metalrodeo.xyz>
2024-01-25 22:11:28 -0600

    wip logged in profile specs

--- packages/playwright/tests/profile.logged-in.spec.ts
+++ packages/playwright/tests/profile.logged-in.spec.ts
@@ -1,8 +1,12 @@
-import { expect, mergeTests } from '@playwright/test'
-import { test } from '@my/playwright/fixtures/supawright'
+import { expect, test as authTest } from './fixtures/auth'
+import { test as supawrightTest } from '@my/playwright/fixtures/supawright'
 import { debug, Debugger } from 'debug'
 import { createOtherUser } from './fixtures/supawright'
 import { assert } from 'app/utils/assert'
+import { OnboardingPage } from './fixtures/send-accounts'
+import { mergeTests } from '@playwright/test'
+
+const test = mergeTests(supawrightTest, authTest)
 
 let log: Debugger
 
@@ -10,14 +14,19 @@ test.beforeAll(async () => {
   log = debug('test:profile:anon')
 })
 
-test('anon user can visit public profile', async ({ page, supawright }) => {
+test('logged in user needs onboarding before visiting profile', async ({ page, supawright }) => {
   const { tag } = await createOtherUser(supawright)
   await page.goto(`/profile/${tag.name}`)
-  const title = await page.title()
-  expect(title).toBe('Send | Profile')
+  expect(await page.title()).toBe('Send | Onboarding')
+  await new OnboardingPage(page).completeOnboarding(expect)
+
+  // @todo check that user is redirected back to profile page
+
+  await page.goto(`/profile/${tag.name}`)
+
+  expect(await page.title()).toBe('Send | Profile')
   await expect(page.getByRole('heading', { name: tag.name })).toBeVisible()
   await expect(page.getByRole('button', { name: 'Send' })).toBeVisible()
-  await expect(page.getByRole('button', { name: 'Request' })).toBeVisible()
 })
 
 test('anon user cannot visit private profile', async ({ page, supawright }) => {
