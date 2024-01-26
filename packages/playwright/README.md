f5043f5461f85c05202c6f630f5b9f982041de18
Big Boss <bigboss@metalrodeo.xyz>
2024-01-25 21:59:11 -0600

    clean up specs, add private profile case

--- packages/playwright/README.md
+++ packages/playwright/README.md
@@ -1,3 +1,13 @@
 # playwright
 
 End-to-end testing for Send web applications.
+
+## Specs
+
+There are currently 3 kinds of specs:
+
+- `*.anon.spec.ts` - anonymous specs, which do not require a user to be logged in.
+- `*.logged-in.spec.ts` - authenticated specs, which require a user to be logged in, but not onboarded.
+- `*.onboarded.spec.ts` - onboarded specs, which require a user to be logged in and onboarded.
+
+These specs leverage fixtures to create the necessary users and data to run the tests using these personas.
--- packages/playwright/tests/profile.anon.spec.ts
+++ packages/playwright/tests/profile.anon.spec.ts
@@ -2,6 +2,7 @@ import { expect, mergeTests } from '@playwright/test'
 import { test } from '@my/playwright/fixtures/supawright'
 import { debug, Debugger } from 'debug'
 import { createOtherUser } from './fixtures/supawright'
+import { assert } from 'app/utils/assert'
 
 let log: Debugger
 
@@ -17,4 +18,22 @@ test('anon user can visit public profile', async ({ page, supawright }) => {
   await expect(page.getByRole('heading', { name: tag.name })).toBeVisible()
   await expect(page.getByRole('button', { name: 'Send' })).toBeVisible()
   await expect(page.getByRole('button', { name: 'Request' })).toBeVisible()
+})
+
+test('anon user cannot visit private profile', async ({ page, supawright }) => {
+  const { otherUser, tag } = await createOtherUser(supawright)
+  const { error } = await supawright
+    .supabase('public')
+    .from('profiles')
+    .update({
+      is_public: false,
+    })
+    .eq('id', otherUser.id)
+  assert(!error, error?.message)
+  await page.goto(`/profile/${tag.name}`)
+  const title = await page.title()
+  expect(title).toBe('Send | Profile')
+  await expect(page.getByRole('heading', { name: tag.name })).toBeVisible()
+  await expect(page.getByRole('button', { name: 'Send' })).not.toBeVisible()
+  await expect(page.getByRole('button', { name: 'Request' })).not.toBeVisible()
 })
