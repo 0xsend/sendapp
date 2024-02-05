9cab95db8db4fe0ddec1810556114432256c8f3c
LeO <devgru0114@gmail.com>
2024-02-05 14:02:49 +0000

    replace settings icon

--- packages/app/components/HomeHeader.tsx
+++ packages/app/components/HomeHeader.tsx
@@ -1,10 +1,10 @@
-import { Anchor, Button, H1, Header, Link, Text, XStack, useToastController } from '@my/ui'
+import { Anchor, Button, H1, Header, Link, Text, XStack, YStack, useToastController } from '@my/ui'
 import { useNav } from 'app/routers/params'
 import { useUserReferralsCount } from 'app/utils/UseUserReferralsCount'
 import { getReferralHref } from 'app/utils/getReferralLink'
 import { useUser } from 'app/utils/useUser'
 import { useAccount, useConnect, useDisconnect } from 'wagmi'
-import { IconClose, IconCopy, IconHamburger, IconSettings, IconStar } from './icons'
+import { IconClose, IconCopy, IconGear, IconHamburger, IconStar } from './icons'
 
 // TODO: this should probably named HomeTopNav
 export function HomeHeader({ children }: { children: string }) {
@@ -109,17 +109,19 @@ function SettingsButton() {
   return (
     <XStack
       ai="center"
+      jc="center"
       space="$2"
       $gtSm={{ fd: 'row' }}
-      cursor="not-allowed"
       bg="$background"
       h="100%"
       f={1}
       px={'$3'}
       br="$4"
     >
-      <Link href="" cursor="not-allowed" alignSelf="stretch">
-        <IconSettings size={'$2'} />
+      <Link href="/settings" alignSelf="stretch">
+        <YStack ai={'center'}>
+          <IconGear size={'$2'} />
+        </YStack>
       </Link>
     </XStack>
   )
