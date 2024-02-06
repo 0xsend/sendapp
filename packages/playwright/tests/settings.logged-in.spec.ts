532eb6dc54ed5a74b6b9d0c1a04ff6932d6dd500
LeO <devgru0114@gmail.com>
2024-02-06 03:50:27 +0000

    add supabase db check, move update-profile spec to settings

--- packages/playwright/tests/settings.logged-in.spec.ts
+++ packages/playwright/tests/settings.logged-in.spec.ts
@@ -16,3 +16,35 @@ test('can visit settings page', async ({ page }) => {
   await page.goto('/settings')
   await expect(page).toHaveURL('/settings')
 })
+
+test('can update profile', async ({ page }) => {
+  await page.goto('/settings')
+  log('beforeEach', `url=${page.url()}`)
+  const editProfileButton = page.getByRole('button', { name: 'Edit Profile' })
+  await editProfileButton.click()
+
+  await page.waitForNavigation()
+  await expect(page).toHaveTitle('Account')
+
+  await page.getByLabel('Name').fill('LeO')
+  await page.getByLabel('About').fill('Sender')
+  await page.getByLabel('IsPublic').setChecked(true)
+
+  await page.getByRole('button', { name: 'Update Profile' }).click()
+
+  await expect(page.getByText('Successfully updated')).toBeVisible()
+
+  const { data, error } = await supabaseAdmin.auth.getSession()
+  expect(error).toBeFalsy()
+
+  if (data.session) {
+    const { data: user, error } = await supabaseAdmin
+      .from('profiles')
+      .select('*')
+      .eq('id', data.session?.user.id)
+      .maybeSingle()
+    expect(error).toBeFalsy()
+    expect(user).toBeTruthy()
+    expect(user?.name === 'LeO' && user.about === 'Sender' && user.is_public === true).toBeTruthy()
+  }
+})
