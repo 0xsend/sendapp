8366446ef3e75a8acc616ce71d7ec89a43c39ec0
LeO <devgru0114@gmail.com>
2024-02-06 14:39:02 +0000

    refactor settings test

--- packages/playwright/tests/settings.logged-in.spec.ts
+++ packages/playwright/tests/settings.logged-in.spec.ts
@@ -1,25 +1,13 @@
-import { supabaseAdmin } from 'app/utils/supabase/admin'
-import debug from 'debug'
-// import { Tables } from '@my/supabase/database.types'
 import { expect, test } from './fixtures/send-accounts'
-// import { Page } from '@playwright/test'
-
-let log: debug.Debugger
-
-test.beforeEach(async ({ page }) => {
-  log = debug(`test:profile:logged-in:${test.info().parallelIndex}`)
-  log('beforeEach', `url=${page.url()}`)
-  // TODO: Make user eligible for distributions
-})
 
 test('can visit settings page', async ({ page }) => {
   await page.goto('/settings')
   await expect(page).toHaveURL('/settings')
 })
 
-test('can update profile', async ({ page }) => {
+test('can update profile', async ({ page, supabase }) => {
   await page.goto('/settings')
-  log('beforeEach', `url=${page.url()}`)
+
   const editProfileButton = page.getByRole('button', { name: 'Edit Profile' })
   await editProfileButton.click()
 
@@ -34,11 +22,11 @@ test('can update profile', async ({ page }) => {
 
   await expect(page.getByText('Successfully updated')).toBeVisible()
 
-  const { data, error } = await supabaseAdmin.auth.getSession()
+  const { data, error } = await supabase.auth.getSession()
   expect(error).toBeFalsy()
 
   if (data.session) {
-    const { data: user, error } = await supabaseAdmin
+    const { data: user, error } = await supabase
       .from('profiles')
       .select('*')
       .eq('id', data.session?.user.id)
