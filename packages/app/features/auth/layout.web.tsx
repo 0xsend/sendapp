421f8f5f12baa8de415cf9ed2d1207caf719076e
youngkidwarrior <victor@she.energy>
2024-02-13 00:13:55 -0800

    Use context to share auth images between pages and layouts

--- packages/app/features/auth/layout.web.tsx
+++ packages/app/features/auth/layout.web.tsx
@@ -1,16 +1,42 @@
 import { ScrollView, YStack, useWindowDimensions } from '@my/ui'
 
 import { SignInSideBarWrapper } from 'app/components/sidebar/SignInSideBar'
+import { useMemo, useState } from 'react'
+import { AuthCarouselContext } from './AuthCarouselContext'
+import { SolitoImage } from 'solito/image'
+import { type GetPlaiceholderImage } from 'app/utils/getPlaiceholderImage'
 
 export function AuthLayout({ children }: { children: React.ReactNode; header?: string }) {
+  const [carouselImages, setCarouselImages] = useState<GetPlaiceholderImage[]>([])
+  const [carouselProgress, setCarouselProgress] = useState(0)
   const { height: windowHeight } = useWindowDimensions()
-  return (
-    <SignInSideBarWrapper>
-      <YStack h={windowHeight} f={1}>
-        <ScrollView f={3} fb={0} jc="center" backgroundColor={'$backgroundHover'}>
-          {children}
-        </ScrollView>
-      </YStack>
-    </SignInSideBarWrapper>
+
+  const carouselImage = carouselImages[carouselProgress]
+
+  return useMemo(
+    () => (
+      <AuthCarouselContext.Provider
+        value={{ carouselImages, setCarouselImages, carouselProgress, setCarouselProgress }}
+      >
+        <SignInSideBarWrapper>
+          {carouselImage && (
+            <SolitoImage
+              placeholder="blur"
+              blurDataURL={carouselImage.base64}
+              src={carouselImage.img.src}
+              fill={true}
+              style={{ objectFit: 'cover' }}
+              alt="sign-in-carousel"
+            />
+          )}
+          <YStack h={windowHeight} f={1}>
+            <ScrollView f={3} fb={0} jc="center" backgroundColor={'$transparent'}>
+              {children}
+            </ScrollView>
+          </YStack>
+        </SignInSideBarWrapper>
+      </AuthCarouselContext.Provider>
+    ),
+    [carouselImage, carouselImages, carouselProgress, children, windowHeight]
   )
 }
