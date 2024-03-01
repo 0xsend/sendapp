d6a292879fd6427e18850f7e9604511560d19ead
LeO <devgru0114@gmail.com>
2024-03-01 01:26:29 +0000

    fix total balance format

--- packages/app/utils/useSendAccountBalances.ts
+++ packages/app/utils/useSendAccountBalances.ts
@@ -5,8 +5,10 @@ import {
 } from '@my/wagmi'
 import { UseBalanceReturnType, useBalance } from 'wagmi'
 import { useSendAccounts } from './send-accounts'
+import { useEffect, useState } from 'react'
 
 export const useSendAccountBalances = () => {
+  const [tokenPrices, setTokenPrices] = useState<{ [key: string]: { usd: number } }>({})
   const { data: sendAccounts } = useSendAccounts()
   const sendAccount = sendAccounts?.[0]
 
@@ -24,12 +26,30 @@ export const useSendAccountBalances = () => {
     })
   }
 
-  let totalBalance = 0n
+  useEffect(() => {
+    const func = async () => {
+      const res = await fetch(
+        'https://api.coingecko.com/api/v3/simple/price?ids=ethereum,usd-coin,send-token&vs_currencies=usd'
+      )
+      const data = await res.json()
+      setTokenPrices({
+        USDC: data['usd-coin'],
+        ETH: data.ethereum,
+        send: data['send-token'],
+      })
+    }
+    func()
+  }, [])
+
+  let totalBalance = 0
   for (const token of tokens) {
     if (balances[token]?.isPending) {
       return { balances, undefined }
     }
-    totalBalance += balances[token]?.data?.value ?? 0n
+    const tokenPrice = tokenPrices[balances[token]?.data?.symbol as string]?.usd ?? 1
+    totalBalance +=
+      (Number(balances[token]?.data?.value ?? 0n) / 10 ** (balances[token]?.data?.decimals ?? 0)) *
+      tokenPrice
   }
 
   return { balances, totalBalance }
