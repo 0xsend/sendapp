4744baa98224dfed6b2eedc7a13dfce05eedd7a8
LeO <devgru0114@gmail.com>
2024-03-04 12:58:29 +0000

    use zod for tokenPrice schema

--- packages/app/utils/useTokenPrices.ts
+++ packages/app/utils/useTokenPrices.ts
@@ -1,4 +1,15 @@
 import { useQuery } from '@tanstack/react-query'
+import { z } from 'zod'
+
+const TokenPriceSchema = z.object({
+  usd: z.number(),
+})
+
+export const TokenPricesSchema = z.object({
+  ethereum: TokenPriceSchema,
+  'send-token': TokenPriceSchema,
+  'usd-coin': TokenPriceSchema,
+})
 
 export const useTokenPrices = () => {
   const {
@@ -11,7 +22,10 @@ export const useTokenPrices = () => {
       const res = await fetch(
         'https://api.coingecko.com/api/v3/simple/price?ids=ethereum,usd-coin,send-token&vs_currencies=usd'
       )
-      return await res.json()
+      const json = await res.json()
+      TokenPricesSchema.parse(json)
+
+      return json
     },
   })
 
