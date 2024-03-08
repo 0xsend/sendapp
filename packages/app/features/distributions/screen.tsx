d7fefa2cbd1efde21655c580fce065c07a3caee9
Big Boss <bigboss@metalrodeo.xyz>
2024-03-07 21:55:30 -0600

    distribution: fix hydration warning

--- packages/app/features/distributions/screen.tsx
+++ packages/app/features/distributions/screen.tsx
@@ -7,6 +7,7 @@ import {
   Text,
   Theme,
   Tooltip,
+  TooltipGroup,
   XStack,
   YStack,
 } from '@my/ui'
@@ -38,37 +39,56 @@ export function DistributionsScreen() {
 
 const DistributionSection = () => {
   const { chain, address } = useAccount()
-
   const blockExplorerLink = useLink({
     href: `${chain?.blockExplorers.default.url}/address/${sendAirdropsSafeAddress}`,
   })
 
   return (
     <YStack gap="$4">
-      <YStack px="$4.5" ai="flex-end" gap="$2" jc="flex-end" alignSelf="flex-end" mb="$4">
-        <Theme name="alt2">
-          {chain && (
+      {chain && (
+        <YStack px="$4.5" ai="flex-end" gap="$2" jc="flex-end" alignSelf="flex-end" mb="$4">
+          <Theme name="alt2">
             <Button size="$2" chromeless {...blockExplorerLink} iconAfter={ArrowRight}>
               View on {chain.blockExplorers.default.name}
             </Button>
-          )}
-        </Theme>
-        <Theme name="alt2">
-          <Tooltip>
-            <Tooltip.Trigger>
-              <Paragraph size="$1">
-                Connected as <Text fontFamily={'$mono'}>{shorten(address)}</Text>
-              </Paragraph>
-            </Tooltip.Trigger>
-            <Tooltip.Content>
-              <Anchor href={`${chain?.blockExplorers.default.url}/address/${address}`} size="$1">
-                <Text fontFamily={'$mono'}>{address}</Text>
-              </Anchor>
-            </Tooltip.Content>
-          </Tooltip>
-        </Theme>
-      </YStack>
-
+          </Theme>
+          <Theme name="alt2">
+            <TooltipGroup delay={{ open: 3000, close: 100 }}>
+              <Tooltip>
+                <Tooltip.Trigger>
+                  <Paragraph size="$1">
+                    Connected as <Text fontFamily={'$mono'}>{shorten(address)}</Text>
+                  </Paragraph>
+                </Tooltip.Trigger>
+                <Tooltip.Content
+                  enterStyle={{ x: 0, y: -5, opacity: 0, scale: 0.9 }}
+                  exitStyle={{ x: 0, y: -5, opacity: 0, scale: 0.9 }}
+                  scale={1}
+                  x={0}
+                  y={0}
+                  opacity={1}
+                  animation={[
+                    'quick',
+                    {
+                      opacity: {
+                        overshootClamping: true,
+                      },
+                    },
+                  ]}
+                >
+                  <Tooltip.Arrow />
+                  <Anchor
+                    href={`${chain?.blockExplorers.default.url}/address/${address}`}
+                    size="$1"
+                  >
+                    <Text fontFamily={'$mono'}>{address}</Text>
+                  </Anchor>
+                </Tooltip.Content>
+              </Tooltip>
+            </TooltipGroup>
+          </Theme>
+        </YStack>
+      )}
       <XStack flexWrap="wrap" ai="flex-start" jc="flex-start" px="$4" gap="$8" mb="$4">
         <DistributionProgressCard />
         <DistributionTimeCard />
