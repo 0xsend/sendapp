c97494cd08425732760b84bf3405058b88f93377
youngkidwarrior <victor@she.energy>
2024-03-08 03:08:32 -0800

    Change Favicon

--- apps/next/pages/_app.tsx
+++ apps/next/pages/_app.tsx
@@ -43,7 +43,13 @@ function MyApp({
           name="viewport"
           content="width=320, initial-scale=1, minimum-scale=1, maximum-scale=1"
         />
-        <Favicons />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
         <meta property="og:image" content="https://0xsend.github.io/assets/sendtags.png" />
       </Head>
       <NextThemeProvider
