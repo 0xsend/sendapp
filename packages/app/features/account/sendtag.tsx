c296a0878878c019e0c5f8ec91fa38ea621055aa
LeO <leoforever913@gmail.com>
2024-03-14 19:43:09 +0000

    import fix

---
+++ packages/app/features/account/sendtag.tsx
@@ -0,0 +1,14 @@
+import { Paragraph, Separator, YStack } from '@my/ui'
+
+export const SendtagScreen = () => {
+  use
+  return (
+    <YStack>
+      <Paragraph>
+        Sendtags are personalized names that serve as unique identifiers within the Send platform.
+        You may register up to 5.
+      </Paragraph>
+      <Separator />
+    </YStack>
+  )
+}
