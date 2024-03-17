e0bf3af42f080f47bbb2f7e2e6d614e4c12a3914
youngkidwarrior <victor@she.energy>
2024-03-16 19:54:28 -0700

    Sendtag page data and styles

--- packages/app/features/account/sendtag/screen.tsx
+++ packages/app/features/account/sendtag/screen.tsx
@@ -1,15 +1,6 @@
-import {
-  Container,
-  H3,
-  Label,
-  ListItem,
-  Paragraph,
-  Spinner,
-  Stack,
-  Theme,
-  XStack,
-  YStack,
-} from '@my/ui'
+import { Tables } from '@my/supabase/database.types'
+import { Container, H3, Label, ListItem, Paragraph, Spinner, Stack, YStack, Link } from '@my/ui'
+
 import { useUser } from 'app/utils/useUser'
 
 const maxNumSendTags = 5
@@ -17,7 +8,11 @@ const maxNumSendTags = 5
 export function SendTagScreen() {
   const { tags, isLoading } = useUser()
 
-  const allTags = tags?.concat(Array(maxNumSendTags - tags.length).fill(undefined))
+  const allTags: (Tables<'tags'> | undefined)[] =
+    tags === undefined
+      ? new Array(maxNumSendTags).fill(undefined)
+      : [...tags, ...Array.from({ length: maxNumSendTags - tags.length })]
+  const nextTagIndex = allTags?.findIndex((tag) => tag === undefined)
 
   if (isLoading)
     return (
@@ -28,45 +23,71 @@ export function SendTagScreen() {
 
   return (
     <Container>
-      <YStack f={1} $lg={{ gap: '$2' }}>
+      <YStack
+        f={1}
+        $lg={{ gap: '$2', ai: 'center' }}
+        $theme-dark={{ btc: '$gray7Dark' }}
+        $theme-light={{ btc: '$gray4Light' }}
+        $gtSm={{ borderTopWidth: '$1' }}
+      >
         <Stack gap="$2" $gtSm={{ py: '$6', gap: '$6' }}>
-          <Label fontFamily={'$mono'} fontSize={'$5'}>
+          <Label fontFamily={'$mono'} fontSize={'$5'} $theme-dark={{ col: '$olive' }}>
             REGISTERED SENDTAGS [
-            <Paragraph fontFamily={'$mono'} fontSize={'$5'} theme={'accent'}>
-              {`${tags?.length} / ${maxNumSendTags}`}
+            <Paragraph fontFamily={'$mono'} fontSize={'$5'} $theme-dark={{ col: '$primary' }}>
+              {`${tags?.length || 0} / ${maxNumSendTags}`}
             </Paragraph>
             ]
           </Label>
         </Stack>
         <YStack gap="$5">
-          {allTags?.map((tag) => {
-            return tag === undefined ? (
-              <ListItem
-                key={tag}
-                w={200}
-                h={54}
-                br={8}
-                borderWidth={1}
-                borderStyle="dashed"
-                $theme-dark={{ borderColor: '$gray4Light' }}
-                $theme-light={{ borderColor: '$gray4Dark' }}
-              />
-            ) : (
-              <ListItem
-                key={tag.name}
-                h={54}
-                br={8}
-                $theme-dark={{ bc: '$darkest' }}
-                $theme-light={{ bc: '$gray4Light' }}
-              >
-                <H3 fontSize={'$5'} fontWeight={'500'} theme="accent">
-                  {tag.name}
-                </H3>
-              </ListItem>
-            )
-          })}
+          {allTags.map((tag, i) => (
+            <TagItem key={tag?.name} tag={tag} isNextTag={i === nextTagIndex} />
+          ))}
         </YStack>
       </YStack>
     </Container>
+  )
+}
+
+function TagItem({ tag, isNextTag }: { tag?: Tables<'tags'>; isNextTag: boolean }) {
+  if (isNextTag)
+    return (
+      <Link
+        href={'/account/sendtag/checkout'}
+        w={200}
+        h={54}
+        br={12}
+        bc={'$primary'}
+        p={0}
+        display="flex"
+        jc="center"
+        ai="center"
+      >
+        <Paragraph fontSize={'$4'} fontWeight={'500'} fontFamily={'$mono'} theme="accent">
+          + New Tag
+        </Paragraph>
+      </Link>
+    )
+
+  if (tag === undefined)
+    return (
+      <ListItem
+        key={tag}
+        w={200}
+        h={54}
+        br={8}
+        borderWidth={1}
+        borderStyle="dashed"
+        $theme-dark={{ borderColor: '$decay' }}
+        $theme-light={{ borderColor: '$gray4Dark' }}
+      />
+    )
+
+  return (
+    <ListItem h={54} br={12} $theme-dark={{ bc: '$darkest' }} $theme-light={{ bc: '$gray4Light' }}>
+      <H3 fontSize={'$5'} fontWeight={'500'} theme="accent">
+        {tag.name}
+      </H3>
+    </ListItem>
   )
 }
