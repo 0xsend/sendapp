4243a6fc7150cbdc0e0cd0725fc0d5b0968ac704
LeO <leoforever913@gmail.com>
2024-03-18 12:20:27 +0000

    header fix

--- packages/app/components/HomeTopNav.tsx
+++ packages/app/components/HomeTopNav.tsx
@@ -1,9 +1,10 @@
-import { Button, H2, Header, Paragraph, XStack, Stack, useMedia } from '@my/ui'
+import { Button, H2, Header, Paragraph, XStack, Stack, useMedia, Link, Container } from '@my/ui'
 import { useNav } from 'app/routers/params'
 import { useThemeSetting } from '@tamagui/next-theme'
-import { IconArrowLeft, IconHamburger, IconQr, IconSendLogo } from 'app/components/icons'
+import { IconArrowLeft, IconGear, IconHamburger, IconQr, IconSendLogo } from 'app/components/icons'
 import { usePathname } from 'app/utils/usePathname'
 import { useRouter } from 'solito/router'
+import { SettingsBottomSheet } from './settings/SettingsBottomSheet'
 
 export function HomeTopNav({
   header,
@@ -13,8 +14,13 @@ export function HomeTopNav({
   const [nav, setNavParam] = useNav()
   const path = usePathname()
   const { push } = useRouter()
+
   const handleHomeBottomSheet = () => {
     setNavParam(nav ? undefined : 'home', { webBehavior: 'replace' })
+  }
+
+  const handleSettingsBottomSheet = () => {
+    setNavParam(nav ? undefined : 'settings', { webBehavior: 'replace' })
   }
 
   const handleBack = () => {
@@ -30,65 +36,79 @@ export function HomeTopNav({
   const isSubRoute = path.split('/').length - 1 > 1
 
   return (
-    <Header w="100%" mb={'$6'}>
-      <XStack
-        $gtLg={{ jc: 'flex-start', pb: '$2' }}
-        jc="space-between"
-        fd="row"
+    <>
+      {media.lg ? <SettingsBottomSheet /> : <></>}
+      <Header
+        w="100%"
+        mb={'$6'}
         $lg={{ borderBottomColor: '$gray11Light', borderBottomWidth: '$1', py: '$4' }}
       >
-        <Stack>
-          {isSubRoute ? (
-            <Button
-              outlineColor={'transparent'}
-              focusStyle={{ outlineColor: 'transparent' }}
-              onPress={handleBack}
-              bg="transparent"
-              ai="flex-start"
-              hoverStyle={{ backgroundColor: 'transparent', borderColor: 'transparent' }}
-              pressStyle={{ backgroundColor: 'transparent', borderColor: 'transparent' }}
-              icon={<IconArrowLeft size={'$2.5'} color={iconColor} />}
-            />
-          ) : (
-            <Button
-              $gtLg={{ disabled: true, opacity: 0 }} // We need the button to be there for layout purposes
-              onPress={handleHomeBottomSheet}
-              bg="transparent"
-              icon={<IconHamburger size={'$2.5'} color={iconColor} />}
-            />
-          )}
-        </Stack>
-        {header === 'Home' && media.md ? (
-          <XStack>
-            <IconSendLogo size={'$2.5'} color={'$color12'} />
+        <Container>
+          <XStack
+            w={'100%'}
+            $gtLg={{ jc: 'flex-start', pb: '$2' }}
+            jc="space-between"
+            ai="center"
+            fd="row"
+          >
+            {isSubRoute ? (
+              <Link href={'#'} onPress={handleBack}>
+                <Stack ai={'center'}>
+                  <IconArrowLeft size={'$2.5'} color={iconColor} />
+                </Stack>
+              </Link>
+            ) : (
+              <Link href={'#'} $gtLg={{ display: 'none' }} onPress={handleHomeBottomSheet}>
+                <Stack ai={'center'}>
+                  <IconHamburger size={'$2.5'} color={iconColor} />
+                </Stack>
+              </Link>
+            )}
+            {header === 'Home' && media.md ? (
+              <XStack>
+                <IconSendLogo size={'$2.5'} color={'$color12'} />
+              </XStack>
+            ) : media.gtLg || (media.lg && submenuHeader === '') ? (
+              <H2 fontWeight={'300'} color={'$color05'} lineHeight={32}>
+                {header}
+              </H2>
+            ) : (
+              <H2 fontSize={'$8'} fontWeight={'300'} color={'$color05'} lineHeight={'$5'}>
+                {submenuHeader}
+              </H2>
+            )}
+            {path.includes('/account') ? (
+              <Link href={'#'} $gtLg={{ display: 'none' }} onPress={handleSettingsBottomSheet}>
+                <Stack ai={'center'}>
+                  <IconGear size={'$2.5'} color={iconColor} />
+                </Stack>
+              </Link>
+            ) : (
+              <Link href={'#'} $gtLg={{ display: 'none' }}>
+                <Stack ai={'center'}>
+                  <IconQr size={'$2.5'} color={iconColor} />
+                </Stack>
+              </Link>
+            )}
           </XStack>
-        ) : (
-          <H2 fontWeight={'300'} color={'$color05'} lineHeight={32}>
-            {media.gtLg ? header : submenuHeader}
-          </H2>
+        </Container>
+        {subheader && (
+          <Paragraph
+            fontWeight={'400'}
+            fontSize={'$5'}
+            fontFamily={'$mono'}
+            lineHeight={24}
+            px="$6"
+            py="$3"
+            $gtSm={{ py: '$6' }}
+            $gtLg={{ ml: '$7', pb: '$4', pt: '$0' }}
+            $theme-light={{ col: '$gray10Light' }}
+            $theme-dark={{ col: '$gray10Dark' }}
+          >
+            {subheader}
+          </Paragraph>
         )}
-        <Button
-          $gtLg={{ display: 'none' }}
-          bg="transparent"
-          icon={<IconQr size={'$2.5'} color={iconColor} />}
-        />
-      </XStack>
-      {subheader && (
-        <Paragraph
-          fontWeight={'400'}
-          fontSize={'$5'}
-          fontFamily={'$mono'}
-          lineHeight={24}
-          px="$6"
-          py="$3"
-          $gtSm={{ py: '$6' }}
-          $gtLg={{ ml: '$7', pb: '$4', pt: '$0' }}
-          $theme-light={{ col: '$gray10Light' }}
-          $theme-dark={{ col: '$gray10Dark' }}
-        >
-          {subheader}
-        </Paragraph>
-      )}
-    </Header>
+      </Header>
+    </>
   )
 }
