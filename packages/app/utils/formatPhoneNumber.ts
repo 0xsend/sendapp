ffbd6c42fac76eef958ff14faee6164327f0dff0
Brandon Young <brandon@envoymobile.com>
2024-03-19 09:12:07 -0400

    Fix phone number formatting on accounts page

--- packages/app/utils/formatPhoneNumber.ts
+++ packages/app/utils/formatPhoneNumber.ts
@@ -2,10 +2,12 @@ export const formatPhoneNumber = (phoneNumber: string | undefined): string => {
   if (!phoneNumber) {
     return ''
   }
-  const number = phoneNumber.trim().replace(/[^0-9]/g, '')
+  const number = phoneNumber.trim().replace(/\D/g, '')
 
-  if (number.length < 4) return number
-  if (number.length < 7) return number.replace(/(\d{3})(\d{1})/, '$1-$2')
-  if (number.length < 11) return number.replace(/(\d{3})(\d{3})(\d{1})/, '$1-$2-$3')
-  return number.replace(/(\d{3})(\d{4})(\d{4})/, '$1-$2-$3')
+  const match = number.match(/^(1|)?(\d{3})(\d{3})(\d{4})$/)
+  if (match) {
+    const intlCode = match[1] ? '+1 ' : ''
+    return [intlCode, '(', match[2], ') ', match[3], '-', match[4]].join('')
+  }
+  return number
 }
