bf83e2fe7ae940deeeea02f121e961da5af99024
BigBoss <bigboss@metalrodeo.xyz>
2024-03-21 17:56:06 -0500

    ui: clean up container and spacings (#226)
    fixes ultrawide screen header position
    makes spacing between header and screen consistent

--- packages/ui/src/components/Container.tsx
+++ packages/ui/src/components/Container.tsx
@@ -4,23 +4,27 @@ export const Container: React.FC<XStackProps> = (props: XStackProps) => {
   return (
     <XStack
       px="$6"
-      $gtSm={{
-        maxWidth: 768,
-      }}
-      $gtMd={{
-        maxWidth: 960,
-      }}
-      $gtLg={{
-        maxWidth: 1440,
-        px: '$11',
-      }}
-      $gtXl={{
-        maxWidth: 1920,
-      }}
       als="center"
       f={1}
       width={'100%'}
       {...props}
+      $gtSm={{
+        maxWidth: 768,
+        ...(props.$gtSm ?? {}),
+      }}
+      $gtMd={{
+        maxWidth: 960,
+        ...(props.$gtMd ?? {}),
+      }}
+      $gtLg={{
+        maxWidth: 1440,
+        px: '$11',
+        ...(props.$gtLg ?? {}),
+      }}
+      $gtXl={{
+        maxWidth: 1920,
+        ...(props.$gtXl ?? {}),
+      }}
     >
       {props.children}
     </XStack>
