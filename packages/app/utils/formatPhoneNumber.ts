90e9708bf90aa35bd1de25d2f5e31bfc7fefa936
Brandon Young <brandon@envoymobile.com>
2024-03-28 14:18:29 -0400

    Update formatphone fun to handle international formatting according to E.164

--- packages/app/utils/formatPhoneNumber.ts
+++ packages/app/utils/formatPhoneNumber.ts
@@ -1,13 +1,27 @@
+import { parsePhoneNumber } from 'libphonenumber-js'
+
 export const formatPhoneNumber = (phoneNumber: string | undefined): string => {
   if (!phoneNumber) {
     return ''
   }
-  const number = phoneNumber.trim().replace(/\D/g, '')
+  let number = phoneNumber.trim().replace(/\D/g, '')
+  if (!number.startsWith('+') && number.length >= 11) {
+    number = `+${number}`
+  }
 
-  const match = number.match(/^(1|)?(\d{3})(\d{3})(\d{4})$/)
-  if (match) {
-    const intlCode = match[1] ? '+1 ' : ''
-    return [intlCode, '(', match[2], ') ', match[3], '-', match[4]].join('')
+  try {
+    const nparse = parsePhoneNumber(number)
+    if (nparse) {
+      // is US number, format it as US number
+      if (number.startsWith('+1') && number.length === 12) {
+        return nparse.formatNational()
+      }
+      return nparse.formatInternational()
+    }
+  } catch (e) {
+    // dont really want to throw an error here as then the app will not display anything,
+    // so just return the original number
+    return number
   }
   return number
 }
