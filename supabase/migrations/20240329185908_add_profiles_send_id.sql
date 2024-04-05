5fb8d12a7ac06383e3afca2738925e8404afa0a0
Brandon Young <byoung0589@gmail.com>
2024-04-05 13:17:14 -0400

    Add trigger to prevent updating send_id and test for it

--- supabase/migrations/20240329185908_add_profiles_send_id.sql
+++ supabase/migrations/20240329185908_add_profiles_send_id.sql
@@ -24,3 +24,21 @@ CREATE SEQUENCE profiles_send_id_seq;
 SELECT setval('profiles_send_id_seq', coalesce(max(send_id), 0) + 1, false) FROM profiles;
 ALTER TABLE profiles ALTER COLUMN send_id SET DEFAULT nextval('profiles_send_id_seq');
 create index concurrently on profiles(send_id);
+
+
+CREATE OR REPLACE FUNCTION stop_change_send_id()
+  RETURNS trigger AS
+$BODY$
+BEGIN
+  IF OLD.send_id <> NEW.send_id THEN
+    RAISE EXCEPTION 'send_id cannot be changed';
+  END IF;
+  RETURN NEW;
+$BODY$
+
+
+CREATE TRIGGER avoid_send_id_change
+  BEFORE UPDATE
+  ON profiles
+  FOR EACH ROW
+  EXECUTE FUNCTION stop_change_send_id();
