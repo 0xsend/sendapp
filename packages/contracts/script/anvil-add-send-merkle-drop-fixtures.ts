640563597a218b30d00352377650350516ba9d43
Big Boss <bigboss@metalrodeo.xyz>
2024-04-07 11:42:03 -0500

    fix send merkle drop fixture and contracts tsconfig

--- packages/contracts/script/anvil-add-send-merkle-drop-fixtures.ts
+++ packages/contracts/script/anvil-add-send-merkle-drop-fixtures.ts
@@ -1,4 +1,5 @@
 import 'zx/globals'
+import { supabaseAdmin } from 'app/utils/supabase/admin'
 
 /**
  * This script is used to deploy the SendMerkleDrop contract and add a tranche to the airdrop. It should be adapted as things are deployed to mainnet.
@@ -9,13 +10,15 @@ const AIRDROP_MULTISIG_SAFE = '0x077c4E5983e5c495599C1Eb5c1511A52C538eB50'
 void (async function main() {
   console.log(chalk.blue('Enable auto-mining...'))
   await $`cast rpc --rpc-url http://localhost:8546 evm_setAutomine true`
-  console.log(chalk.blue('Deploying SendMerkleDrop contract...'))
-  await $`forge script ./script/DeploySendMerkleDrop.s.sol:DeploySendMerkleDropScript \
-              -vvvv \
-              --fork-url http://localhost:8546 \
-              --sender 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266 \
-              --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 \
-              --broadcast`
+
+  // merkle drop is deployed to base 🎉
+  // console.log(chalk.blue('Deploying SendMerkleDrop contract...'))
+  // await $`forge script ./script/DeploySendMerkleDrop.s.sol:DeploySendMerkleDropScript \
+  //             -vvvv \
+  //             --fork-url http://localhost:8546 \
+  //             --sender 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266 \
+  //             --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 \
+  //             --broadcast`
 
   console.log(chalk.blue('Sending 10 ether to the airdrop multisig...'))
   await $`cast send --rpc-url http://localhost:8546 \
@@ -32,7 +35,7 @@ void (async function main() {
   // parse merkle drop address from broadcast folder
   const broadcast = JSON.parse(
     await Bun.file(
-      `${import.meta.dir}/../broadcast/DeploySendMerkleDrop.s.sol/845337/run-latest.json`
+      `${import.meta.dir}/../broadcast/DeploySendMerkleDrop.s.sol/8453/run-latest.json`
     ).text()
   ) as {
     transactions: {
@@ -61,5 +64,24 @@ void (async function main() {
     $.env.ANVIL_BLOCK_TIME ?? '2'
   }` // mimics Tiltfile default
 
+  console.log(chalk.blue('Update all chain ids to use local...'))
+  console.log(chalk.blue('base mainnet -> base local'))
+  const { error } = await supabaseAdmin
+    .from('distributions')
+    .update({ chain_id: 845337 })
+    .filter('chain_id', 'eq', 8453)
+  if (error) {
+    console.log('error updating chain_id', error)
+    throw error
+  }
+  console.log(chalk.blue('eth mainnet -> eth local'))
+  const { error: error2 } = await supabaseAdmin
+    .from('distributions')
+    .update({ chain_id: 1337 })
+    .filter('chain_id', 'eq', 1)
+  if (error2) {
+    console.log('error updating chain_id', error2)
+    throw error2
+  }
   console.log(chalk.green('Done!'))
 })()
