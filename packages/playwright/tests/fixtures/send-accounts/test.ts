3d78cb345e006de3063f00492f4b60c425779ffe
Big Boss <bigboss@metalrodeo.xyz>
2024-04-11 21:56:26 -0500

    send usdc with no eth using paymaster

--- packages/playwright/tests/fixtures/send-accounts/test.ts
+++ packages/playwright/tests/fixtures/send-accounts/test.ts
@@ -6,15 +6,7 @@ import { assert } from 'app/utils/assert'
 import { parseEther, zeroAddress } from 'viem'
 import { debug } from 'debug'
 import { setERC20Balance } from 'app/utils/useSetErc20Balance'
-
-// @todo playwright is incompatible with esm modules only like wagmi, hardcode the USDC address
-const usdcAddress = {
-  1: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
-  1337: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
-  8453: '0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913',
-  84532: '0x036CbD53842c5426634e7929541eC2318f3dCF7e',
-  845337: '0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913',
-} as const
+import { usdcAddress } from '../viem'
 
 let log: debug.Debugger
 
--- packages/playwright/tests/send.onboarded.spec.ts
+++ packages/playwright/tests/send.onboarded.spec.ts
@@ -5,6 +5,8 @@ import { debug, type Debugger } from 'debug'
 import { assert } from 'app/utils/assert'
 import { userOnboarded } from '@my/snaplet/src/models'
 import { ProfilePage } from './fixtures/profiles'
+import { erc20Abi, formatUnits, zeroAddress } from 'viem'
+import { testBaseClient, usdcAddress } from './fixtures/viem'
 
 const test = mergeTests(sendAccountTest, snapletTest)
 
@@ -75,4 +77,69 @@ test('can send USDC to user on profile', async ({ page, seed }) => {
     timeout: 20000,
   })
   await expect(sendDialog.getByRole('button', { name: 'View on Otterscan' })).toBeVisible()
+})
+
+test('can send USDC to user on profile using paymaster', async ({
+  page,
+  seed,
+  supabase,
+  setEthBalance,
+}) => {
+  const { data: sendAccount, error } = await supabase.from('send_accounts').select('*').single()
+  if (error) {
+    log('error fetching send account', error)
+    throw error
+  }
+  assert(!!sendAccount, 'no send account found')
+  assert(sendAccount.address !== zeroAddress, 'send account address is zero')
+
+  await setEthBalance({ address: sendAccount.address, value: 0n }) // set balance to 0 ETH
+
+  const plan = await seed.users([userOnboarded])
+  const tag = plan.tags[0]
+  const profile = plan.profiles[0]
+  assert(!!tag?.name, 'tag not found')
+  assert(!!profile?.name, 'profile name not found')
+  assert(!!profile?.about, 'profile about not found')
+  const profilePage = new ProfilePage(page, {
+    name: profile.name,
+    about: profile.about,
+  })
+  await profilePage.visit(tag.name, expect)
+  await expect(profilePage.sendButton).toBeVisible()
+  await profilePage.sendButton.click()
+
+  // @todo create send form fixture
+  const sendDialog = page.getByTestId('sendDialogContainer')
+  await expect(sendDialog).toBeVisible()
+  const amountInput = sendDialog.getByLabel('Amount')
+  await expect(amountInput).toBeVisible()
+  await amountInput.fill('5')
+  const tokenSelect = sendDialog.getByRole('combobox') // @todo when tamagui supports this , { name: 'Token' })
+  await expect(tokenSelect).toBeVisible()
+  await tokenSelect.selectOption('USDC')
+  const sendDialogButton = sendDialog.getByRole('button', { name: 'Send' })
+  expect(sendDialogButton).toBeVisible()
+  const usdcBalBefore = await testBaseClient.readContract({
+    address: usdcAddress[testBaseClient.chain.id],
+    abi: erc20Abi,
+    functionName: 'balanceOf',
+    args: [sendAccount.address],
+  })
+  await sendDialogButton.click()
+  await expect(sendDialog.getByText(/Sent user op [0-9a-f]+/).first()).toBeVisible({
+    timeout: 20000,
+  })
+  await expect(sendDialog.getByRole('link', { name: 'View on Otterscan' })).toBeVisible()
+
+  const usdcBalAfter = await testBaseClient.readContract({
+    address: usdcAddress[testBaseClient.chain.id],
+    abi: erc20Abi,
+    functionName: 'balanceOf',
+    args: [sendAccount.address],
+  })
+  expect(Number(formatUnits(usdcBalBefore, 6)) - Number(formatUnits(usdcBalAfter, 6))).toBeCloseTo(
+    5,
+    0
+  ) // allow for ¢10 for gas
 })
