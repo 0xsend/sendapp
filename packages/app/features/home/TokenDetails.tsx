724ff3e55796b070b09742143b3b545d7b47ce3a
youngkidwarrior <victor@she.energy>
2024-04-18 01:49:45 -0700

    Use Market Data in TokenDetails

---
+++ packages/app/features/home/TokenDetails.tsx
@@ -0,0 +1,156 @@
+import {
+  BigHeading,
+  H1,
+  Label,
+  Paragraph,
+  Separator,
+  Spinner,
+  Stack,
+  XStack,
+  YStack,
+  useMedia,
+} from '@my/ui'
+import type { coins } from 'app/data/coins'
+import { type UseBalanceReturnType, useBalance } from 'wagmi'
+import { baseMainnet } from '@my/wagmi'
+import { useSendAccounts } from 'app/utils/send-accounts'
+
+import formatAmount from 'app/utils/formatAmount'
+import { useTokenMarketData } from 'app/utils/coin-gecko'
+import { ArrowDown, ArrowUp } from '@tamagui/lucide-icons'
+import { IconError } from 'app/components/icons'
+
+export const TokenDetails = ({ coin }: { coin: coins[number] }) => {
+  const media = useMedia()
+  const { data: sendAccounts } = useSendAccounts()
+  const sendAccount = sendAccounts?.[0]
+  const balance = useBalance({
+    address: sendAccount?.address,
+    token: coin.token === 'eth' ? undefined : coin.token,
+    query: { enabled: !!sendAccount },
+    chainId: baseMainnet.id,
+  })
+
+  return (
+    <YStack f={1}>
+      {media.gtLg && coin.label !== 'USDC' && (
+        <XStack w={'100%'} ai={'center'} jc={'space-between'} $gtLg={{ mt: '$9' }} mt={'$6'}>
+          <Separator $theme-dark={{ boc: '$decay' }} $theme-light={{ boc: '$gray4Light' }} />
+          <Stack
+            bw={1}
+            br={'$2'}
+            $theme-dark={{ boc: '$decay' }}
+            $theme-light={{ boc: '$gray4Light' }}
+            p={'$1.5'}
+            jc="center"
+            miw="$18"
+          >
+            <TokenDetailsMarketData coin={coin} />
+          </Stack>
+        </XStack>
+      )}
+      <YStack>
+        <Label fontSize={'$5'} fontWeight={'500'} color={'$color11'} textTransform={'uppercase'}>
+          {`${coin.label} BALANCE`}
+        </Label>
+        <TokenDetailsBalance balance={balance} />
+      </YStack>
+      <Stack w={'100%'} py={'$6'}>
+        <Separator $theme-dark={{ boc: '$decay' }} $theme-light={{ boc: '$gray4Light' }} />
+      </Stack>
+      <YStack>
+        <Label fontSize="$7" fontWeight="500" color={'$color11'} textTransform={'uppercase'}>
+          HISTORY
+        </Label>
+        <H1 fontSize="$9" fontWeight="700" color={'$color12'}>
+          Coming Soon
+        </H1>
+      </YStack>
+    </YStack>
+  )
+}
+
+export const TokenDetailsMarketData = ({ coin }: { coin: coins[number] }) => {
+  const { data: tokenMarketData, status } = useTokenMarketData(coin.coingeckoTokenId)
+
+  const price = tokenMarketData?.at(0)?.current_price
+
+  const changePercent24h = tokenMarketData?.at(0)?.price_change_percentage_24h
+
+  if (status === 'pending') return <Spinner size="small" />
+  if (status === 'error' || !price || !changePercent24h)
+    return (
+      <XStack gap="$2" ai="center" jc={'center'}>
+        <Paragraph>Failed to load market data</Paragraph>
+        <IconError size="$1.75" color={'$redVibrant'} />
+      </XStack>
+    )
+
+  const formatPriceChange = (change: number) => {
+    const fixedChange = change.toFixed(2)
+    if (change > 0)
+      return (
+        <>
+          <Paragraph fontSize="$4" fontWeight="500" color={'$olive'}>{`${fixedChange}%`}</Paragraph>
+          <ArrowUp col={'$olive'} size={'$0.9'} />
+        </>
+      )
+    if (change < 0)
+      return (
+        <>
+          <Paragraph
+            fontSize="$4"
+            fontWeight="500"
+            color={'$redVibrant'}
+          >{`${fixedChange}%`}</Paragraph>
+          <ArrowDown col={'$redVibrant'} size={'$0.9'} />
+        </>
+      )
+    return (
+      <>
+        <Paragraph
+          fontSize="$4"
+          fontWeight="500"
+          color={'$redVibrant'}
+        >{`${fixedChange}%`}</Paragraph>
+      </>
+    )
+  }
+
+  return (
+    <XStack gap="$2" ai="center" jc={'space-around'}>
+      <Paragraph
+        fontSize="$4"
+        fontWeight="500"
+        $theme-dark={{ color: '$gray8Light' }}
+        color={'$color12'}
+      >
+        {`1 ${coin.symbol} = ${price} USD`}
+      </Paragraph>
+      <XStack gap={'$1.5'} ai="center" jc={'space-around'}>
+        {formatPriceChange(changePercent24h)}
+      </XStack>
+    </XStack>
+  )
+}
+
+const TokenDetailsBalance = ({ balance }: { balance: UseBalanceReturnType }) => {
+  if (balance) {
+    if (balance.isError) {
+      return <>---</>
+    }
+    if (balance.isPending) {
+      return <Spinner size={'small'} />
+    }
+    if (balance?.data?.value === undefined) {
+      return <></>
+    }
+    return (
+      <BigHeading color={'$color12'}>
+        {formatAmount(
+          (Number(balance.data.value) / 10 ** (balance.data?.decimals ?? 0)).toString()
+        )}
+      </BigHeading>
+    )
+  }
+}
--- packages/app/features/home/screen.tsx
+++ packages/app/features/home/screen.tsx
@@ -8,28 +8,22 @@ import {
   useMedia,
   useToastController,
   Stack,
-  Label,
-  H1,
-  Spinner,
-  BigHeading,
 } from '@my/ui'
 import { IconDeposit, IconPlus } from 'app/components/icons'
 import { TokenBalanceList } from './TokenBalanceList'
 import { coins } from 'app/data/coins'
 import { TokenBalanceCard } from './TokenBalanceCard'
-import { type UseBalanceReturnType, useAccount, useBalance } from 'wagmi'
+import { useAccount } from 'wagmi'
 import { useToken } from 'app/routers/params'
 import { OpenConnectModalWrapper } from 'app/utils/OpenConnectModalWrapper'
 import { useThemeSetting } from '@tamagui/next-theme'
-import { useTokenPrice } from '../../utils/coin-gecko'
-import { useSendAccounts } from 'app/utils/send-accounts'
-import { baseMainnet } from '@my/wagmi'
-import formatAmount from 'app/utils/formatAmount'
+import { X } from '@tamagui/lucide-icons'
+import { TokenDetails } from './TokenDetails'
 
 export function HomeScreen() {
   const media = useMedia()
   const toast = useToastController()
-  const [tokenParam] = useToken()
+  const [tokenParam, setTokenParam] = useToken()
   const { address } = useAccount()
   const { resolvedTheme } = useThemeSetting()
   const separatorColor = resolvedTheme?.startsWith('dark') ? '#343434' : '#E6E6E6'
@@ -38,6 +32,35 @@ export function HomeScreen() {
 
   return (
     <Container fd={'column'} $gtMd={{ pt: '$6' }}>
+      {selectedCoin !== undefined && media.gtLg && (
+        <Stack>
+          <Button
+            top={'$-8'}
+            right={0}
+            position="absolute"
+            bc="transparent"
+            chromeless
+            circular
+            jc={'center'}
+            ai={'center'}
+            $lg={{ display: 'none' }}
+            hoverStyle={{
+              backgroundColor: 'transparent',
+              borderColor: '$color11',
+            }}
+            pressStyle={{
+              backgroundColor: 'transparent',
+            }}
+            focusStyle={{
+              backgroundColor: 'transparent',
+            }}
+            icon={<X size={'$2.5'} color={'$color11'} />}
+            onPress={() => {
+              setTokenParam(undefined)
+            }}
+          />
+        </Stack>
+      )}
       <XStack w={'100%'} jc={'space-between'} $gtLg={{ gap: '$11' }} f={1}>
         {(selectedCoin === undefined || media.gtLg) && (
           <YStack $gtLg={{ width: 360 }} width="100%" ai={'center'}>
@@ -98,120 +121,4 @@ export function HomeScreen() {
       </XStack>
     </Container>
   )
-}
-
-const TokenDetails = ({ coin }: { coin: coins[number] }) => {
-  const { data: sendAccounts } = useSendAccounts()
-  const sendAccount = sendAccounts?.[0]
-  const balance = useBalance({
-    address: sendAccount?.address,
-    token: coin.token === 'eth' ? undefined : coin.token,
-    query: { enabled: !!sendAccount },
-    chainId: baseMainnet.id,
-  })
-
-  return (
-    <YStack f={1}>
-      {/* This goes in TopNav
-      <Stack jc="flex-end" ai="flex-end">
-        <Button
-          bc="transparent"
-          chromeless
-          circular
-          jc={'center'}
-          ai={'center'}
-          hoverStyle={{
-            backgroundColor: 'transparent',
-            borderColor: '$color11',
-          }}
-          pressStyle={{
-            backgroundColor: 'transparent',
-          }}
-          focusStyle={{
-            backgroundColor: 'transparent',
-          }}
-          icon={<X size={'$2.5'} color={'$color11'} />}
-          onPress={() => {
-            setTokenParam(undefined)
-          }}
-        />
-      </Stack> */}
-      {coin.label !== 'USDC' && (
-        <XStack w={'100%'} ai={'center'} jc={'space-between'} mt={'$6'}>
-          <Separator $theme-dark={{ boc: '$decay' }} $theme-light={{ boc: '$gray4Light' }} />
-          <Stack
-            bw={1}
-            br={'$2'}
-            $theme-dark={{ boc: '$decay' }}
-            $theme-light={{ boc: '$gray4Light' }}
-            p={'$2'}
-          >
-            <TokenDetailsPrice coin={coin} />
-          </Stack>
-        </XStack>
-      )}
-      <YStack>
-        <Label fontSize={'$5'} fontWeight={'500'} color={'$color11'}>
-          {`${coin.label} Balance`}
-        </Label>
-        <TokenDetailsBalance balance={balance} />
-      </YStack>
-      <Stack w={'100%'} py={'$6'}>
-        <Separator $theme-dark={{ boc: '$decay' }} $theme-light={{ boc: '$gray4Light' }} />
-      </Stack>
-
-      <YStack>
-        <Label fontSize="$7" fontWeight="500" color={'$color11'}>
-          History
-        </Label>
-        <H1 fontSize="$9" fontWeight="700" color={'$color12'}>
-          Coming Soon
-        </H1>
-      </YStack>
-    </YStack>
-  )
-}
-
-const TokenDetailsPrice = ({ coin }: { coin: coins[number] }) => {
-  const { data: tokenPriceData, status } = useTokenPrice(coin.coingeckoTokenId)
-
-  const maybePrice = tokenPriceData?.[coin.coingeckoTokenId]?.usd
-
-  const tokenPrice = () => {
-    switch (status) {
-      case 'pending':
-        return <Spinner size="small" />
-      case 'error' || !maybePrice:
-        return <Paragraph>?</Paragraph>
-      default:
-        return maybePrice
-    }
-  }
-
-  return (
-    <Paragraph fontSize="$5" fontWeight="500" color={'$color11'}>
-      {`1 ${coin.label} = ${tokenPrice()} USD`}
-    </Paragraph>
-  )
-}
-
-const TokenDetailsBalance = ({ balance }: { balance: UseBalanceReturnType }) => {
-  if (balance) {
-    if (balance.isError) {
-      return <>---</>
-    }
-    if (balance.isPending) {
-      return <Spinner size={'small'} />
-    }
-    if (balance?.data?.value === undefined) {
-      return <></>
-    }
-    return (
-      <BigHeading color={'$color12'}>
-        {formatAmount(
-          (Number(balance.data.value) / 10 ** (balance.data?.decimals ?? 0)).toString()
-        )}
-      </BigHeading>
-    )
-  }
 }
