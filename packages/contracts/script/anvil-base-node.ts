acaaa900707584be5210f4d9e91994d486281318
Big Boss <bigboss@metalrodeo.xyz>
2024-04-28 11:39:21 -0500

    small fixes (#368)
    * hide secrets when starting anvil base node
    
    * ui: fix error tooltip position on text field
    
    * supabase: increase dbdev install timeouts
    
    * add reset-supabase script

--- packages/contracts/script/anvil-base-node.ts
+++ packages/contracts/script/anvil-base-node.ts
@@ -1,20 +1,21 @@
 import 'zx/globals'
 
-const baseForkUrl = $.env.ANVIL_BASE_FORK_URL ?? 'https://base-pokt.nodies.app'
-const baseBlockTime = $.env.ANVIL_BASE_BLOCK_TIME ?? '2'
-const baseExtraArgs = $.env.ANVIL_BASE_EXTRA_ARGS ?? '--silent'
-const baseBaseFee = await $`cast base-fee --rpc-url ${baseForkUrl}`
-const baseGasPrice = await $`cast gas-price --rpc-url ${baseForkUrl}`
+$.env.ANVIL_BASE_FORK_URL ||= 'https://base-pokt.nodies.app'
+$.env.ANVIL_BASE_BLOCK_TIME ||= '2'
+$.env.ANVIL_BASE_EXTRA_ARGS ||= '--silent'
+
+const baseBaseFee = await $`cast base-fee --rpc-url $ANVIL_BASE_FORK_URL`
+const baseGasPrice = await $`cast gas-price --rpc-url $ANVIL_BASE_FORK_URL`
 
 await $`anvil \
     --host=0.0.0.0 \
     --port=8546 \
     --chain-id=845337 \
-    --fork-url=${baseForkUrl} \
-    --block-time=${baseBlockTime} \
+    --fork-url=$ANVIL_BASE_FORK_URL \
+    --block-time=$ANVIL_BASE_BLOCK_TIME  \
     --base-fee=${baseBaseFee} \
     --gas-price=${baseGasPrice} \
     --no-storage-caching \
     --prune-history \
-    ${baseExtraArgs}
+    $ANVIL_BASE_EXTRA_ARGS
 `
