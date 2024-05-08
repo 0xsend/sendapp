346477c6c923be7ed383bb7da1157134c9d23d72
youngkidwarrior <victor@she.energy>
2024-05-08 01:10:20 -0700

    Increase Sidebar Link text size

--- packages/app/components/sidebar/HomeSideBar.tsx
+++ packages/app/components/sidebar/HomeSideBar.tsx
@@ -33,28 +33,28 @@ import { ReferralLink } from '../ReferralLink'
 const links = [
   {
     icon: <IconHome size={'$1.75'} />,
-    text: 'home',
+    text: 'Home',
     href: '/',
   },
   // {
   //   icon: <IconSLogo size={'$1'} />,
-  //   text: 'send',
+  //   text: 'Send',
   //   href: '/send',
   // },
   // {
   //   icon: <IconActivity size={'$1'} />,
-  //   text: 'activity',
+  //   text: 'Activity',
   //   href: '/activity',
   // },
   {
     icon: <IconAccount size={'$1'} />,
-    text: 'account',
+    text: 'Account',
     href: '/account',
   },
   __DEV__ || baseMainnet.id === 84532
     ? {
         icon: <Paragraph px="$1">🔒</Paragraph>,
-        text: 'secret shop',
+        text: 'Secret shop',
         href: '/secret-shop',
       }
     : undefined,
@@ -63,11 +63,11 @@ const links = [
 const HomeSideBar = ({ ...props }: YStackProps) => {
   return (
     <SideBar {...props}>
-      <Nav display="flex" flex={1}>
-        <Link href={'/'} display="flex" pl={'$4.5'}>
+      <Nav display="flex" w="100%" pl={'$7'}>
+        <Link href={'/'} display="flex">
           <IconSendLogo size={'$2.5'} color={'$color12'} />
         </Link>
-        <YStack gap={'$4'} pt={'$10'} alignItems="stretch" w={'100%'} f={1}>
+        <YStack gap={'$3.5'} pt={'$10'}>
           {links.map((link) => (
             <SideBarNavLink key={link.href} {...link} />
           ))}
@@ -104,6 +104,7 @@ const HomeBottomSheet = () => {
               alignItems="stretch"
               justifyContent="center"
               f={1}
+              pl={'$5'}
               h={'$7'}
             >
               <SideBarNavLink key={link.href} {...link} />
@@ -134,8 +135,8 @@ export const HomeSideBarWrapper = ({ children }: { children?: React.ReactNode })
           <HomeSideBar
             $theme-dark={{ backgroundColor: '$charcoal' }}
             $theme-light={{ backgroundColor: '$gray3Light' }}
-            width={208}
-            minWidth={208}
+            width={234}
+            minWidth={234}
             btlr={0}
             bblr={0}
             pt={80}
