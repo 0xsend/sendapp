5dd37e5071a287187ded4e7e4526b09b47657d9f
Tawnee <tawnee.a@proton.me>
2024-05-16 13:24:06 -0600

    card styles

--- packages/app/features/account/settings/backup/index.tsx
+++ packages/app/features/account/settings/backup/index.tsx
@@ -1,7 +1,23 @@
-import { Button, H1, H4, H5, Link, Paragraph, Separator, Spinner, XStack, YStack } from '@my/ui'
+import { useState } from 'react'
+import {
+  Button,
+  H1,
+  H4,
+  H5,
+  Input,
+  Link,
+  Paragraph,
+  Separator,
+  Spinner,
+  Text,
+  XStack,
+  YStack,
+  isWeb,
+} from '@my/ui'
 import { baseMainnetClient, useReadSendAccountGetActiveSigningKeys } from '@my/wagmi'
-import { IconNote } from 'app/components/icons'
+import { IconDots, IconNote, IconX } from 'app/components/icons'
 import { assert } from 'app/utils/assert'
+import { formatTimeDate } from 'app/utils/formatTimeDate'
 import { COSEECDHAtoXY } from 'app/utils/passkeys'
 import { pgBase16ToBytes } from 'app/utils/pgBase16ToBytes'
 import { useSendAccount } from 'app/utils/send-accounts/useSendAccounts'
@@ -85,14 +101,15 @@ const WebauthnCreds = ({
 
       <Separator w={'100%'} borderColor="$decay" />
 
-      <YStack w={'100%'} gap={'$6'} pb={'$6'}>
-        {sendAcct.send_account_credentials.map((cred, idx) => (
-          <YStack key={`${sendAcct.id}-${cred.key_slot}`} w={'100%'} gap={'$2'}>
-            <SendAccountCredentials address={sendAcct.address} cred={cred} />
-            {idx !== sendAcct.send_account_credentials.length - 1 && <Separator w={'100%'} />}
-          </YStack>
+      <XStack gap="$5" flexWrap="wrap" ai="flex-start">
+        {sendAcct.send_account_credentials.map((cred) => (
+          <SendAccountCredentials
+            key={`${sendAcct.id}-${cred.key_slot}`}
+            address={sendAcct.address}
+            cred={cred}
+          />
         ))}
-      </YStack>
+      </XStack>
     </YStack>
   )
 }
@@ -104,6 +121,7 @@ const SendAccountCredentials = ({
   address: `0x${string}`
   cred: NonNullable<ReturnType<typeof useSendAccount>['data']>['send_account_credentials'][number]
 }) => {
+  const [cardStatus, setCardStatus] = useState<'default' | 'settings' | 'remove'>('default')
   const webauthnCred = cred.webauthn_credentials
   assert(!!webauthnCred, 'webauthnCred not found')
   const {
@@ -126,55 +144,200 @@ const SendAccountCredentials = ({
   })
   assert(!!webauthnCred, 'webauthnCred not found')
   return (
-    <YStack w={'100%'} gap={'$3'} mt={'$2'} mb={'$2'}>
-      <H4 fontWeight={'300'} color={'$color05'}>
-        {webauthnCred.display_name}
-      </H4>
-      <H5 fontWeight={'300'} color={'$color05'}>
-        Created At
-      </H5>
-      <Paragraph fontWeight={'300'} color={'$color05'} fontFamily={'$mono'}>
-        {cred.created_at}
-      </Paragraph>
-      <H5 fontWeight={'300'} color={'$color05'}>
-        Key Slot
-      </H5>
-      <Paragraph fontWeight={'300'} color={'$color05'} fontFamily={'$mono'}>
-        {cred.key_slot}
-      </Paragraph>
+    <YStack
+      w={'100%'}
+      gap={'$size.1.5'}
+      p={'$size.1.5'}
+      $theme-dark={{
+        backgroundColor: '$darkest',
+      }}
+      borderRadius={'$5'}
+      $gtLg={{
+        width: isWeb ? 'calc((100% - 24px) / 2)' : '100%',
+      }}
+      $gtXl={{
+        width: isWeb ? 'calc((100% - 48px) / 3)' : '100%',
+      }}
+    >
+      <XStack jc="space-between" ai="center">
+        <H4 fontWeight={'700'} color={cardStatus === 'remove' ? '$error' : '$primary'}>
+          {cardStatus === 'remove' ? 'Remove Passkey?' : webauthnCred.display_name}
+        </H4>
+        {cardStatus !== 'remove' && (
+          <Button
+            chromeless
+            px={'0'}
+            borderRadius={'$1'}
+            height={'$size.1.5'}
+            width={'$size.1.5'}
+            onPress={() => setCardStatus(cardStatus === 'default' ? 'settings' : 'default')}
+          >
+            {cardStatus === 'default' ? <IconDots color={'$primary'} /> : <IconX />}
+          </Button>
+        )}
+      </XStack>
+
       {(() => {
-        switch (true) {
-          case isLoadingActiveSigningKeys:
-            return <Spinner size="small" />
-          case activeSigningKeysError !== null:
+        switch (cardStatus) {
+          case 'settings':
             return (
-              <Paragraph maxWidth={'600'} fontFamily={'$mono'} fontSize={'$5'} color={'$color12'}>
-                {activeSigningKeysError?.message ??
-                  `Something went wrong: ${activeSigningKeysError}`}
-              </Paragraph>
+              <Button
+                borderColor={'$error'}
+                color={'$error'}
+                variant="outlined"
+                mt={'$size.0.9'}
+                hoverStyle={{ borderColor: '$error' }}
+                onPress={() => setCardStatus('remove')}
+              >
+                REMOVE PASSKEY
+              </Button>
             )
-          case !isActive:
+          case 'remove':
             return (
-              <>
-                <Paragraph fontWeight={'300'} color={'$yellowVibrant'} fontFamily={'$mono'}>
-                  Passkey is not confirmed onchain. Finish confirming the passkey onchain.
-                </Paragraph>
-                <Button theme="warning" {...link}>
-                  Confirm
-                </Button>
-              </>
-            )
-          case onchainSlot !== cred.key_slot:
-            return (
-              <Paragraph fontWeight={'300'} color={'$yellowVibrant'} fontFamily={'$mono'}>
-                Onchain Slot: {onchainSlot} does not match Webauthn Slot: {cred.key_slot}. This
-                should never happen.
-              </Paragraph>
+              <RemovePasskeyConfirmation
+                name={webauthnCred.display_name}
+                onCancel={() => setCardStatus('default')}
+                onRemove={() => {
+                  throw new Error('`onRemove` has not been implemented')
+                }}
+              />
             )
           default:
-            return <></>
+            return (
+              <>
+                {cred.created_at && (
+                  <CardTextBlock label="Created At" text={formatTimeDate(cred.created_at)} />
+                )}
+
+                <CardTextBlock label="Key Slot" text={cred.key_slot.toString().padStart(2, '0')} />
+
+                {(() => {
+                  switch (true) {
+                    case isLoadingActiveSigningKeys:
+                      return <Spinner size="small" />
+                    case activeSigningKeysError !== null:
+                      return (
+                        <Paragraph
+                          maxWidth={'600'}
+                          fontFamily={'$mono'}
+                          fontSize={'$5'}
+                          color={'$color12'}
+                        >
+                          {activeSigningKeysError?.message ??
+                            `Something went wrong: ${
+                              activeSigningKeysError?.message.split('.').at(0) ??
+                              'Something went wrong'
+                            }`}
+                        </Paragraph>
+                      )
+                    case !isActive:
+                      return (
+                        <YStack gap={'$size.1.5'} ai="flex-start">
+                          <CardTextBlock
+                            label="Status"
+                            text="Passkey is not confirmed onchain. Finish confirming the passkey onchain."
+                            warningText
+                          />
+                          <Button theme={'accent'} color="$primary" variant="outlined" {...link}>
+                            CONFIRM
+                          </Button>
+                        </YStack>
+                      )
+                    case onchainSlot !== cred.key_slot:
+                      return (
+                        <Paragraph fontWeight={'300'} color={'$yellowVibrant'} fontFamily={'$mono'}>
+                          Onchain Slot: {onchainSlot} does not match Webauthn Slot: {cred.key_slot}.
+                          This should never happen.
+                        </Paragraph>
+                      )
+                    default:
+                      return <></>
+                  }
+                })()}
+              </>
+            )
         }
       })()}
+    </YStack>
+  )
+}
+
+const CardTextBlock = ({
+  label,
+  text,
+  warningText = false,
+}: { label: string; text: string; warningText?: boolean }) => {
+  return (
+    <YStack gap={'$size.0.5'}>
+      <H5 size={'$5'} color={'$lightGrayTextField'} fontWeight={'400'}>
+        {label}
+      </H5>
+      <Paragraph fontWeight={'300'} color={warningText ? '$error' : '$white'} fontFamily={'$mono'}>
+        {text}
+      </Paragraph>
+    </YStack>
+  )
+}
+
+const RemovePasskeyConfirmation = ({
+  name,
+  onCancel,
+  onRemove,
+}: {
+  name: string
+  onCancel: () => void
+  onRemove: () => void
+}) => {
+  const [inputVal, setInputVal] = useState('')
+
+  return (
+    <YStack gap={'$size.3.5'}>
+      <Text fontWeight={'400'} fontSize={'$5'} color={'$white'} fontFamily={'$mono'}>
+        Removing &quot;{name}&quot; as a signer on your Send account.{' '}
+        <Text fontWeight={'400'} fontSize={'$5'} color={'$warning'} fontFamily={'$mono'}>
+          This cannot be undone.
+        </Text>
+      </Text>
+
+      <YStack gap={'$size.1.5'}>
+        <Text fontWeight={'400'} fontSize={'$5'} color={'$white'} fontFamily={'$mono'}>
+          Please enter &quot;{name}&quot; below
+        </Text>
+
+        <Input
+          boc={'transparent'}
+          borderRadius={'$4'}
+          color={'$white'}
+          ff={'$mono'}
+          autoFocus
+          onChangeText={setInputVal}
+        />
+
+        <XStack gap="$size.1" jc="space-between" w={'100%'} pt={'$size.0.9'}>
+          <Button
+            borderColor={'$primary'}
+            color={'$primary'}
+            variant="outlined"
+            flex={1}
+            hoverStyle={{ borderColor: '$primary' }}
+            onPress={onCancel}
+          >
+            CANCEL
+          </Button>
+          <Button
+            borderColor={'$error'}
+            color={'$error'}
+            variant="outlined"
+            flex={1}
+            hoverStyle={{ borderColor: '$error' }}
+            onPress={onRemove}
+            disabledStyle={{ opacity: 0.5 }}
+            disabled={name !== inputVal}
+          >
+            REMOVE
+          </Button>
+        </XStack>
+      </YStack>
     </YStack>
   )
 }
