074c2920efb98c06468b2105384e3d8a60a5eaad
Big Boss <bigboss@metalrodeo.xyz>
2024-05-17 15:58:17 -0500

    sync snaplet

--- .snaplet/snaplet.d.ts
+++ .snaplet/snaplet.d.ts
@@ -253,28 +253,6 @@ interface Table_auth_refresh_tokens {
   parent: string | null;
   session_id: string | null;
 }
-interface Table_storage_s_3_multipart_uploads {
-  id: string;
-  in_progress_size: number;
-  upload_signature: string;
-  bucket_id: string;
-  key: string;
-  version: string;
-  owner_id: string | null;
-  created_at: string;
-}
-interface Table_storage_s_3_multipart_uploads_parts {
-  id: string;
-  upload_id: string;
-  size: number;
-  part_number: number;
-  bucket_id: string;
-  key: string;
-  etag: string;
-  owner_id: string | null;
-  version: string;
-  created_at: string;
-}
 interface Table_auth_saml_providers {
   id: string;
   sso_provider_id: string;
@@ -613,8 +591,6 @@ interface Schema_storage {
   buckets: Table_storage_buckets;
   migrations: Table_storage_migrations;
   objects: Table_storage_objects;
-  s3_multipart_uploads: Table_storage_s_3_multipart_uploads;
-  s3_multipart_uploads_parts: Table_storage_s_3_multipart_uploads_parts;
 }
 interface Schema_supabase_functions {
   hooks: Table_supabase_functions_hooks;
@@ -661,8 +637,6 @@ interface Tables_relationships {
     };
     children: {
        objects_bucketId_fkey: "storage.objects";
-       s3_multipart_uploads_bucket_id_fkey: "storage.s3_multipart_uploads";
-       s3_multipart_uploads_parts_bucket_id_fkey: "storage.s3_multipart_uploads_parts";
     };
   };
   "public.chain_addresses": {
@@ -796,23 +770,6 @@ interface Tables_relationships {
   "auth.refresh_tokens": {
     parent: {
        refresh_tokens_session_id_fkey: "auth.sessions";
-    };
-    children: {
-
-    };
-  };
-  "storage.s3_multipart_uploads": {
-    parent: {
-       s3_multipart_uploads_bucket_id_fkey: "storage.buckets";
-    };
-    children: {
-       s3_multipart_uploads_parts_upload_id_fkey: "storage.s3_multipart_uploads_parts";
-    };
-  };
-  "storage.s3_multipart_uploads_parts": {
-    parent: {
-       s3_multipart_uploads_parts_bucket_id_fkey: "storage.buckets";
-       s3_multipart_uploads_parts_upload_id_fkey: "storage.s3_multipart_uploads";
     };
     children: {
 
