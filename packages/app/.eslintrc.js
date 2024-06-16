69ae0e187c90af06b52ed539f90ffdf272808885
Big Boss <bigboss@metalrodeo.xyz>
2024-06-16 13:19:22 -0500

    fix more type stuff

--- packages/app/.eslintrc.js
+++ packages/app/.eslintrc.js
@@ -1,7 +1,7 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   extends: ['custom', 'plugin:@tanstack/eslint-plugin-query/recommended'],
-  ignorePatterns: ['.eslintrc.js'],
+  ignorePatterns: ['.eslintrc.js', 'coverage/**'],
   parserOptions: {
     project: './tsconfig.json',
     tsconfigRootDir: __dirname,
