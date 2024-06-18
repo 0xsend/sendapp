f2c3363f6bdbf90a3066212487581aa320be98b1
Nicky <nickhigham.jobs@proton.me>
2024-06-18 19:23:53 +0100

    refac: add auto-mining to send-check fixture

--- packages/contracts/script/anvil-add-send-check-fixtures.ts
+++ packages/contracts/script/anvil-add-send-check-fixtures.ts
@@ -2,6 +2,9 @@ import 'zx/globals'
 
 const RPC_URL = 'http://localhost:8546'
 void (async function main() {
+  console.log(chalk.blue('Enable auto-mining...'))
+  await $`cast rpc --rpc-url ${RPC_URL} evm_setAutomine true`
+
   console.log(chalk.blue('Deploying SendCheck contract...'))
   await $`forge script ./script/DeploySendCheck.s.sol:DeploySendCheckScript \
               -vvvv \
