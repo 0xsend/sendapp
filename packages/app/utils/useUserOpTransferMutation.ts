39959aef5957de71d3453598a2db38acfd4384bd
Big Boss <bigboss@metalrodeo.xyz>
2024-06-29 11:12:06 -0500

    increase default paymaster post op gas limit (#506)
    in case of needing to update cached price, the post op uses around 95K gas.

--- packages/app/utils/useUserOpTransferMutation.ts
+++ packages/app/utils/useUserOpTransferMutation.ts
@@ -20,8 +20,15 @@ import { assert } from './assert'
 import { byteaToBase64 } from './byteaToBase64'
 import { signUserOp } from './userop'
 
-// default user op with preset gas values that work
-// will probably need to move this to the database
+/**
+ * default user op with preset gas values that work will probably need to move this to the database.
+ * Paymaster post-op gas limit could be set dynamically based on the status of the paymaster if the price cache is
+ * outdated, otherwise, a lower post op gas limit around only 50K is needed. In case of needing to update cached price,
+ * the post op uses around 75K gas.
+ *
+ * - [example no update price](https://www.tdly.co/shared/simulation/a0122fae-a88c-47cd-901c-02de87901b45)
+ * - [Failed due to OOG](https://www.tdly.co/shared/simulation/c259922c-8248-4b43-b340-6ebbfc69bcea)
+ */
 export const defaultUserOp: Pick<
   UserOperation<'v0.7'>,
   | 'callGasLimit'
@@ -38,7 +45,7 @@ export const defaultUserOp: Pick<
   maxFeePerGas: 10000000n,
   maxPriorityFeePerGas: 10000000n,
   paymasterVerificationGasLimit: 150000n,
-  paymasterPostOpGasLimit: 75000n,
+  paymasterPostOpGasLimit: 100000n,
 }
 
 export type UseUserOpTransferMutationArgs = {
