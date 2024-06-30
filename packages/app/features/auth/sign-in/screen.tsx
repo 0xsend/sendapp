b0fb8fe3c771c1797009d680d8b7dd7b54bcb183
youngkidwarrior <victor@she.energy>
2024-06-30 00:46:07 -0700

    Implement redirect uri on auth screens

--- packages/app/features/auth/sign-in/screen.tsx
+++ packages/app/features/auth/sign-in/screen.tsx
@@ -1,15 +1,21 @@
 import { Anchor, Paragraph, XStack, YStack } from '@my/ui'
 import { SignInForm } from 'app/features/auth/sign-in/sign-in-form'
+import { useAuthScreenParams } from 'app/routers/params'
 import { useLink } from 'solito/link'
 
 export const SignInScreen = () => {
+  const [queryParams] = useAuthScreenParams()
+  const { redirectUri } = queryParams
   return (
     <YStack h="100%" jc="center" ai="center">
       <SignInForm />
       <XStack jc="center" ai="center" mt="$4">
         <Paragraph size="$2" color="$color11">
           Don&apos;t have an account?{' '}
-          <Anchor color="$color12" {...useLink({ href: '/auth/sign-up' })}>
+          <Anchor
+            color="$color12"
+            {...useLink({ href: `/auth/sign-up?redirectUri=${redirectUri}` })}
+          >
             Sign up
           </Anchor>
         </Paragraph>
