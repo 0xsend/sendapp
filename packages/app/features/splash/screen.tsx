e9a6c68b70c1ece6f4860ae0ad80d189c1d67e31
Big Boss <bigboss@metalrodeo.xyz>
2024-07-04 11:06:18 -0500

    clean up splash page

--- packages/app/features/splash/screen.tsx
+++ packages/app/features/splash/screen.tsx
@@ -1,24 +1,24 @@
 import {
-  YStack,
-  XStack,
-  Button,
   Anchor,
-  useMedia,
-  Stack,
+  Button,
   LinearGradient,
+  Stack,
+  XStack,
+  YStack,
+  isWeb,
+  useMedia,
   type ButtonProps,
 } from '@my/ui'
 import { IconSendLogo } from 'app/components/icons'
-import { AnimationLayout } from '../../components/layout/animation-layout'
-import { SolitoImage } from 'solito/image'
 import { useAuthCarouselContext } from 'app/features/auth/AuthCarouselContext'
-import { Carousel } from 'app/features/auth/components/Carousel'
+import { Carousel, carouselImagePositions } from 'app/features/auth/components/Carousel'
+import { SolitoImage } from 'solito/image'
 import { useLink } from 'solito/link'
-import { isWeb } from '@my/ui'
+import { AnimationLayout } from '../../components/layout/animation-layout'
 
 export function SplashScreen() {
   return (
-    <YStack f={1}>
+    <YStack h={isWeb ? '100svh' : '100%'} bc={'$black'}>
       {/* Top section with carousel */}
       <Hero />
 
@@ -67,88 +67,91 @@ function Hero() {
   const media = useMedia()
   const { carouselImages, carouselProgress } = useAuthCarouselContext()
   const carouselImage = carouselImages[carouselProgress]
+  const mobileImagePosition = carouselImagePositions[carouselProgress]
 
   return (
-    <YStack h={isWeb ? '100vh' : '100%'} minHeight={'$22'}>
-      <YStack h={isWeb ? '100vh' : '100%'} minHeight={'$22'} overflow="hidden">
-        {carouselImage && (
+    <YStack h={isWeb ? '100svh' : '100%'} bc="$black">
+      {carouselImage && (
+        <Stack
+          pos="absolute"
+          top={0}
+          left={0}
+          right={0}
+          bottom={0}
+          height={isWeb ? '100dvh' : '100%'}
+        >
           <Stack
+            bc="$black"
             pos="absolute"
             top={0}
             left={0}
             right={0}
             bottom={0}
-            height={isWeb ? '100vh' : '100%'}
-            overflow="hidden"
+            height={isWeb ? '100dvh' : '100%'}
+          />
+          <AnimationLayout
+            currentKey={carouselImage.base64 || 'none'}
+            direction={1}
+            fullscreen={true}
           >
             <Stack
-              bc="$black"
               pos="absolute"
               top={0}
               left={0}
               right={0}
               bottom={0}
-              height={isWeb ? '100vh' : '100%'}
-            />
-            <AnimationLayout
-              currentKey={carouselImage.base64 || 'none'}
-              direction={1}
-              fullscreen={true}
+              height={isWeb ? '100dvh' : '100%'}
             >
-              <Stack
+              <SolitoImage
+                placeholder="blur"
+                blurDataURL={carouselImage.base64}
+                src={carouselImage.img.src}
+                fill={true}
+                contentPosition={media.gtMd ? undefined : mobileImagePosition}
+                style={{ objectFit: 'cover' }}
+                alt="splash-screen-carousel"
+              />
+              <LinearGradient
                 pos="absolute"
-                top={0}
-                left={0}
-                right={0}
-                bottom={0}
-                height={isWeb ? '100vh' : '100%'}
-              >
-                <SolitoImage
-                  placeholder="blur"
-                  blurDataURL={carouselImage.base64}
-                  src={carouselImage.img.src}
-                  fill={true}
-                  style={{ objectFit: 'cover' }}
-                  alt="splash-screen-carousel"
-                />
-                <LinearGradient
-                  pos="absolute"
-                  w="100%"
-                  h="100%"
-                  locations={media.gtMd ? [0.5, 1] : [0, 0.5, 1]}
-                  colors={
-                    media.gtMd ? ['transparent', 'black'] : ['$black', 'transparent', '$black']
-                  }
-                />
-                <LinearGradient
-                  pos="absolute"
-                  w="100%"
-                  h="100%"
-                  locations={[0, 0.5, 1]}
-                  colors={['$black', 'transparent', '$black']}
-                />
-              </Stack>
-            </AnimationLayout>
-          </Stack>
-        )}
+                w="100%"
+                h="100%"
+                locations={media.gtMd ? [0.5, 1] : [0, 0.5, 1]}
+                colors={media.gtMd ? ['transparent', 'black'] : ['$black', 'transparent', '$black']}
+              />
+              <LinearGradient
+                pos="absolute"
+                w="100%"
+                h="100%"
+                locations={[0, 0.5, 1]}
+                colors={['$black', 'transparent', '$black']}
+              />
+            </Stack>
+          </AnimationLayout>
+        </Stack>
+      )}
 
-        <YStack f={1} jc="space-between" ai="center" p="$4" zi={1}>
-          <XStack ai="center" space="$4" w="100%" mt="$4">
-            <IconSendLogo size="$4" mx="auto" />
-            <SignInButton position="absolute" right={'$6'} display={media.gtMd ? 'flex' : 'none'} />
-          </XStack>
+      <YStack f={1} jc="space-between" ai="center" px="$4" pt={'$4'} pb="$6">
+        <XStack ai="center" gap="$4" w="100%" mt="$4">
+          <IconSendLogo size="$4" mx="auto" color="$white" />
+          <SignInButton position="absolute" right={'$6'} display={media.gtMd ? 'flex' : 'none'} />
+        </XStack>
 
-          <YStack w="100%">
-            <YStack jc="flex-end" f={1} gap="$2" $gtMd={{ pb: '$8' }} ml="auto" w="100%" maw={738}>
-              <Carousel currentKey={carouselProgress.toString()} fullscreen={false} />
-            </YStack>
-            <XStack space="$4" my="$4" ai="center" jc="center">
-              <SignInButton display={media.gtMd ? 'none' : 'flex'} />
-              <Anchor href="https://info.send.it">About</Anchor>
-              <Anchor href="https://info.send.it/legal/privacy-policy">Privacy</Anchor>
-              <Anchor href="https://info.send.it/legal/terms-of-service">Terms</Anchor>
-            </XStack>
+        <YStack w="100%">
+          <YStack jc="flex-end" f={1} gap="$2" $gtMd={{ pb: '$8' }} ml="auto" w="100%" maw={738}>
+            <Carousel currentKey={carouselProgress.toString()} fullscreen={false} />
           </YStack>
+          <XStack gap="$4" ai="center" jc="center">
+            <SignInButton display={media.gtMd ? 'none' : 'flex'} />
+            <Anchor color="$white" href="https://info.send.it">
+              About
+            </Anchor>
+            <Anchor color="$white" href="https://info.send.it/legal/privacy-policy">
+              Privacy
+            </Anchor>
+            <Anchor color="$white" href="https://info.send.it/legal/terms-of-service">
+              Terms
+            </Anchor>
+          </XStack>
         </YStack>
       </YStack>
     </YStack>
@@ -159,7 +162,7 @@ function SignInButton(props: ButtonProps) {
   const signInLink = useLink({ href: '/auth/sign-in' })
   return (
     <Button {...signInLink} size="$4" theme="green" w="$12" {...props}>
-      Sign In
+      /SIGN IN
     </Button>
   )
 }
