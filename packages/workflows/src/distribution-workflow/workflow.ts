09c61543a16634ab5749265f6facc21f8ebd0468
Big Boss <bigboss@metalrodeo.xyz>
2024-07-11 12:50:02 -0500

    add fetch all distributions

--- packages/workflows/src/distribution-workflow/workflow.ts
+++ packages/workflows/src/distribution-workflow/workflow.ts
@@ -2,9 +2,11 @@
 import { proxyActivities, log, ApplicationFailure } from '@temporalio/workflow'
 import type { createActivities } from './activities'
 
-const { calculateDistributionSharesActivity, fetchDistributionActivity } = proxyActivities<
-  ReturnType<typeof createActivities>
->({
+const {
+  calculateDistributionSharesActivity,
+  fetchDistributionActivity,
+  fetchAllOpenDistributionsActivity,
+} = proxyActivities<ReturnType<typeof createActivities>>({
   startToCloseTimeout: '30 seconds',
 })
 
@@ -12,4 +14,14 @@ export async function DistributionWorkflow(distributionId: number): Promise<void
   const distribution = await fetchDistributionActivity(distributionId.toString())
   if (!distribution) throw new ApplicationFailure('Distribution not found')
   await calculateDistributionSharesActivity(distribution)
+}
+
+export async function DistributionsWorkflow(): Promise<void> {
+  // fetch all distributions in qualification period
+  const distributions = await fetchAllOpenDistributionsActivity()
+  if (!distributions) throw new ApplicationFailure('No distributions found')
+
+  for (const distribution of distributions) {
+    await DistributionWorkflow(distribution.id) // calculate one distribution at a time for now
+  }
 }
