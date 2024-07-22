c28cce20847e8ae3daf0e379fa26bc4c8d8c82ac
Big Boss <bigboss@metalrodeo.xyz>
2024-07-22 15:32:42 -0700

    simplify referrer check

--- packages/api/src/routers/tag.ts
+++ packages/api/src/routers/tag.ts
@@ -130,7 +130,7 @@ export const tagRouter = createTRPCRouter({
       const invalidAmount = !amount || BigInt(amount) !== amountDue
       const invalidReferrer =
         (!referrer && rewardSent !== 0n) || // no referrer and reward is sent
-        (referrer && (!reward || rewardSent !== rewardDue)) // referrer and invalid reward
+        (referrer && rewardSent !== rewardDue) // referrer and invalid reward
 
       if (invalidAmount || invalidReferrer) {
         log('transaction is not a payment for tags or incorrect amount', `txHash=${txHash}`, data)
