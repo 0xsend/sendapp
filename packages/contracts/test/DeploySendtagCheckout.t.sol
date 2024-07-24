8dd6df7db9eb58598093cd320c2c4d48c16cfd94
Big Boss <bigboss@metalrodeo.xyz>
2024-07-24 07:32:36 -0500

    deploy sendtagcheckout to base sepolia

--- packages/contracts/test/DeploySendtagCheckout.t.sol
+++ packages/contracts/test/DeploySendtagCheckout.t.sol
@@ -16,10 +16,13 @@ contract DeploySendtagCheckoutTest is Test, Helper {
         DeploySendtagCheckoutScript script = new DeploySendtagCheckoutScript();
         address multisig = address(SEND_REVENUE_SAFE);
         address token = address(0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913);
+        address owner = address(0xB055);
         vm.setEnv("MULTISIG", vm.toString(SEND_REVENUE_SAFE));
         vm.setEnv("TOKEN", vm.toString(token));
+        vm.setEnv("OWNER", vm.toString(owner));
         SendtagCheckout sc = script.run();
         assertEq(sc.multisig(), multisig);
         assertEq(address(sc.token()), token);
+        assertEq(sc.owner(), owner);
     }
 }
