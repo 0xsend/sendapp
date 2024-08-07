740e93591e89c5ad8ed640940eb13388d2e0ecc2
Tawnee <tawnee.a@proton.me>
2024-08-07 12:46:29 -0600

    leaderboard data (#652)
    Get leaderboard data from db

--- packages/app/features/leaderboard/screen.test.tsx
+++ packages/app/features/leaderboard/screen.test.tsx
@@ -3,6 +3,26 @@ import { Provider } from 'app/__mocks__/app/provider'
 import { act, render, screen } from '@testing-library/react-native'
 import { LeaderboardScreen } from './screen'
 
+const testReferral = {
+  referrals: 10,
+  rewards_usdc: 12,
+  user: {
+    avatar_url: 'https://i.pravatar.cc/500?u=Frederick Bartell',
+    id: 1,
+    name: 'Sophie Jenkins Jr.',
+    send_id: 47788,
+  },
+}
+
+jest.mock('./utils/useLeaderBoard', () => ({
+  useLeaderboard: jest.fn().mockReturnValue({
+    data: {
+      referrals: [testReferral],
+      rewards: [],
+    },
+  }),
+}))
+
 describe('LeaderboardScreen', () => {
   it('renders leaderboard screen', async () => {
     jest.useFakeTimers()
@@ -15,8 +35,8 @@ describe('LeaderboardScreen', () => {
       jest.runAllTimers()
     })
 
-    const title = await screen.findByTestId('mainTitle')
-    expect(title).toHaveTextContent('Best in Class')
+    expect(screen.getByTestId('mainTitle')).toHaveTextContent('Best in Class')
+    expect(screen.getByTestId(`${testReferral.user.id}-${testReferral.user.send_id}`)).toBeVisible()
     expect(screen.toJSON()).toMatchSnapshot('LeaderboardScreen')
   })
 })
