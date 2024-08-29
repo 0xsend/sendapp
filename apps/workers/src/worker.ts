97f8125170b39c77a1e8553ccf1d9d944b64fde7
youngkidwarrior <victor@she.energy>
2024-08-28 17:57:55 -0700

    Create transfer tempora workflow

--- apps/workers/src/worker.ts
+++ apps/workers/src/worker.ts
@@ -1,5 +1,8 @@
 import { Worker } from '@temporalio/worker'
-import { createActivities } from '@my/workflows/all-activities'
+import {
+  createTransferActivities,
+  createDistributionActivities,
+} from '@my/workflows/all-activities'
 import { URL, fileURLToPath } from 'node:url'
 import path from 'node:path'
 
@@ -8,20 +11,27 @@ async function run() {
     `../../../packages/workflows/src/all-workflows${path.extname(import.meta.url)}`,
     import.meta.url
   )
-
   // Step 1: Register Workflows and Activities with the Worker and connect to
   // the Temporal server.
-  const worker = await Worker.create({
+  const transferWorker = await Worker.create({
     workflowsPath: fileURLToPath(workflowsPathUrl),
-    activities: createActivities(
-      process.env.NEXT_PUBLIC_SUPABASE_URL,
-      process.env.SUPABASE_SERVICE_ROLE
-    ),
-    taskQueue: 'dev',
+    activities: {
+      ...createTransferActivities(
+        process.env.NEXT_PUBLIC_SUPABASE_URL,
+        process.env.SUPABASE_SERVICE_ROLE
+      ),
+      // ...createDistributionActivities(
+      //   process.env.NEXT_PUBLIC_SUPABASE_URL,
+      //   process.env.SUPABASE_SERVICE_ROLE
+      // ),
+    },
+    namespace: 'default',
+    taskQueue: 'monorepo',
     bundlerOptions: {
       ignoreModules: ['@supabase/supabase-js'],
     },
   })
+
   // Worker connects to localhost by default and uses console.error for logging.
   // Customize the Worker by passing more options to create():
   // https://typescript.temporal.io/api/classes/worker.Worker
@@ -30,7 +40,7 @@ async function run() {
   // https://github.com/temporalio/samples-typescript/tree/main/hello-world-mtls
 
   // Step 2: Start accepting tasks on the `monorepo` queue
-  await worker.run()
+  await transferWorker.run()
 
   // You may create multiple Workers in a single process in order to poll on multiple task queues.
 }
