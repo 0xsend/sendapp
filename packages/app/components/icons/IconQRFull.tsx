8ece09b35a736055df7fcb2e943ff9bee52c3b9f
Tawnee <tawnee.a@proton.me>
2024-09-11 17:03:17 -0600

    account page styles

---
+++ packages/app/components/icons/IconQRFull.tsx
@@ -0,0 +1,25 @@
+import { type IconProps, themed } from '@tamagui/helpers-icon'
+import { memo } from 'react'
+import { Path, Svg } from 'react-native-svg'
+import type { ColorTokens } from '@my/ui/types'
+
+const QRFull = (props) => {
+  const { size, color, ...rest } = props
+  return (
+    <Svg
+      width={size ?? 28}
+      height={size ?? 28}
+      color={color as ColorTokens | undefined}
+      viewBox="0 0 30 30"
+      fill="none"
+      {...rest}
+    >
+      <Path
+        fill="currentColor"
+        d="M0 13.611V0h13.611v13.611H0Zm2.778-2.778h8.055V2.778H2.778v8.055ZM0 30V16.389h13.611V30H0Zm2.778-2.778h8.055v-8.055H2.778v8.055Zm13.61-13.61V0H30v13.611H16.389Zm2.779-2.779h8.055V2.778h-8.055v8.055ZM26.61 30v-3.389H30V30h-3.389ZM16.39 19.805V16.39h3.389v3.416h-3.39Zm3.389 3.39v-3.39h3.417v3.39h-3.417Zm-3.39 3.416v-3.416h3.39v3.416h-3.39ZM19.779 30v-3.389h3.417V30h-3.417Zm3.417-3.389v-3.416h3.416v3.416h-3.416Zm0-6.806V16.39h3.416v3.416h-3.416Zm3.416 3.39v-3.39H30v3.39h-3.389Z"
+      />
+    </Svg>
+  )
+}
+const IconQRFull = memo<IconProps>(themed(QRFull))
+export { IconQRFull }
---
+++ packages/app/components/icons/IconStarOutline.tsx
@@ -0,0 +1,25 @@
+import { type IconProps, themed } from '@tamagui/helpers-icon'
+import { memo } from 'react'
+import { Path, Svg } from 'react-native-svg'
+import type { ColorTokens } from '@my/ui/types'
+
+const StarOutline = (props) => {
+  const { size, color, ...rest } = props
+  return (
+    <Svg
+      width={size ?? 28}
+      height={size ?? 28}
+      color={color as ColorTokens | undefined}
+      viewBox="0 0 20 19"
+      fill="none"
+      {...rest}
+    >
+      <Path
+        fill="currentColor"
+        d="m6.85 14.825 3.15-1.9 3.15 1.925-.825-3.6 2.775-2.4-3.65-.325-1.45-3.4L8.55 8.5l-3.65.325 2.775 2.425-.825 3.575ZM3.825 19l1.625-7.025L0 7.25l7.2-.625L10 0l2.8 6.625 7.2.625-5.45 4.725L16.175 19 10 15.275 3.825 19Z"
+      />
+    </Svg>
+  )
+}
+const IconStarOutline = memo<IconProps>(themed(StarOutline))
+export { IconStarOutline }
