5e3de62e121a66ada9f408af982da75b6be90883
Tawnee <tawnee.a@proton.me>
2024-09-13 18:52:12 -0600

    settings section

--- packages/app/features/account/settings/layout.web.tsx
+++ packages/app/features/account/settings/layout.web.tsx
@@ -1,4 +1,4 @@
-import { Separator, XStack, YStack } from '@my/ui'
+import { Card, Separator, XStack, YStack } from '@my/ui'
 import { SettingsLinks } from './SettingsLinks'
 
 export type SettingsLayoutProps = {
@@ -14,29 +14,32 @@ export type SettingsLayoutProps = {
 
 export const SettingsLayout = ({ children }: SettingsLayoutProps) => {
   return (
-    <XStack f={1} pt={'$4'}>
-      <YStack
-        // this file is web-only so we can safely use CSS
-        style={{
-          transition: '200ms ease width',
-        }}
-        width="$14"
-        display="none"
-        $gtLg={{ display: 'flex' }}
-        py={'$4'}
-      >
-        <YStack width={'100%'} gap={'$4'}>
-          <YStack jc={'space-between'} zIndex={4} flex={1} width={'100%'}>
-            <YStack h={'inherit'} gap={'$6'} width={'100%'}>
-              <SettingsLinks />
+    <YStack pb={'$size.3.5'} f={1} pt={'$size.0.9'} $gtLg={{ pt: 0 }}>
+      <Card width={'100%'} p={'$size.3.5'}>
+        <XStack f={1}>
+          <YStack
+            // this file is web-only so we can safely use CSS
+            style={{
+              transition: '200ms ease width',
+            }}
+            width="$size.16"
+            display="none"
+            $gtLg={{ display: 'flex' }}
+          >
+            <YStack width={'100%'} gap={'$4'}>
+              <YStack jc={'space-between'} zIndex={4} flex={1} width={'100%'}>
+                <YStack h={'inherit'} gap={'$size.1.5'} width={'100%'}>
+                  <SettingsLinks />
+                </YStack>
+              </YStack>
             </YStack>
           </YStack>
-        </YStack>
-      </YStack>
-      <Separator display="none" $gtLg={{ display: 'flex' }} vertical />
-      <YStack f={1} ai="center" $gtLg={{ ml: '$8' }}>
-        <YStack width="100%">{children}</YStack>
-      </YStack>
-    </XStack>
+          <Separator display="none" $gtLg={{ display: 'flex' }} vertical />
+          <YStack f={1} ai="center" $gtLg={{ ml: '$size.8' }}>
+            <YStack width="100%">{children}</YStack>
+          </YStack>
+        </XStack>
+      </Card>
+    </YStack>
   )
 }
