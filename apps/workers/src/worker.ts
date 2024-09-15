35a8f95e5140a0fdc9824fe7721a5ac33fb604f5
youngkidwarrior <victor@she.energy>
2024-09-15 01:11:48 -0700

    Use Temporal Cloud in dev and production

--- apps/workers/src/worker.ts
+++ apps/workers/src/worker.ts
@@ -1,13 +1,36 @@
-import { Worker } from '@temporalio/worker'
+import { Worker, NativeConnection } from '@temporalio/worker'
 import { createTransferActivities } from '@my/workflows/all-activities'
+import fs from 'node:fs/promises'
 
 import { createRequire } from 'node:module'
 const require = createRequire(import.meta.url)
 
+const { NODE_ENV = 'development' } = process.env
+const isDeployed = ['production', 'staging'].includes(NODE_ENV)
+
 async function run() {
+  const connection = isDeployed
+    ? await NativeConnection.connect({
+        address: `${process.env.TEMPORAL_NAMESPACE}.tmprl.cloud:7233`,
+        tls: {
+          clientCertPair: {
+            crt: await fs.readFile(process.env.TEMPORAL_MTLS_TLS_CERT ?? '').catch((e) => {
+              console.error(e)
+              throw new Error('no cert found. Check the TEMPORAL_MTLS_TLS_CERT env var')
+            }),
+            key: await fs.readFile(process.env.TEMPORAL_MTLS_TLS_KEY ?? '').catch((e) => {
+              console.error(e)
+              throw new Error('no key found. Check the TEMPORAL_MTLS_TLS_KEY env var')
+            }),
+          },
+        },
+      })
+    : undefined
+
   // Step 1: Register Workflows and Activities with the Worker and connect to
   // the Temporal server.
   const worker = await Worker.create({
+    connection,
     workflowsPath: require.resolve('@my/workflows'),
     dataConverter: {
       payloadConverterPath: require.resolve(
@@ -17,7 +40,7 @@ async function run() {
     activities: {
       ...createTransferActivities(process.env),
     },
-    namespace: 'default',
+    namespace: process.env.TEMPORAL_NAMESPACE ?? 'default',
     taskQueue: 'monorepo',
     bundlerOptions: {
       ignoreModules: ['@supabase/supabase-js'],
