ea848a8a1ea0de62c10644f8196f49dd02cfa298
youngkidwarrior <victor@she.energy>
2024-09-21 14:53:23 -0700

    Fix Action Buttons to bottom of screen on mobile

--- packages/app/features/home/layout.web.tsx
+++ packages/app/features/home/layout.web.tsx
@@ -1,6 +1,18 @@
-import { YStack, ScrollView, Container, type ScrollViewProps, usePwa } from '@my/ui'
+import {
+  YStack,
+  ScrollView,
+  Container,
+  type ScrollViewProps,
+  Stack,
+  usePwa,
+  LinearGradient,
+} from '@my/ui'
+
 import { HomeSideBarWrapper } from 'app/components/sidebar/HomeSideBar'
 import { TagSearchProvider } from 'app/provider/tag-search'
+
+import { usePathname } from 'app/utils/usePathname'
+import { ActionButtonRow } from './ActionButtonRow'
 
 export function HomeLayout({
   children,
@@ -11,6 +23,8 @@ export function HomeLayout({
   TopNav?: React.ReactNode
 } & ScrollViewProps) {
   const isPwa = usePwa()
+
+  const pathname = usePathname()
   return (
     <HomeSideBarWrapper>
       <TagSearchProvider>
@@ -24,10 +38,31 @@ export function HomeLayout({
           <YStack gap="$3" $gtLg={{ pt: 80 }} w={'100%'}>
             {TopNav}
           </YStack>
-          <Container $gtLg={{ pt: '$5', pb: '$0' }} safeAreaPadding={isPwa && 'b'}>
-            {children}
-          </Container>
+          <Container $gtLg={{ pt: '$5', pb: '$0' }}>{children}</Container>
         </ScrollView>
+        {pathname === '/' && (
+          <Stack
+            w={'100%'}
+            pb={!isPwa && '$5'}
+            px="$4"
+            $platform-web={{
+              position: 'fixed',
+              bottom: 0,
+              left: '50%',
+              transform: 'translate(-50%, 0)', //center fixed element
+            }}
+            $gtLg={{ display: 'none' }}
+          >
+            <LinearGradient
+              start={{ x: 0, y: 1 }}
+              end={{ x: 0, y: 0 }}
+              locations={[0, 0.8]}
+              fullscreen
+              colors={['transparent', '$background']}
+            />
+            <ActionButtonRow />
+          </Stack>
+        )}
       </TagSearchProvider>
     </HomeSideBarWrapper>
   )
