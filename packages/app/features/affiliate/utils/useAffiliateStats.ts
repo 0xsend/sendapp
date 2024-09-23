dc8be929850264ac3e3e3a6d8213504139c32cb0
youngkidwarrior <victor@she.energy>
2024-09-23 01:19:56 -0700

    Affiliate Stats and Referral Hooks

--- packages/app/features/affiliate/utils/useAffiliateStats.ts
+++ packages/app/features/affiliate/utils/useAffiliateStats.ts
@@ -1,22 +1,25 @@
-import type { Tables } from '@my/supabase/database-generated.types'
-import type { PostgrestError } from '@supabase/postgrest-js'
-import { useQuery, type UseQueryResult } from '@tanstack/react-query'
+import { useQuery } from '@tanstack/react-query'
 import { useSupabase } from 'app/utils/supabase/useSupabase'
 
-export const useAffiliateStats = (): UseQueryResult<Tables<'affiliate_stats'>, PostgrestError> => {
+export function useAffiliateStats() {
   const supabase = useSupabase()
-  return useQuery({
-    queryKey: ['affiliate_stats'],
-    queryFn: async () => {
-      const { data, error } = await supabase.from('affiliate_stats').select('*').single()
-      if (error) {
-        // no rows in receipts table
-        if (error.code === 'PGRST116') {
-          return null
-        }
-        throw new Error(error.message)
+  async function fetchAffiliateStats() {
+    const request = supabase.from('affiliate_stats_summary').select('*').single()
+
+    const { data, error } = await request
+    if (error) {
+      // no rows in receipts table
+      if (error.code === 'PGRST116') {
+        return null
       }
-      return data
-    },
+      throw new Error(error.message)
+    }
+
+    return data
+  }
+
+  return useQuery({
+    queryKey: ['affiliate_stats_summary'],
+    queryFn: fetchAffiliateStats,
   })
 }
