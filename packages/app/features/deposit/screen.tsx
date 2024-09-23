052dcfcaceede0e083a7ae5cde5d7bccd3213bae
youngkidwarrior <victor@she.energy>
2024-09-23 01:22:55 -0700

    Add back arrow to mobile deposit screen

--- packages/app/features/deposit/screen.tsx
+++ packages/app/features/deposit/screen.tsx
@@ -1,20 +1,10 @@
-import { H4, XStack, YStack, type YStackProps } from '@my/ui'
+import { H4, XStack, YStack } from '@my/ui'
 import { DepositAddress } from 'app/components/DepositAddress'
 import { useSendAccount } from 'app/utils/send-accounts'
 
-/**
- * Deposit screen shows the various options for depositing funds.
- * - Web3 Wallet (window.ethereum)
- * - Coinbase Pay
- * - ???
- */
 export function DepositScreen() {
-  return <DepositWelcome />
-}
-
-export function DepositWelcome(props: YStackProps) {
   return (
-    <YStack mt="$4" mx="auto" width={'100%'} $sm={{ maxWidth: 600 }} {...props}>
+    <YStack mt="$4" mx="auto" width={'100%'} $sm={{ maxWidth: 600 }}>
       <YStack w={'100%'}>
         <DepositAddressWrapper />
       </YStack>
