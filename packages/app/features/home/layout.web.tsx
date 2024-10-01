d7ca54c854c15f2255a5521b6f295e01e908813c
youngkidwarrior <victor@she.energy>
2024-09-30 18:47:16 -0700

    Refactor Mobile Buttons Row and use scroll context

--- packages/app/features/home/layout.web.tsx
+++ packages/app/features/home/layout.web.tsx
@@ -1,10 +1,7 @@
-import { YStack, ScrollView, Container, type ScrollViewProps, useMedia } from '@my/ui'
+import { YStack, ScrollView, Container, type ScrollViewProps } from '@my/ui'
 import { HomeSideBarWrapper } from 'app/components/sidebar/HomeSideBar'
 import { TagSearchProvider } from 'app/provider/tag-search'
-import { usePathname } from 'app/utils/usePathname'
-import { HomeButtonRow } from './HomeButtons'
-import type { NativeScrollEvent, NativeSyntheticEvent } from 'react-native'
-import { useState } from 'react'
+import { useScrollDirection } from 'app/provider/scroll'
 
 export function HomeLayout({
   children,
@@ -14,24 +11,7 @@ export function HomeLayout({
   children: React.ReactNode
   TopNav?: React.ReactNode
 } & ScrollViewProps) {
-  const pathname = usePathname()
-  const media = useMedia()
-  const [isActionRowVisible, setIsActionRowVisible] = useState(true)
-  const [, setScrollY] = useState(0)
-  const onScroll = (e: NativeSyntheticEvent<NativeScrollEvent>) => {
-    const { contentOffset, contentSize, layoutMeasurement } = e.nativeEvent
-    const isEndOfView = layoutMeasurement.height + contentOffset.y >= contentSize.height - 50
-    setScrollY((prev) => {
-      if ((prev > contentOffset.y && !isEndOfView) || contentOffset.y < 50) {
-        setIsActionRowVisible(true)
-      } else if (prev < e.nativeEvent.contentOffset.y || isEndOfView) {
-        setIsActionRowVisible(false)
-      } else {
-        setIsActionRowVisible(true)
-      }
-      return e.nativeEvent.contentOffset.y
-    })
-  }
+  const { onScroll } = useScrollDirection()
   return (
     <HomeSideBarWrapper>
       <TagSearchProvider>
@@ -49,7 +29,6 @@ export function HomeLayout({
           </YStack>
           <Container $gtLg={{ pt: '$5', pb: '$0' }}>{children}</Container>
         </ScrollView>
-        {media.lg && pathname === '/' && <HomeButtonRow isVisible={isActionRowVisible} />}
       </TagSearchProvider>
     </HomeSideBarWrapper>
   )
