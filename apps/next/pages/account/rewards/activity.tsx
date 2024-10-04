2ac4f3432cf1d5aee83279732ee7b9e70f6adf20
youngkidwarrior <victor@she.energy>
2024-10-03 18:57:34 -0700

    Rewards Activity Page

--- apps/next/pages/account/rewards/activity.tsx
+++ apps/next/pages/account/rewards/activity.tsx
@@ -1,40 +1,25 @@
-import { RewardsScreen } from 'app/features/account/rewards/activity/screen'
+import { ActivityRewardsScreen } from 'app/features/account/rewards/activity/screen'
 import Head from 'next/head'
 import { userProtectedGetSSP } from 'utils/userProtected'
 import type { NextPageWithLayout } from 'next-app/pages/_app'
 import { HomeLayout } from 'app/features/home/layout.web'
-import { ButtonOption, TopNav } from 'app/components/TopNav'
+import { TopNav } from 'app/components/TopNav'
 
 export const Page: NextPageWithLayout = () => {
   return (
     <>
       <Head>
-        <title>Send | Send Rewards</title>
+        <title>Send | Activity Rewards</title>
       </Head>
-      <RewardsScreen />
+      <ActivityRewardsScreen />
     </>
   )
 }
 
 export const getServerSideProps = userProtectedGetSSP()
 
-const subheader =
-  'Register at least 1 Sendtag, maintain the minimum balance, avoid selling, and refer others for a bonus multiplier. '
-
 Page.getLayout = (children) => (
-  <HomeLayout
-    $platform-web={{
-      h: '100svh',
-      contentContainerStyle: {
-        h: '100svh',
-      },
-    }}
-    TopNav={
-      <TopNav header="Send Rewards" showLogo subheader={subheader} button={ButtonOption.PROFILE} />
-    }
-  >
-    {children}
-  </HomeLayout>
+  <HomeLayout TopNav={<TopNav header="Activity Rewards" showLogo />}>{children}</HomeLayout>
 )
 
 export default Page
