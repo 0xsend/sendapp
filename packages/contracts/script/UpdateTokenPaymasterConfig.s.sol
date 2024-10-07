0feffb619ce3e00af4647e64912b0f45246a318b
Big Boss <bigboss@metalrodeo.xyz>
2024-10-07 12:30:09 -0700

    update ci and add clear base fee contract

--- packages/contracts/script/UpdateTokenPaymasterConfig.s.sol
+++ packages/contracts/script/UpdateTokenPaymasterConfig.s.sol
@@ -7,6 +7,7 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import "../src/TokenPaymaster.sol";
 
+// solhint-disable no-console
 contract UpdateTokenPaymasterConfigScript is Script, Helper {
     uint256 private constant PRICE_DENOM = 1e26;
     uint40 private constant BASE_FEE_DEFAULT = 5e4; // ¢5
@@ -28,12 +29,26 @@ contract UpdateTokenPaymasterConfigScript is Script, Helper {
             uint40 _baseFee,
             address _rewardsPool
         ) = paymaster.tokenPaymasterConfig();
+
+        console2.log("current priceMarkup", _priceMarkup);
+        console2.log("current minEntryPointBalance", _minEntryPointBalance);
+        console2.log("current refundPostopCost", _refundPostopCost);
+        console2.log("current priceMaxAge", _priceMaxAge);
+        console2.log("current baseFee", _baseFee);
+        console2.log("current rewardsPool", _rewardsPool);
+
         uint256 priceMarkup = vm.envOr("PRICE_MARKUP", _priceMarkup);
         uint128 minEntryPointBalance = uint128(vm.envOr("MIN_ENTRY_POINT_BALANCE", _minEntryPointBalance));
         uint48 refundPostopCost = uint48(vm.envOr("REFUND_POSTOP_COST", _refundPostopCost));
         uint48 priceMaxAge = uint48(vm.envOr("PRICE_MAX_AGE", _priceMaxAge));
         uint40 baseFee = uint40(vm.envOr("BASE_FEE", _baseFee));
+        bool baseFeeClear = vm.envOr("BASE_FEE_CLEAR", false);
         address rewardsPool = vm.envOr("REWARDS_POOL", _rewardsPool);
+
+        if (baseFeeClear) {
+            console2.log("Clearing baseFee", baseFeeClear);
+            baseFee = 0;
+        }
 
         require(
             priceMaxAge != _priceMaxAge || refundPostopCost != _refundPostopCost
@@ -45,7 +60,7 @@ contract UpdateTokenPaymasterConfigScript is Script, Helper {
         require(refundPostopCost > 0, "REFUND_POSTOP_COST env variable not set");
         require(minEntryPointBalance > 0, "MIN_ENTRY_POINT_BALANCE env variable not set");
         require(priceMarkup > 0, "PRICE_MARKUP env variable not set");
-        require(baseFee > 0, "BASE_FEE env variable not set");
+        require(baseFee > 0 || (baseFee == 0 && baseFeeClear), "BASE_FEE env variable not set");
 
         vm.startBroadcast();
         TokenPaymasterConfig memory tpc = TokenPaymasterConfig({
