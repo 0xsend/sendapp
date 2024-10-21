a3365455f2ae1ea200f96e1cc17fd5c2fd420110
youngkidwarrior <victor@she.energy>
2024-10-20 20:34:15 -0700

    Modify TopNav behavior to allow for page back

--- apps/next/pages/account/rewards/index.tsx
+++ apps/next/pages/account/rewards/index.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head'
 import { userProtectedGetSSP } from 'utils/userProtected'
 import type { NextPageWithLayout } from 'next-app/pages/_app'
 import { HomeLayout } from 'app/features/home/layout.web'
-import { ButtonOption, TopNav } from 'app/components/TopNav'
+import { TopNav } from 'app/components/TopNav'
 
 export const Page: NextPageWithLayout = () => {
   return (
@@ -18,9 +18,7 @@ export const Page: NextPageWithLayout = () => {
 
 export const getServerSideProps = userProtectedGetSSP()
 Page.getLayout = (children) => (
-  <HomeLayout TopNav={<TopNav header="Rewards" button={ButtonOption.PROFILE} />}>
-    {children}
-  </HomeLayout>
+  <HomeLayout TopNav={<TopNav header="Rewards" />}>{children}</HomeLayout>
 )
 
 export default Page
