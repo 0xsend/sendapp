c501febd4b09c3cd20e3fc85394868c9be6a08ef
youngkidwarrior <victor@she.energy>
2024-11-03 16:05:46 -0800

    Refactor MobileButtonRow and detect for path change

--- packages/app/components/MobileButtonRowLayout.tsx
+++ packages/app/components/MobileButtonRowLayout.tsx
@@ -38,8 +38,53 @@ const Row = styled(XStack, {
   pointerEvents: 'auto',
 })
 
-export const Home = ({ children, ...props }: XStackProps) => {
+const MobileButtonRow = ({
+  children,
+  isLoading,
+  isVisible = true,
+}: { children: React.ReactElement; isLoading: boolean; isVisible?: boolean } & XStackProps) => {
+  const { direction } = useScrollDirection()
   const isPwa = usePwa()
+
+  return (
+    <AnimatePresence>
+      {!isLoading && isVisible && direction !== 'down' && (
+        <Stack
+          w={'100%'}
+          pb={isPwa ? '$1' : '$5'}
+          px="$4"
+          $platform-web={{
+            position: 'fixed',
+            bottom: 0,
+          }}
+          $gtLg={{
+            display: 'none',
+          }}
+          animation="200ms"
+          opacity={1}
+          animateOnly={['scale', 'transform', 'opacity']}
+          enterStyle={{ opacity: 0, scale: 0.9 }}
+          exitStyle={{ opacity: 0, scale: 0.95 }}
+        >
+          <LinearGradient
+            h={'150%'}
+            top={'-50%'}
+            start={{ x: 0, y: 1 }}
+            end={{ x: 0, y: 0 }}
+            locations={[0, 0.33]}
+            fullscreen
+            colors={['transparent', '$background']}
+            $gtLg={{ display: 'none' }}
+            pointerEvents="none"
+          />
+          {children}
+        </Stack>
+      )}
+    </AnimatePresence>
+  )
+}
+
+const Home = ({ children, ...props }: XStackProps) => {
   const { isLoading: isLoadingSendAccount } = useSendAccount()
   const { balances, isLoading: isLoadingBalances } = useSendAccountBalances()
   const selectedCoin = useCoinFromTokenParam()
@@ -48,146 +93,77 @@ export const Home = ({ children, ...props }: XStackProps) => {
     usdcBalance !== undefined &&
     usdcBalance >= parseUnits('.20', coinsDict[usdcAddress[baseMainnet.id]].decimals)
 
-  const { direction } = useScrollDirection()
-
   const isLoading = isLoadingSendAccount || isLoadingBalances
 
   return (
     <>
       {children}
-      <AnimatePresence>
-        {!isLoading && direction !== 'down' && (
-          <Stack
-            w={'100%'}
-            pb={isPwa ? '$1' : '$5'}
-            px="$4"
-            $platform-web={{
-              position: 'fixed',
-              bottom: 0,
-            }}
-            $gtLg={{
-              display: 'none',
-            }}
-            animation="200ms"
-            opacity={1}
-            animateOnly={['scale', 'transform', 'opacity']}
-            enterStyle={{ opacity: 0, scale: 0.9 }}
-            exitStyle={{ opacity: 0, scale: 0.95 }}
-          >
-            <LinearGradient
-              h={'150%'}
-              top={'-50%'}
-              start={{ x: 0, y: 1 }}
-              end={{ x: 0, y: 0 }}
-              locations={[0, 0.33]}
-              fullscreen
-              colors={['transparent', '$background']}
-              $gtLg={{ display: 'none' }}
-              pointerEvents="none"
-            />
-            <Row {...props}>
-              {(() => {
-                switch (true) {
-                  case !canSend:
-                    return null
-                  case selectedCoin !== undefined:
-                    return (
-                      <Stack f={1} $gtSm={{ w: '50%' }} flexDirection="row-reverse" maw={350}>
-                        <HomeButtons.SendButton />
-                      </Stack>
-                    )
-                  default:
-                    return (
-                      <>
-                        <Stack f={1} w="50%" flexDirection="row-reverse" maw={350}>
-                          <HomeButtons.GhostDepositButton />
-                        </Stack>
+      <MobileButtonRow isLoading={isLoading}>
+        <Row {...props}>
+          {(() => {
+            switch (true) {
+              case !canSend:
+                return null
+              case selectedCoin !== undefined:
+                return (
+                  <Stack f={1} $gtSm={{ w: '50%' }} flexDirection="row-reverse" maw={350}>
+                    <HomeButtons.SendButton />
+                  </Stack>
+                )
+              default:
+                return (
+                  <>
+                    <Stack f={1} w="50%" flexDirection="row-reverse" maw={350}>
+                      <HomeButtons.GhostDepositButton />
+                    </Stack>
 
-                        <Stack f={1} w="50%" jc={'center'} maw={350}>
-                          <HomeButtons.SendButton />
-                        </Stack>
-                      </>
-                    )
-                }
-              })()}
-            </Row>
-          </Stack>
-        )}
-      </AnimatePresence>
+                    <Stack f={1} w="50%" jc={'center'} maw={350}>
+                      <HomeButtons.SendButton />
+                    </Stack>
+                  </>
+                )
+            }
+          })()}
+        </Row>
+      </MobileButtonRow>
     </>
   )
 }
 
-export const Profile = (
+const Profile = (
   { children, ...props }: XStackProps //@todo another use case for a generated type for our route names
 ) => {
-  const isPwa = usePwa()
   const [{ sendid, tag }] = useProfileScreenParams()
   const identifier = tag ?? sendid ?? ''
   const identifierType = tag ? 'tag' : 'sendid'
   const { profile, isLoading } = useUser()
   const { data: otherUserProfile } = useProfileLookup(identifierType, identifier?.toString() || '')
 
-  const { direction } = useScrollDirection()
-
   const isVisible = Boolean(otherUserProfile) && profile?.send_id !== otherUserProfile?.sendid
 
   return (
     <>
       {children}
-      <AnimatePresence>
-        {!isLoading && isVisible && direction !== 'down' && (
-          <Stack
-            w={'100%'}
-            pb={isPwa ? '$1' : '$5'}
-            px="$4"
-            $platform-web={{
-              position: 'fixed',
-              bottom: 0,
-            }}
-            $gtLg={{
-              display: 'none',
-            }}
-            animation="200ms"
-            opacity={1}
-            animateOnly={['scale', 'transform', 'opacity']}
-            enterStyle={{ opacity: 0, scale: 0.9 }}
-            exitStyle={{ opacity: 0, scale: 0.95 }}
-            pointerEvents="none"
-          >
-            <LinearGradient
-              h={'150%'}
-              top={'-50%'}
-              start={{ x: 0, y: 1 }}
-              end={{ x: 0, y: 0 }}
-              locations={[0, 0.33]}
-              fullscreen
-              colors={['transparent', '$background']}
-              $gtLg={{ display: 'none' }}
+      <MobileButtonRow isLoading={isLoading} isVisible={isVisible}>
+        <Row {...props}>
+          <Stack w={200}>
+            <ProfileButtons.SendButton
+              identifier={otherUserProfile?.tag ?? otherUserProfile?.sendid ?? ''}
+              idType="tag"
             />
-            <Row {...props}>
-              <Stack w={200}>
-                <ProfileButtons.SendButton
-                  identifier={otherUserProfile?.tag ?? otherUserProfile?.sendid ?? ''}
-                  idType="tag"
-                />
-              </Stack>
-            </Row>
           </Stack>
-        )}
-      </AnimatePresence>
+        </Row>
+      </MobileButtonRow>
     </>
   )
 }
 
-export const ActivityRewards = ({ children, ...props }: XStackProps) => {
-  const isPwa = usePwa()
+const ActivityRewards = ({ children, ...props }: XStackProps) => {
   const [queryParams] = useRewardsScreenParams()
   const { data: distributions, isLoading } = useMonthlyDistributions()
   const distribution =
     distributions?.find((d) => d.number === queryParams.distribution) ?? distributions?.[0]
   const shareAmount = distribution?.distribution_shares?.[0]?.amount
-  const { direction } = useScrollDirection()
 
   const isVisible = distribution !== undefined && shareAmount !== undefined && shareAmount > 0
   const distributionMonth = distribution?.timezone_adjusted_qualification_end.toLocaleString(
@@ -204,62 +180,39 @@ export const ActivityRewards = ({ children, ...props }: XStackProps) => {
   return (
     <>
       {children}
-      <AnimatePresence>
-        {!isLoading && isVisible && direction !== 'down' && (
-          <Stack
-            w={'100%'}
-            pb={isPwa ? '$1' : '$5'}
-            px="$4"
-            $platform-web={{
-              position: 'fixed',
-              bottom: 0,
-            }}
-            $gtSm={{
-              display: 'none',
-            }}
-            animation="200ms"
-            opacity={1}
-            animateOnly={['scale', 'transform', 'opacity']}
-            enterStyle={{ opacity: 0, scale: 0.9 }}
-            exitStyle={{ opacity: 0, scale: 0.95 }}
-            pointerEvents="none"
-          >
-            <LinearGradient
-              h={'150%'}
-              top={'-50%'}
-              start={{ x: 0, y: 1 }}
-              end={{ x: 0, y: 0 }}
-              locations={[0, 0.33]}
-              fullscreen
-              colors={['transparent', '$background']}
-              $gtLg={{ display: 'none' }}
-            />
-            <Stack ai="center" jc="space-between" gap="$3" pt="$1">
-              <H3 fontWeight={'600'} color={'$color10'}>
-                {isQualificationOver
-                  ? `Total ${distributionMonth} Rewards`
-                  : `Estimated ${distributionMonth} Rewards`}
-              </H3>
+      <MobileButtonRow isLoading={isLoading} isVisible={isVisible}>
+        <Stack ai="center" jc="space-between" gap="$3" pt="$1">
+          <H3 fontWeight={'600'} color={'$color10'}>
+            {isQualificationOver
+              ? `Total ${distributionMonth} Rewards`
+              : `Estimated ${distributionMonth} Rewards`}
+          </H3>
 
-              <Row {...props} $xs={{ fd: 'column' }}>
-                <Paragraph
-                  fontFamily={'$mono'}
-                  $gtXs={{ fontSize: '$10' }}
-                  fontSize={'$9'}
-                  fontWeight={'500'}
-                  lh={40}
-                >
-                  {shareAmount === undefined ? '' : `${formatAmount(shareAmount, 10, 0)} SEND`}
-                </Paragraph>
-                <DistributionClaimButton distribution={distribution} />
-              </Row>
-            </Stack>
-          </Stack>
-        )}
-      </AnimatePresence>
+          <Row {...props} $xs={{ fd: 'column' }}>
+            <Paragraph
+              fontFamily={'$mono'}
+              $gtXs={{ fontSize: '$10' }}
+              fontSize={'$9'}
+              fontWeight={'500'}
+              lh={40}
+            >
+              {shareAmount === undefined ? '' : `${formatAmount(shareAmount, 10, 0)} SEND`}
+            </Paragraph>
+            {isVisible && <DistributionClaimButton distribution={distribution} />}
+          </Row>
+        </Stack>
+      </MobileButtonRow>
     </>
   )
 }
+
+Home.displayName = 'Home'
+Profile.displayName = 'Profile'
+ActivityRewards.displayName = 'ActivityRewards'
+
+MobileButtonRow.Home = Home
+MobileButtonRow.Profile = Profile
+MobileButtonRow.ActivityRewards = ActivityRewards
 
 export const MobileButtonRowLayout = {
   Home: Home,
