4490bbe7a7514b86ce0b1a4aa0062945019c1327
musidlo <musidlo@gmail.com>
2024-11-15 10:50:36 +0100

    Handle already used phone in UI

--- packages/app/features/auth/sign-up/sign-up-form.tsx
+++ packages/app/features/auth/sign-up/sign-up-form.tsx
@@ -9,13 +9,14 @@ import {
   XStack,
   YStack,
   useToastController,
+  Button,
 } from '@my/ui'
-import { TRPCClientError } from '@trpc/client'
 import { bytesToHex, hexToBytes } from 'viem'
 import { useRouter } from 'solito/router'
 import { z } from 'zod'
 import { FormProvider, useForm } from 'react-hook-form'
 import { RecoveryOptions } from '@my/api/src/routers/account-recovery/types'
+import { AuthStatus } from '@my/api/src/routers/auth/types'
 import { VerifyCode } from 'app/features/auth/components/VerifyCode'
 import { SchemaForm, formFields } from 'app/utils/SchemaForm'
 import { api } from 'app/utils/api'
@@ -30,22 +31,27 @@ const SignUpSchema = z.object({
 
 export const SignUpForm = () => {
   const form = useForm<z.infer<typeof SignUpSchema>>()
-  const signInWithOtp = api.auth.signInWithOtp.useMutation()
   const router = useRouter()
   const [queryParams] = useAuthScreenParams()
   const { redirectUri } = queryParams
   const toast = useToastController()
   const [captchaToken, setCaptchaToken] = useState<string | undefined>()
   const [isSigningIn, setIsSigningIn] = useState(false)
+  const [shouldShowBackUpPrompt, setShouldShowBackUpPrompt] = useState(false)
+
+  const { mutateAsync: signInWithOtpMutateAsync } = api.auth.signInWithOtp.useMutation({
+    retry: false,
+  })
 
   const { mutateAsync: getChallengeMutateAsync } = api.challenge.getChallenge.useMutation({
     retry: false,
   })
+
   const { mutateAsync: validateSignatureMutateAsync } = api.challenge.validateSignature.useMutation(
     { retry: false }
   )
 
-  const handleSignIn = async () => {
+  const handleSignIn = async (isNumberAlreadyUsed = false) => {
     setIsSigningIn(true)
     try {
       const challengeData = await getChallengeMutateAsync()
@@ -70,6 +76,15 @@ export const SignUpForm = () => {
 
       router.push(redirectUri ?? '/')
     } catch (error) {
+      if (
+        isNumberAlreadyUsed &&
+        error instanceof DOMException &&
+        error.name === 'NotAllowedError'
+      ) {
+        setShouldShowBackUpPrompt(true)
+        return
+      }
+
       toast.show(formatErrorMessage(error), {
         preset: 'error',
         isUrgent: true,
@@ -80,166 +95,208 @@ export const SignUpForm = () => {
     }
   }
 
-  async function signUpWithPhone({ phone, countrycode }: z.infer<typeof SignUpSchema>) {
-    const { error } = await signInWithOtp
-      .mutateAsync({
+  async function signUpWithPhone(formData: z.infer<typeof SignUpSchema>, bypassPhoneCheck = false) {
+    const { phone, countrycode } = formData
+
+    try {
+      const { status } = await signInWithOtpMutateAsync({
         phone,
         countrycode,
         captchaToken,
-      })
-      .catch((e) => {
-        console.error("Couldn't send OTP", e)
-        if (e instanceof TRPCClientError) {
-          return { error: { message: e.message } }
-        }
-        return { error: { message: e.message } }
+        bypassPhoneCheck,
       })
 
-    if (error) {
+      if (status === AuthStatus.PhoneAlreadyUsed) {
+        await handleSignIn(true)
+      }
+    } catch (error) {
+      console.error("Couldn't send OTP", error)
       const errorMessage = error.message.toLowerCase()
       form.setError('phone', { type: 'custom', message: errorMessage })
     }
   }
 
+  function handleBackUpConfirm() {
+    const formData = form.getValues()
+    void signUpWithPhone(formData, true)
+    setShouldShowBackUpPrompt(false)
+  }
+
+  function handleBackUpDenial() {
+    void handleSignIn(true)
+  }
+
   useEffect(() => () => toast.hide(), [toast])
+
+  const verifyCode = (
+    <VerifyCode
+      phone={`${form.getValues().countrycode}${form.getValues().phone}`}
+      onSuccess={() => {
+        router.push('/')
+      }}
+    />
+  )
+
+  const signUpForm = (
+    <SchemaForm
+      form={form}
+      schema={SignUpSchema}
+      onSubmit={(formData) => signUpWithPhone(formData, false)}
+      defaultValues={{ phone: '', countrycode: '' }}
+      formProps={{
+        flex: 1,
+      }}
+      props={{
+        countrycode: {
+          size: '$3',
+        },
+        phone: {
+          'aria-label': 'Phone number',
+          '$theme-dark': {
+            borderBottomColor: '$green10Dark',
+          },
+          '$theme-light': {
+            borderBottomColor: '$green9Light',
+          },
+          fontFamily: '$mono',
+          fontVariant: ['tabular-nums'],
+          fontSize: '$7',
+          fontWeight: '400',
+          borderWidth: 0,
+          borderBottomWidth: 2,
+          borderRadius: '$0',
+          width: '100%',
+          backgroundColor: 'transparent',
+          color: '$color12',
+          outlineColor: 'transparent',
+          theme: 'green',
+          focusStyle: {
+            borderBottomColor: '$green3Light',
+          },
+          fieldsetProps: {
+            f: 1,
+          },
+        },
+      }}
+      renderAfter={({ submit }) => (
+        <YStack jc="center" ai="center" mt="$4">
+          <XStack
+            f={1}
+            mt={'0'}
+            jc={'space-between'}
+            $sm={{ jc: 'center', height: '100%' }}
+            ai={'center'}
+          >
+            <SubmitButton
+              disabled={!captchaToken}
+              onPress={() => submit()}
+              br="$3"
+              bc={'$green9Light'}
+              $sm={{ w: '100%' }}
+              $gtMd={{
+                mt: '0',
+                als: 'flex-end',
+                mx: 0,
+                ml: 'auto',
+                w: '$10',
+                h: '$3.5',
+              }}
+            >
+              <ButtonText size={'$2'} padding={'unset'} ta="center" margin={'unset'} col="black">
+                {'/SIGN UP'}
+              </ButtonText>
+            </SubmitButton>
+          </XStack>
+          <YStack pos="relative" pb={'$size.1'} w="100%" maw="$size.22">
+            <XStack jc="center" ai="center" mt="$4" gap="$1">
+              <Paragraph size="$2" color="$color11">
+                Already have an account?
+              </Paragraph>
+              <SubmitButton onPress={() => handleSignIn()} disabled={isSigningIn} unstyled>
+                <ButtonText color="$color11" size="$2" textDecorationLine="underline">
+                  {isSigningIn ? 'Signing in...' : 'Sign in'}
+                </ButtonText>
+              </SubmitButton>
+            </XStack>
+          </YStack>
+          <YStack pt="$4">
+            {!!process.env.NEXT_PUBLIC_TURNSTILE_SITE_KEY && (
+              <Turnstile
+                siteKey={process.env.NEXT_PUBLIC_TURNSTILE_SITE_KEY}
+                onSuccess={(token) => {
+                  setCaptchaToken(token)
+                }}
+              />
+            )}
+          </YStack>
+        </YStack>
+      )}
+    >
+      {({ countrycode: CountryCode, phone: Phone }) => (
+        <YStack gap="$5" jc="center" $sm={{ f: 1 }}>
+          <BigHeading color="$color12">CREATE YOUR ACCOUNT</BigHeading>
+          <H3
+            lineHeight={28}
+            $platform-web={{ fontFamily: '$mono' }}
+            $theme-light={{ col: '$gray10Light' }}
+            $theme-dark={{ col: '$olive' }}
+            fontWeight={'300'}
+            $sm={{ size: '$5' }}
+          >
+            Sign up with your phone number.
+          </H3>
+
+          <YStack gap="$4">
+            <Paragraph color="$color12" size={'$1'} fontWeight={'500'}>
+              Your Phone
+            </Paragraph>
+            <XStack gap="$5">
+              {CountryCode}
+              {Phone}
+            </XStack>
+          </YStack>
+        </YStack>
+      )}
+    </SchemaForm>
+  )
+
+  const backUpForm = (
+    <YStack gap="$5" jc="center" $sm={{ f: 1 }} maw={535}>
+      <BigHeading color="$color12">ARE YOU BACKING UP?</BigHeading>
+      <H3
+        lineHeight={28}
+        $platform-web={{ fontFamily: '$mono' }}
+        $theme-light={{ col: '$gray10Light' }}
+        $theme-dark={{ col: '$olive' }}
+        fontWeight={'300'}
+        $sm={{ size: '$5' }}
+      >
+        This number is already associated with an account.
+      </H3>
+      <XStack gap="$4" mt="$7">
+        <SubmitButton size="$4" w="$12" onPress={handleBackUpConfirm}>
+          <ButtonText>YES</ButtonText>
+        </SubmitButton>
+        <Button
+          borderColor="$primary"
+          variant="outlined"
+          size="$4"
+          w="$12"
+          onPress={handleBackUpDenial}
+        >
+          <Button.Text color="$white" $gtMd={{ color: '$color12' }}>
+            NO
+          </Button.Text>
+        </Button>
+      </XStack>
+    </YStack>
+  )
 
   return (
     <FormProvider {...form}>
-      {form.formState.isSubmitSuccessful ? (
-        <VerifyCode
-          phone={`${form.getValues().countrycode}${form.getValues().phone}`}
-          onSuccess={() => {
-            router.push('/')
-          }}
-        />
-      ) : (
-        <SchemaForm
-          form={form}
-          schema={SignUpSchema}
-          onSubmit={signUpWithPhone}
-          defaultValues={{ phone: '', countrycode: '' }}
-          formProps={{
-            flex: 1,
-          }}
-          props={{
-            countrycode: {
-              size: '$3',
-            },
-            phone: {
-              'aria-label': 'Phone number',
-              '$theme-dark': {
-                borderBottomColor: '$green10Dark',
-              },
-              '$theme-light': {
-                borderBottomColor: '$green9Light',
-              },
-              fontFamily: '$mono',
-              fontVariant: ['tabular-nums'],
-              fontSize: '$7',
-              fontWeight: '400',
-              borderWidth: 0,
-              borderBottomWidth: 2,
-              borderRadius: '$0',
-              width: '100%',
-              backgroundColor: 'transparent',
-              color: '$color12',
-              outlineColor: 'transparent',
-              theme: 'green',
-              focusStyle: {
-                borderBottomColor: '$green3Light',
-              },
-              fieldsetProps: {
-                f: 1,
-              },
-            },
-          }}
-          renderAfter={({ submit }) => (
-            <YStack jc="center" ai="center" mt="$4">
-              <XStack
-                f={1}
-                mt={'0'}
-                jc={'space-between'}
-                $sm={{ jc: 'center', height: '100%' }}
-                ai={'center'}
-              >
-                <SubmitButton
-                  disabled={!captchaToken}
-                  onPress={() => submit()}
-                  br="$3"
-                  bc={'$green9Light'}
-                  $sm={{ w: '100%' }}
-                  $gtMd={{
-                    mt: '0',
-                    als: 'flex-end',
-                    mx: 0,
-                    ml: 'auto',
-                    w: '$10',
-                    h: '$3.5',
-                  }}
-                >
-                  <ButtonText
-                    size={'$2'}
-                    padding={'unset'}
-                    ta="center"
-                    margin={'unset'}
-                    col="black"
-                  >
-                    {'/SIGN UP'}
-                  </ButtonText>
-                </SubmitButton>
-              </XStack>
-              <YStack pos="relative" pb={'$size.1'} w="100%" maw="$size.22">
-                <XStack jc="center" ai="center" mt="$4" gap="$1">
-                  <Paragraph size="$2" color="$color11">
-                    Already have an account?
-                  </Paragraph>
-                  <SubmitButton onPress={handleSignIn} disabled={isSigningIn} unstyled>
-                    <ButtonText color="$color11" size="$2" textDecorationLine="underline">
-                      {isSigningIn ? 'Signing in...' : 'Sign in'}
-                    </ButtonText>
-                  </SubmitButton>
-                </XStack>
-              </YStack>
-              <YStack pt="$4">
-                {!!process.env.NEXT_PUBLIC_TURNSTILE_SITE_KEY && (
-                  <Turnstile
-                    siteKey={process.env.NEXT_PUBLIC_TURNSTILE_SITE_KEY}
-                    onSuccess={(token) => {
-                      setCaptchaToken(token)
-                    }}
-                  />
-                )}
-              </YStack>
-            </YStack>
-          )}
-        >
-          {({ countrycode: CountryCode, phone: Phone }) => (
-            <YStack gap="$5" jc="center" $sm={{ f: 1 }}>
-              <BigHeading color="$color12">CREATE YOUR ACCOUNT</BigHeading>
-              <H3
-                lineHeight={28}
-                $platform-web={{ fontFamily: '$mono' }}
-                $theme-light={{ col: '$gray10Light' }}
-                $theme-dark={{ col: '$olive' }}
-                fontWeight={'300'}
-                $sm={{ size: '$5' }}
-              >
-                Sign up with your phone number.
-              </H3>
-
-              <YStack gap="$4">
-                <Paragraph color="$color12" size={'$1'} fontWeight={'500'}>
-                  Your Phone
-                </Paragraph>
-                <XStack gap="$5">
-                  {CountryCode}
-                  {Phone}
-                </XStack>
-              </YStack>
-            </YStack>
-          )}
-        </SchemaForm>
-      )}
+      {!form.formState.isSubmitSuccessful && !shouldShowBackUpPrompt && signUpForm}
+      {form.formState.isSubmitSuccessful && !shouldShowBackUpPrompt && verifyCode}
+      {form.formState.isSubmitSuccessful && shouldShowBackUpPrompt && backUpForm}
     </FormProvider>
   )
 }
