12558f2a06a7e1fd6e64129c47ddb090985a8599
youngkidwarrior <victor@she.energy>
2024-11-15 10:43:28 -0800

    Change theme-color and fix deprecated apple pwa

--- apps/next/pages/_app.tsx
+++ apps/next/pages/_app.tsx
@@ -76,7 +76,7 @@ function MyApp({
           name="viewport"
           content="viewport-fit=cover, user-scalable=no, width=device-width, initial-scale=1, maximum-scale=1"
         />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
@@ -85,7 +85,7 @@ function MyApp({
         <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#122023" />
         <meta name="msapplication-TileColor" content="#122023" />
         <meta name="theme-color" content="#FFFFFF" media="(prefers-color-scheme: light)" />
-        <meta name="theme-color" content="#122023" media="(prefers-color-scheme: dark)" />
+        <meta name="theme-color" content="#081619" media="(prefers-color-scheme: dark)" />
         <meta property="og:image" content="https://ghassets.send.app/2024/04/send-og-image.png" />
       </Head>
       <NextThemeProvider
