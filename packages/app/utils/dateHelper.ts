fa6103bd547f7a911bd95a398e1527f9e933f932
musidlo <musidlo@gmail.com>
2024-12-03 17:14:48 +0100

    Add birthday to personal info

--- packages/app/utils/dateHelper.ts
+++ packages/app/utils/dateHelper.ts
@@ -83,3 +83,11 @@ export const formatDateToLongFormWithoutYear = (date?: Date): string => {
 
   return `${day} ${month}`
 }
+
+export const formatDateToSupabaseFormat = (date: Date): string => {
+  const year = date.getFullYear()
+  const month = String(date.getMonth() + 1).padStart(2, '0')
+  const day = String(date.getDate()).padStart(2, '0')
+
+  return `${year}-${month}-${day}`
+}
