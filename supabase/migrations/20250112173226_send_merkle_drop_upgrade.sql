e6a8607279935196b35a68d9e1d81ef3624452c1
Big Boss <bigboss@metalrodeo.xyz>
2025-01-13 22:07:30 -0600

    fix(supabase): update more tables to use numeric

--- supabase/migrations/20250112173226_send_merkle_drop_upgrade.sql
+++ supabase/migrations/20250112173226_send_merkle_drop_upgrade.sql
@@ -21,6 +21,19 @@ drop view distribution_verifications_summary;
 alter table distribution_verification_values
     alter column fixed_value type numeric;
 
+-- update distribution verifications to use numeric format
+alter table distribution_verifications
+    alter column weight type numeric;
+
+-- update affiliate stats to use numeric format
+alter table affiliate_stats
+    alter column send_plus_minus type numeric;
+
+-- scale affiliate stats
+update affiliate_stats
+set send_plus_minus = send_plus_minus * 1e16
+where send_plus_minus > 0;
+
 -- Recreate the distribution verification summary view
 create view distribution_verifications_summary(distribution_id, user_id, verification_values, multipliers) as
 WITH base_counts AS (SELECT dv.distribution_id,
@@ -57,3 +70,232 @@ alter table distribution_shares
     alter column hodler_pool_amount type numeric,
     alter column bonus_pool_amount type numeric,
     alter column fixed_pool_amount type numeric;
+
+-- update send_ceiling verifications to use numeric format ensuring jsonb type uses text for clientside javascript
+create or replace function insert_verification_send_ceiling() returns trigger
+    language plpgsql
+as
+$$
+DECLARE
+    _user_id             uuid;
+    _recipient_address   citext;
+    _distribution_id     integer;
+    _distribution_number integer;
+    _send_ceiling        numeric;
+    _verification_exists boolean;
+BEGIN
+    -- Get the sender's user_id
+    SELECT send_accounts.user_id
+    INTO _user_id
+    FROM send_accounts
+    WHERE address = concat('0x', encode(NEW.f, 'hex'))::citext;
+    -- Get recipient address
+    _recipient_address := concat('0x', encode(NEW.t, 'hex'))::citext;
+    -- Get the active distribution id and number
+    SELECT d.id,
+           d.number
+    INTO _distribution_id,
+        _distribution_number
+    FROM distributions d
+    WHERE extract(epoch FROM d.qualification_start) <= NEW.block_time
+      AND extract(epoch FROM d.qualification_end) > NEW.block_time;
+    -- If we found matching distribution and user
+    IF _user_id IS NOT NULL AND _distribution_id IS NOT NULL THEN
+        -- Check if verification exists
+        SELECT EXISTS (SELECT 1
+                       FROM distribution_verifications dv
+                       WHERE dv.user_id = _user_id
+                         AND distribution_id = _distribution_id
+                         AND type = 'send_ceiling')
+        INTO _verification_exists;
+        IF NOT _verification_exists THEN
+            -- Calculate send ceiling
+            WITH send_settings AS (SELECT minimum_sends * scaling_divisor AS divider
+                                   FROM send_slash s_s
+                                            JOIN distributions d ON d.id = s_s.distribution_id
+                                   WHERE d.number = _distribution_number),
+                 previous_distribution AS (SELECT ds.user_id,
+                                                  ds.amount_after_slash AS user_prev_shares
+                                           FROM distribution_shares ds
+                                           WHERE ds.distribution_id = (SELECT id
+                                                                       FROM distributions
+                                                                       WHERE number = _distribution_number - 1)
+                                             AND ds.user_id = _user_id),
+                 distribution_info AS (SELECT hodler_min_balance
+                                       FROM distributions
+                                       WHERE id = _distribution_id)
+            SELECT ROUND(COALESCE(pd.user_prev_shares, di.hodler_min_balance) / ss.divider)::numeric
+            INTO _send_ceiling
+            FROM distribution_info di
+                     CROSS JOIN send_settings ss
+                     LEFT JOIN previous_distribution pd ON pd.user_id = _user_id;
+            -- Create new verification
+            INSERT INTO distribution_verifications(distribution_id,
+                                                   user_id,
+                                                   type,
+                                                   weight,
+                                                   metadata)
+            VALUES (_distribution_id,
+                    _user_id,
+                    'send_ceiling',
+                    LEAST(
+                            NEW.v, _send_ceiling),
+                            -- Cast to text to avoid overflow errors on client
+                    jsonb_build_object('value', _send_ceiling::text, 'sent_to', ARRAY [_recipient_address] ::citext[]));
+        ELSE
+            -- Get the send ceiling for existing verification
+            SELECT (metadata ->> 'value')::numeric
+            INTO _send_ceiling
+            FROM distribution_verifications dv
+            WHERE dv.user_id = _user_id
+              AND distribution_id = _distribution_id
+              AND type = 'send_ceiling';
+            -- Update existing verification, only if this recipient hasn't been counted before
+            UPDATE
+                distribution_verifications dv
+            SET
+                -- Cast to text to avoid overflow errors on client
+                metadata = jsonb_build_object('value', _send_ceiling::text, 'sent_to',
+                                              metadata -> 'sent_to' || jsonb_build_array(_recipient_address)),
+                weight   = weight + LEAST(NEW.v, _send_ceiling)
+            WHERE dv.user_id = _user_id
+              AND distribution_id = _distribution_id
+              AND type = 'send_ceiling';
+        END IF;
+    END IF;
+    RETURN NEW;
+END;
+$$;
+
+create or replace function calculate_and_insert_send_ceiling_verification(distribution_number integer) returns void
+    security definer
+    language plpgsql
+as
+$$
+BEGIN
+    -- Step 1: Get qualifying sends first
+    CREATE TEMPORARY TABLE all_qualifying_sends AS
+    SELECT
+        *
+    FROM
+        sum_qualification_sends($1);
+    CREATE TEMPORARY TABLE send_ceiling_settings AS
+    WITH send_settings AS(
+        SELECT
+            minimum_sends * scaling_divisor AS divider
+        FROM
+            send_slash s_s
+                JOIN distributions d ON d.id = s_s.distribution_id
+        WHERE
+            d.number = $1
+    ),
+         previous_distribution AS(
+             SELECT
+                 ds.user_id,
+                 ds.amount_after_slash AS user_prev_shares
+             FROM
+                 distribution_shares ds
+             WHERE
+                 ds.distribution_id =(
+                     SELECT
+                         id
+                     FROM
+                         distributions d
+                     WHERE
+                         d.number = $1 - 1))
+    SELECT
+        qs.user_id,
+        ROUND(COALESCE(pd.user_prev_shares, d.hodler_min_balance) /(
+            SELECT
+                minimum_sends * scaling_divisor
+            FROM send_slash s_s
+            WHERE
+                s_s.distribution_id =(
+                    SELECT
+                        id
+                    FROM distributions
+                    WHERE
+                        number = $1)))::numeric AS send_ceiling
+    FROM( SELECT DISTINCT
+              user_id
+          FROM
+              all_qualifying_sends) qs
+            CROSS JOIN(
+        SELECT
+            hodler_min_balance
+        FROM
+            distributions
+        WHERE
+            number = $1) d
+            LEFT JOIN previous_distribution pd ON pd.user_id = qs.user_id;
+    -- Step 2: Update existing verifications
+    UPDATE
+        distribution_verifications dv
+    SET
+        weight = qs.amount,
+        -- Cast to text to avoid overflow errors on client
+        metadata = jsonb_build_object('value', scs.send_ceiling::text, 'sent_to', qs.sent_to)
+    FROM
+        send_ceiling_settings scs
+            JOIN all_qualifying_sends qs ON qs.user_id = scs.user_id
+    WHERE
+        dv.user_id = qs.user_id
+      AND dv.distribution_id =(
+        SELECT
+            id
+        FROM
+            distributions
+        WHERE
+            number = $1)
+      AND dv.type = 'send_ceiling'
+      AND COALESCE(qs.amount, 0) > 0;
+    -- Step 3: Insert new verifications
+    INSERT INTO distribution_verifications(
+        distribution_id,
+        user_id,
+        type,
+        weight,
+        metadata)
+    SELECT
+        (
+            SELECT
+                id
+            FROM
+                distributions d
+            WHERE
+                d.number = $1),
+                qs.user_id,
+                'send_ceiling'::public.verification_type,
+                qs.amount,
+                -- Cast to text to avoid overflow errors on client
+                jsonb_build_object('value', scs.send_ceiling::text, 'sent_to', qs.sent_to)
+    FROM
+        send_ceiling_settings scs
+            JOIN all_qualifying_sends qs ON qs.user_id = scs.user_id
+    WHERE
+        COALESCE(qs.amount, 0) > 0
+      AND NOT EXISTS(
+        SELECT
+            1
+        FROM
+            distribution_verifications dv
+        WHERE
+            dv.user_id = qs.user_id
+          AND dv.distribution_id =(
+            SELECT
+                id
+            FROM
+                distributions
+            WHERE
+                number = $1)
+          AND dv.type = 'send_ceiling');
+    -- Cleanup temporary tables
+    DROP TABLE IF EXISTS send_ceiling_settings;
+    DROP TABLE IF EXISTS all_qualifying_sends;
+END;
+$$;
+
+alter function calculate_and_insert_send_ceiling_verification(integer) owner to postgres;
+revoke all on function calculate_and_insert_send_ceiling_verification(integer) from public;
+revoke all on function calculate_and_insert_send_ceiling_verification(integer) from anon;
+revoke all on function calculate_and_insert_send_ceiling_verification(integer) from authenticated;
