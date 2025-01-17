5a1a88157bddc1a8affcd209c947e96e73713bcd
Big Boss <bigboss@metalrodeo.xyz>
2025-01-17 16:27:50 -0600

    fix: send token v0 upgrade

--- packages/playwright/tests/send-token-upgrade.onboarded.spec.ts
+++ packages/playwright/tests/send-token-upgrade.onboarded.spec.ts
@@ -1,19 +1,10 @@
 import { expect, test as sendAccountTest } from '@my/playwright/fixtures/send-accounts'
 import { test as snapletTest } from '@my/playwright/fixtures/snaplet'
-import { userOnboarded } from '@my/snaplet/models'
-import type { Database } from '@my/supabase/database.types'
-import { mergeTests, type Page } from '@playwright/test'
-import type { SupabaseClient } from '@supabase/supabase-js'
-import { coins, sendCoin, sendV0Coin, type coin } from 'app/data/coins'
-import { assert } from 'app/utils/assert'
-import { hexToBytea } from 'app/utils/hexToBytea'
-import { shorten } from 'app/utils/strings'
+import { mergeTests } from '@playwright/test'
+import { sendCoin, sendV0Coin } from 'app/data/coins'
 import { setERC20Balance } from 'app/utils/useSetErc20Balance'
 import debug from 'debug'
-import { isAddress, parseUnits } from 'viem'
-import { generatePrivateKey, privateKeyToAccount } from 'viem/accounts'
-import { ProfilePage } from './fixtures/profiles'
-import { SendPage } from './fixtures/send'
+import { parseUnits } from 'viem'
 import { lookupBalance, testBaseClient } from './fixtures/viem'
 
 const test = mergeTests(sendAccountTest, snapletTest)
@@ -24,12 +15,7 @@ test.beforeEach(async ({ user: { profile }, supabase }) => {
   log = debug(`test:send:${profile.id}:${test.info().parallelIndex}`)
 })
 
-test('can upgrade their Send Token V0 to Send Token V1', async ({
-  page,
-  seed,
-  supabase,
-  sendAccount,
-}) => {
+test('can upgrade their Send Token V0 to Send Token V1', async ({ page, sendAccount }) => {
   const balance = 100_000_000n
 
   // set a Send Token V0 balance
@@ -51,11 +37,18 @@ test('can upgrade their Send Token V0 to Send Token V1', async ({
   await upgradeButton.click()
 
   // wait until the upgrade is complete
-  await expect(() =>
-    page
+  await expect(async () => {
+    await page
       .getByRole('button', { name: 'UPGRADING...' })
       .waitFor({ state: 'detached', timeout: 2_000 })
-  ).toPass({
+
+    // send token v0 balance should be 0
+    const sendTokenV0Balance = await lookupBalance({
+      address: sendAccount.address as `0x${string}`,
+      token: sendV0Coin.token,
+    })
+    expect(sendTokenV0Balance).toEqual(0n)
+  }).toPass({
     timeout: 10_000,
   })
 
@@ -65,11 +58,4 @@ test('can upgrade their Send Token V0 to Send Token V1', async ({
     token: sendCoin.token,
   })
   expect(sendTokenV1Balance).toEqual(parseUnits((balance / 100n).toString(), sendCoin.decimals))
-
-  // send token v0 balance should be 0
-  const sendTokenV0Balance = await lookupBalance({
-    address: sendAccount.address as `0x${string}`,
-    token: sendV0Coin.token,
-  })
-  expect(sendTokenV0Balance).toEqual(0n)
 })
