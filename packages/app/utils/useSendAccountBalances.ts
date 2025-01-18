c6662c3690a3d9e9aebc301955b1492876ba0682
Big Boss <bigboss@metalrodeo.xyz>
2025-01-18 17:33:16 -0600

    chore(app): drop default useQuery params

--- packages/app/utils/useSendAccountBalances.ts
+++ packages/app/utils/useSendAccountBalances.ts
@@ -40,11 +40,7 @@ export const useSendAccountBalances = () => {
   const tokensQuery = useReadContracts({
     query: {
       enabled: !!sendAccount,
-      refetchOnWindowFocus: true,
-      refetchOnReconnect: true,
-      refetchOnMount: true,
       refetchInterval: 10 * 1000,
-      staleTime: 10 * 1000,
     },
     contracts: tokenContracts,
     multicallAddress: multicall3Address[baseMainnet.id],
