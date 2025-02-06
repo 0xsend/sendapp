2858b46ee76d56ac2d5bdea55e399245463d7fcf
youngkidwarrior <victor@she.energy>
2025-02-06 11:40:14 -0800

    Adjust memoized safe area insets

--- packages/ui/src/utils/useSafeAreaInsets.ts
+++ packages/ui/src/utils/useSafeAreaInsets.ts
@@ -1,37 +1,24 @@
-import { useMemo } from 'react'
+import { useLayoutEffect, useState } from 'react'
 
 const sanitizeSafeAreaInset = (value: string) => {
   const sanitizedInset = value.endsWith('px') ? Number(value.slice(0, -2)) : Number(value)
   return Number.isNaN(sanitizedInset) ? 0 : sanitizedInset
 }
 
-// The values will only be recalculated if the component is unmounted and remounted
 export const useSafeAreaInsets = () => {
-  // @todo: SSR breaks insets
-
   if (typeof window === 'undefined') return { sat: 0, sar: 0, sab: 0, sal: 0 }
-  return useMemo(() => {
-    const sat = sanitizeSafeAreaInset(
-      getComputedStyle(document.documentElement).getPropertyValue('--sat')
-    )
-    console.log(getComputedStyle(document.documentElement).getPropertyValue('--sat'))
 
-    const sab = sanitizeSafeAreaInset(
-      getComputedStyle(document.documentElement).getPropertyValue('--sab')
-    )
+  const [insets, setInsets] = useState({ sat: 0, sar: 0, sab: 0, sal: 0 })
 
-    const sar = sanitizeSafeAreaInset(
-      getComputedStyle(document.documentElement).getPropertyValue('--sar')
-    )
-    const sal = sanitizeSafeAreaInset(
-      getComputedStyle(document.documentElement).getPropertyValue('--sal')
-    )
+  useLayoutEffect(() => {
+    const styles = getComputedStyle(document.documentElement)
+    const sat = sanitizeSafeAreaInset(styles.getPropertyValue('--sat')) || 24
+    const sab = sanitizeSafeAreaInset(styles.getPropertyValue('--sab')) || 40
+    const sar = sanitizeSafeAreaInset(styles.getPropertyValue('--sar'))
+    const sal = sanitizeSafeAreaInset(styles.getPropertyValue('--sal'))
 
-    return {
-      sat: !sat ? 24 : sat,
-      sab: !sab ? 40 : sab,
-      sar,
-      sal,
-    }
+    setInsets({ sat, sab, sar, sal })
   }, [])
+
+  return insets
 }
