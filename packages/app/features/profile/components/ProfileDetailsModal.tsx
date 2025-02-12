6ad74b31cf6acec00c39e7b0d7fb30a0bce315a0
musidlo <musidlo@gmail.com>
2025-02-12 13:02:47 +0100

    Added loader for profile modal

--- packages/app/features/profile/components/ProfileDetailsModal.tsx
+++ packages/app/features/profile/components/ProfileDetailsModal.tsx
@@ -1,4 +1,4 @@
-import { isWeb, YStack } from '@my/ui'
+import { isWeb, Spinner, YStack } from '@my/ui'
 import { ProfileAboutTile } from 'app/features/profile/components/ProfileAboutTile'
 import { useRootScreenParams } from 'app/routers/params'
 import { useProfileLookup } from 'app/utils/useProfileLookup'
@@ -7,35 +7,38 @@ import { useRouter } from 'solito/router'
 export const ProfilesDetailsModal = () => {
   const { back } = useRouter()
   const [{ profile: profileParam }] = useRootScreenParams()
-  const { data: profile } = useProfileLookup('sendid', profileParam || '')
+  const { data: profile, isLoading } = useProfileLookup('sendid', profileParam || '')
 
-  if (!profile || profileParam === undefined) {
-    return null
+  switch (true) {
+    case isLoading:
+      return <Spinner size="large" />
+    case !profile || profileParam === undefined:
+      return null
+    default:
+      return (
+        <YStack
+          w={'100%'}
+          ai={'center'}
+          $gtLg={{
+            width: '35%',
+            minWidth: 400,
+            height: isWeb ? '81vh' : 'auto',
+            // @ts-expect-error typescript is complaining about overflowY not available and advising overflow. Overflow will work differently than overflowY here, overflowY is working fine
+            overflowY: 'scroll',
+          }}
+          className={'hide-scroll'}
+        >
+          <YStack
+            w={'100%'}
+            maxWidth={500}
+            pb={'$10'}
+            $gtLg={{
+              pb: 0,
+            }}
+          >
+            <ProfileAboutTile profile={profile} onClose={() => back()} />
+          </YStack>
+        </YStack>
+      )
   }
-
-  return (
-    <YStack
-      w={'100%'}
-      ai={'center'}
-      $gtLg={{
-        width: '35%',
-        minWidth: 400,
-        height: isWeb ? '81vh' : 'auto',
-        // @ts-expect-error typescript is complaining about overflowY not available and advising overflow. Overflow will work differently than overflowY here, overflowY is working fine
-        overflowY: 'scroll',
-      }}
-      className={'hide-scroll'}
-    >
-      <YStack
-        w={'100%'}
-        maxWidth={500}
-        pb={'$10'}
-        $gtLg={{
-          pb: 0,
-        }}
-      >
-        <ProfileAboutTile profile={profile} onClose={() => back()} />
-      </YStack>
-    </YStack>
-  )
 }
