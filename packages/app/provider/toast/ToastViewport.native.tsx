e7277d24feab798793f2ab0b7eb2227332046769
youngkidwarrior <victor@she.energy>
2025-02-17 01:57:58 -0800

    Refactor ToastViewport with new useSafeAreaInsets

--- packages/app/provider/toast/ToastViewport.native.tsx
+++ packages/app/provider/toast/ToastViewport.native.tsx
@@ -3,4 +3,9 @@ import type { ToastViewportProps } from './ToastViewport'
 
 export const ToastViewport = ({ noSafeArea }: ToastViewportProps) => {
   const insets = useSafeAreaInsets()
+  if (!insets || noSafeArea) {
+    return <ToastViewportOg top={0} left={0} right={0} />
+  }
+
+  return <ToastViewportOg top={insets.top + 5} left={insets.left + 5} right={insets.right + 5} />
 }
