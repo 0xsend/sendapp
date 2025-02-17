614ecf11515788f57b0bf3acc9dbfc15dd5038fd
crisog <ortega.cpp@gmail.com>
2025-02-16 20:38:00 -0400

    refactor(app): extract deposit success components

--- apps/next/pages/deposit/success/callback.tsx
+++ apps/next/pages/deposit/success/callback.tsx
@@ -1,17 +1,5 @@
-import { useEffect } from 'react'
-import { YStack, Spinner } from '@my/ui'
+import { SuccessCallback } from 'app/features/deposit/components/SuccessCallback'
 
 export default function PopupCallback() {
-  useEffect(() => {
-    if (window.opener) {
-      window.opener.postMessage({ type: 'COINBASE_ONRAMP_SUCCESS' }, window.location.origin)
-      window.close()
-    }
-  }, [])
-
-  return (
-    <YStack f={1} ai="center" jc="center">
-      <Spinner size="large" color="$primary" />
-    </YStack>
-  )
+  return <SuccessCallback />
 }
