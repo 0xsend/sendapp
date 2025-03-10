e41c3ea48885ca618b3688f84ea46e53202d0134
youngkidwarrior <victor@she.energy>
2025-03-10 10:41:50 -0700

    Break up init activity to improve retryability

--- packages/workflows/src/transfer-workflow/workflow.ts
+++ packages/workflows/src/transfer-workflow/workflow.ts
@@ -8,8 +8,10 @@ import { hexToBytea } from 'app/utils/hexToBytea'
 const log = debug('workflows:transfer')
 
 const {
-  initializeTransferActivity,
-  insertTemporalSendAccountTransfer,
+  simulateTransferActivity,
+  getBaseBlockNumberActivity,
+  decodeTransferUserOpActivity,
+  insertTemporalSendAccountTransferActivity,
   sendUserOpActivity,
   updateTemporalTransferActivity,
   waitForTransactionReceiptActivity,
@@ -20,11 +22,23 @@ const {
 
 export async function TransferWorkflow(userOp: UserOperation<'v0.7'>) {
   const workflowId = workflowInfo().workflowId
-  log('SendTransferWorkflow Initializing with userOp:', workflowId)
-  const { token, from, to, amount, blockNumber } = await initializeTransferActivity(userOp)
+  log('Starting SendTransfer Workflow with userOp:', workflowId)
+
+  log('Simulating transfer', workflowId)
+  await simulateTransferActivity(userOp)
+  log('Successfully simulated transfer', workflowId)
+
+  log('Getting latest base block', workflowId)
+  const blockNumber = await getBaseBlockNumberActivity()
+  log('Base block:', { workflowId, blockNumber: blockNumber.toString() })
+
+  log('Decoding transfer userOp', workflowId)
+  const { token, from, to, amount } = await decodeTransferUserOpActivity(userOp)
+  log('Decoded transfer userOp', { workflowId, token, from, to, amount: amount.toString() })
 
   log('Inserting temporal transfer into temporal.send_account_transfers', workflowId)
-  await insertTemporalSendAccountTransfer(workflowId, from, to, amount, token, blockNumber)
+  await insertTemporalSendAccountTransferActivity(workflowId, from, to, amount, token, blockNumber)
+  log('Inserted temporal transfer into temporal.send_account_transfers', workflowId)
 
   log('Sending UserOperation', superjson.stringify(userOp))
   const hash = await sendUserOpActivity(workflowId, userOp)
