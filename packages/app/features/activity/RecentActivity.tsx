ce7f89665a58cc5a1d001508bfd4fcad29729e28
beezy <beezy@barg.systems>
2025-03-12 09:42:24 -0400

    Fix activity home feed layout, there was annoying bug where inner items in list view were not aligned with the main indentation of the first and last item causing a staggered appearance

--- packages/app/features/activity/RecentActivity.tsx
+++ packages/app/features/activity/RecentActivity.tsx
@@ -130,6 +130,8 @@ function ActivityFeed({
       sections={sections}
       testID={'RecentActivity'}
       showsVerticalScrollIndicator={false}
+      contentContainerStyle={{ width: '100%' }}
+      style={{ width: '100%' }}
       keyExtractor={(activity) =>
         `${activity.event_name}-${activity.created_at}-${activity?.from_user?.id}-${activity?.to_user?.id}`
       }
