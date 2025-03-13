b16e828e1044c30ba836f21a0d374c86501e1495
youngkidwarrior <victor@she.energy>
2025-03-13 15:18:59 -0700

    Clean up decodeTransferUserOp

--- packages/app/utils/decodeTransferUserOp.ts
+++ packages/app/utils/decodeTransferUserOp.ts
@@ -1,23 +1,31 @@
-import { decodeFunctionData } from 'viem'
+import { decodeFunctionData, isAddress } from 'viem'
 import { sendAccountAbi, erc20Abi } from '@my/wagmi'
 import type { UserOperation } from 'permissionless'
-import type { allCoinsDict } from 'app/data/coins'
+import { allCoinsDict } from 'app/data/coins'
 
 export function decodeTransferUserOp({ userOp }: { userOp: UserOperation<'v0.7'> }) {
   const { args } = decodeFunctionData({ abi: sendAccountAbi, data: userOp.callData })
 
+  const from = isAddress(userOp.sender) ? userOp.sender : null
+
   const decodedTokenTransfer =
     args?.[0]?.[0].data !== '0x'
       ? decodeFunctionData({ abi: erc20Abi, data: args?.[0]?.[0].data })
-      : undefined
+      : null
 
-  const amount = (
-    decodedTokenTransfer ? decodedTokenTransfer.args[1] : args?.[0]?.[0].value
-  ) as bigint
+  const rawAmount = decodedTokenTransfer ? decodedTokenTransfer.args?.[1] : args?.[0]?.[0].value
+  const amount = typeof rawAmount === 'bigint' ? rawAmount : null
 
-  const to = (
-    decodedTokenTransfer ? decodedTokenTransfer.args[0] : args?.[0]?.[0].dest
-  ) as `0x${string}`
-  const token = (decodedTokenTransfer ? args?.[0]?.[0].dest : 'eth') as keyof allCoinsDict
-  return { from: userOp.sender, to, token, amount }
+  const rawTo = decodedTokenTransfer ? decodedTokenTransfer.args[0] : args?.[0]?.[0].dest
+  const to = isAddress(rawTo) ? rawTo : null
+
+  const rawToken = decodedTokenTransfer ? args?.[0]?.[0].dest : 'eth'
+  const token = rawToken in allCoinsDict || rawToken === 'eth' ? rawToken : null
+
+  return {
+    from,
+    to,
+    token,
+    amount,
+  }
 }
