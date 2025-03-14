fb77771f98ddd43c65acbd3014cfd45a58d09c3f
musidlo <musidlo@gmail.com>
2025-03-14 10:49:35 +0100

    Added menu entry for swaps

--- packages/app/components/sidebar/HomeSideBar.tsx
+++ packages/app/components/sidebar/HomeSideBar.tsx
@@ -19,6 +19,7 @@ import {
   IconDeviceReset,
   IconHome,
   IconSendLogo,
+  IconSwap,
 } from 'app/components/icons'
 import { SideBarNavLink } from 'app/components/sidebar/SideBarNavLink'
 
@@ -39,6 +40,11 @@ const links = [
     icon: <IconActivity size={'$1'} color={'inherit'} />,
     text: 'Send',
     href: '/send',
+  },
+  {
+    icon: <IconSwap size={'$1'} color={'inherit'} />,
+    text: 'Swap',
+    href: '/swap',
   },
   {
     icon: <IconDeviceReset size={'$1'} color={'inherit'} />,
