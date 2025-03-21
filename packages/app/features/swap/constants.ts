eff575e6723d0db583e7f5b4a00de5184775ab9f
musidlo <musidlo@gmail.com>
2025-03-21 22:41:29 +0100

    Changed the way activities recognise swap transfers. Finished unit tests. Clean up some files

--- packages/app/features/swap/constants.ts
+++ packages/app/features/swap/constants.ts
@@ -1,3 +1,2 @@
 export const SWAP_ROUTE_SUMMARY_QUERY_KEY = 'swapRouteSummary'
 export const DEFAULT_SLIPPAGE = 50
-export const SWAP_ENABLED_USERS = (process.env.NEXT_PUBLIC_SWAP_ALLOWLIST ?? '').split(',')
