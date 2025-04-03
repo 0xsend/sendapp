4e082361bccbb7fbb15382e34bd72ef0f012b1cd
youngkidwarrior <victor@she.energy>
2025-04-03 02:42:18 -0700

    Notes input form and confirm screen

--- packages/app/features/send/SendAmountForm.tsx
+++ packages/app/features/send/SendAmountForm.tsx
@@ -1,25 +1,39 @@
-import { Button, Paragraph, Spinner, Stack, SubmitButton, XStack, YStack } from '@my/ui'
+import {
+  Button,
+  Paragraph,
+  Spinner,
+  Stack,
+  SubmitButton,
+  type TamaguiElement,
+  useDebounce,
+  XStack,
+  YStack,
+} from '@my/ui'
 import { type allCoins, allCoinsDict } from 'app/data/coins'
 import { useRootScreenParams, useSendScreenParams } from 'app/routers/params'
 import { formFields, SchemaForm } from 'app/utils/SchemaForm'
 import formatAmount, { localizeAmount, sanitizeAmount } from 'app/utils/formatAmount'
 
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
 import { FormProvider, useForm } from 'react-hook-form'
 import { useRouter } from 'solito/router'
 import { formatUnits } from 'viem'
 
-import { z } from 'zod'
+import { type BRAND, z } from 'zod'
 
 import { useCoinFromSendTokenParam } from 'app/utils/useCoinFromTokenParam'
 import { useCoins } from 'app/provider/coins'
 import { useProfileLookup } from 'app/utils/useProfileLookup'
 import { ProfileHeader } from 'app/features/profile/components/ProfileHeader'
 import { ProfilesDetailsModal } from 'app/features/profile/components/ProfileDetailsModal'
+import { IconX } from 'app/components/icons'
+import { useThemeSetting } from 'app/provider/theme'
+import { MAX_NOTE_LENGTH } from 'app/components/FormFields/NoteField'
 
 const SendAmountSchema = z.object({
   amount: formFields.text,
   token: formFields.coin,
+  note: formFields.note,
 })
 
 export function SendAmountForm() {
@@ -31,34 +45,61 @@ export function SendAmountForm() {
   const { recipient, idType } = sendParams
   const { data: profile } = useProfileLookup(idType ?? 'tag', recipient ?? '')
   const [{ profile: profileParam }] = useRootScreenParams()
-  const [isInputFocused, setIsInputFocused] = useState<boolean>(false)
+
+  const [isAmountInputFocused, setIsAmountInputFocused] = useState<boolean>(false)
+  const [isNoteInputFocused, setIsNoteInputFocused] = useState<boolean>(false)
+  const noteFieldRef = useRef<TamaguiElement>(null)
+  const { resolvedTheme } = useThemeSetting()
+
+  const formNote = form.watch('note')
+
+  const onFormChange = useDebounce(
+    useCallback(
+      (values) => {
+        const { amount, token: _token, note } = values
+        const token = _token as allCoins[number]['token']
+        const sanitizedAmount = sanitizeAmount(amount, allCoinsDict[token]?.decimals)
+
+        setSendParams(
+          {
+            ...sendParams,
+            amount: sanitizedAmount.toString(),
+            sendToken: token,
+            note: encodeURIComponent(note.trim()),
+          },
+          { webBehavior: 'replace' }
+        )
+      },
+      [setSendParams, sendParams]
+    ),
+    300,
+    { leading: false },
+    []
+  )
 
   useEffect(() => {
-    const subscription = form.watch(({ amount, token: _token }) => {
-      const token = _token as allCoins[number]['token']
-      // use allCoinsDict because form updates before query params. This feels hacky
-      const sanitizedAmount = sanitizeAmount(amount, allCoinsDict[token]?.decimals)
-      setSendParams(
-        {
-          ...sendParams,
-          amount: sanitizedAmount.toString(),
-          sendToken: token,
-        },
-        { webBehavior: 'replace' }
-      )
+    const subscription = form.watch((values) => {
+      onFormChange(values)
     })
-    return () => subscription.unsubscribe()
-  }, [form, setSendParams, sendParams])
+
+    return () => {
+      subscription.unsubscribe()
+      onFormChange.cancel()
+    }
+  }, [form.watch, onFormChange])
 
   const parsedAmount = BigInt(sendParams.amount ?? '0')
   const formAmount = form.watch('amount')
+
+  const isNoteTooLong = (formNote?.length ?? 0) > MAX_NOTE_LENGTH
 
   const canSubmit =
     !isLoadingCoins &&
     coin?.balance !== undefined &&
     sendParams.amount !== undefined &&
     coin.balance >= parsedAmount &&
-    parsedAmount > BigInt(0)
+    parsedAmount > BigInt(0) &&
+    !isNoteTooLong
 
   const insufficientAmount =
     coin?.balance !== undefined && sendParams.amount !== undefined && parsedAmount > coin?.balance
@@ -73,15 +114,26 @@ export function SendAmountForm() {
         recipient: sendParams.recipient,
         amount: sendParams.amount,
         sendToken: coin?.token,
+        note: sendParams.note,
       },
     })
   }
+
+  const handleNoteClearClick = () => {
+    form.setValue('note', '' as string & BRAND<'note'>)
+  }
+
+  const noteBorderActiveColor = isNoteTooLong
+    ? '$error'
+    : resolvedTheme?.startsWith('dark')
+      ? '$primary'
+      : '$color12'
 
   return (
     <XStack w={'100%'} gap={'$4'}>
       <YStack
         f={1}
-        gap={'$4'}
+        gap={'$5'}
         display={profileParam ? 'none' : 'flex'}
         $gtLg={{
           display: 'flex',
@@ -140,8 +192,8 @@ export function SendAmountForm() {
                   const localizedAmount = localizeAmount(amount)
                   form.setValue('amount', localizedAmount)
                 },
-                onFocus: () => setIsInputFocused(true),
-                onBlur: () => setIsInputFocused(false),
+                onFocus: () => setIsAmountInputFocused(true),
+                onBlur: () => setIsAmountInputFocused(false),
                 fieldsetProps: {
                   width: '60%',
                 },
@@ -149,6 +201,47 @@ export function SendAmountForm() {
               },
               token: {
                 defaultValue: coin?.token,
+              },
+              note: {
+                ref: noteFieldRef,
+                py: '$4',
+                px: '$5',
+                pr: '$10',
+                fontSize: '$5',
+                fontStyle: 'normal',
+                minHeight: 40,
+                '$theme-dark': {
+                  placeholderTextColor: '$silverChalice',
+                },
+                '$theme-light': {
+                  placeholderTextColor: '$darkGrayTextField',
+                },
+                focusStyle: {
+                  boc: noteBorderActiveColor,
+                  bw: 1,
+                  outlineWidth: 0,
+                  fontStyle: 'normal',
+                },
+                hoverStyle: {
+                  boc: noteBorderActiveColor,
+                },
+                iconAfter: (
+                  <Button
+                    chromeless
+                    unstyled
+                    cursor={'pointer'}
+                    icon={
+                      <IconX
+                        color={'$primary'}
+                        $theme-light={{ color: '$darkGrayTextField' }}
+                        size="$1"
+                      />
+                    }
+                    onPress={handleNoteClearClick}
+                  />
+                ),
+                onFocus: () => setIsNoteInputFocused(true),
+                onBlur: () => setIsNoteInputFocused(false),
               },
             }}
             formProps={{
@@ -165,6 +258,7 @@ export function SendAmountForm() {
                 sendParams.amount && coin !== undefined
                   ? localizeAmount(formatUnits(BigInt(sendParams.amount), coin.decimals))
                   : undefined,
+              note: sendParams.note ? decodeURIComponent(sendParams.note) : '',
             }}
             renderAfter={({ submit }) => (
               <SubmitButton
@@ -179,80 +273,89 @@ export function SendAmountForm() {
               </SubmitButton>
             )}
           >
-            {({ amount, token }) => (
-              <YStack
-                gap="$5"
-                $gtSm={{ p: '$7' }}
-                bg={'$color1'}
-                br={'$6'}
-                p={'$5'}
-                borderColor={insufficientAmount ? '$error' : 'transparent'}
-                bw={1}
-              >
-                <XStack ai={'center'} position="relative" jc={'space-between'}>
-                  {amount}
-                  {token}
-                  <XStack
-                    position="absolute"
-                    bottom={-8}
-                    left={0}
-                    right={0}
-                    height={1}
-                    backgroundColor={isInputFocused ? '$primary' : '$silverChalice'}
-                    $theme-light={{
-                      backgroundColor: isInputFocused ? '$color12' : '$silverChalice',
-                    }}
-                  />
-                </XStack>
-                <XStack jc="space-between" ai={'flex-start'}>
-                  <Stack>
-                    {(() => {
-                      switch (true) {
-                        case isLoadingCoins:
-                          return <Spinner size="small" />
-                        case !coin?.balance:
-                          return null
-                        default:
-                          return (
-                            <XStack
-                              gap={'$2'}
-                              flexDirection={'column'}
-                              $gtSm={{ flexDirection: 'row' }}
-                            >
-                              <XStack gap={'$2'}>
-                                <Paragraph
-                                  testID="SendFormBalance"
-                                  color={insufficientAmount ? '$error' : '$silverChalice'}
-                                  size={'$5'}
-                                  $theme-light={{
-                                    color: insufficientAmount ? '$error' : '$darkGrayTextField',
-                                  }}
-                                >
-                                  Balance:
-                                </Paragraph>
-                                <Paragraph
-                                  color={insufficientAmount ? '$error' : '$color12'}
-                                  size={'$5'}
-                                  fontWeight={'600'}
-                                >
-                                  {formatAmount(
-                                    formatUnits(coin.balance, coin.decimals),
-                                    12,
-                                    coin.formatDecimals
-                                  )}
-                                </Paragraph>
+            {({ amount, token, note }) => (
+              <YStack gap="$5">
+                <YStack
+                  gap="$5"
+                  $gtSm={{ p: '$7' }}
+                  bg={'$color1'}
+                  br={'$6'}
+                  p={'$5'}
+                  borderColor={insufficientAmount ? '$error' : 'transparent'}
+                  bw={1}
+                >
+                  <XStack ai={'center'} position="relative" jc={'space-between'}>
+                    {amount}
+                    {token}
+                    <XStack
+                      position="absolute"
+                      bottom={-8}
+                      left={0}
+                      right={0}
+                      height={1}
+                      backgroundColor={isAmountInputFocused ? '$primary' : '$silverChalice'}
+                      $theme-light={{
+                        backgroundColor: isAmountInputFocused ? '$color12' : '$silverChalice',
+                      }}
+                    />
+                  </XStack>
+                  <XStack jc="space-between" ai={'flex-start'}>
+                    <Stack>
+                      {(() => {
+                        switch (true) {
+                          case isLoadingCoins:
+                            return <Spinner size="small" />
+                          case !coin?.balance:
+                            return null
+                          default:
+                            return (
+                              <XStack
+                                gap={'$2'}
+                                flexDirection={'column'}
+                                $gtSm={{ flexDirection: 'row' }}
+                              >
+                                <XStack gap={'$2'}>
+                                  <Paragraph
+                                    testID="SendFormBalance"
+                                    color={insufficientAmount ? '$error' : '$silverChalice'}
+                                    size={'$5'}
+                                    $theme-light={{
+                                      color: insufficientAmount ? '$error' : '$darkGrayTextField',
+                                    }}
+                                  >
+                                    Balance:
+                                  </Paragraph>
+                                  <Paragraph
+                                    color={insufficientAmount ? '$error' : '$color12'}
+                                    size={'$5'}
+                                    fontWeight={'600'}
+                                  >
+                                    {formatAmount(formatUnits(coin.balance, coin.decimals), 12, 4)}
+                                  </Paragraph>
+                                </XStack>
+                                {insufficientAmount && (
+                                  <Paragraph color={'$error'} size={'$5'}>
+                                    Insufficient funds
+                                  </Paragraph>
+                                )}
                               </XStack>
-                              {insufficientAmount && (
-                                <Paragraph color={'$error'} size={'$5'}>
-                                  Insufficient funds
-                                </Paragraph>
-                              )}
-                            </XStack>
-                          )
-                      }
-                    })()}
-                  </Stack>
-                </XStack>
+                            )
+                        }
+                      })()}
+                    </Stack>
+                  </XStack>
+                </YStack>
+                <YStack gap={'$2'}>
+                  {note}
+                  {(isNoteInputFocused || isNoteTooLong) && (
+                    <Paragraph
+                      color={isNoteTooLong ? '$error' : '$lightGrayTextField'}
+                      $theme-light={{ color: '$darkGrayTextField' }}
+                    >
+                      Max: {MAX_NOTE_LENGTH} characters
+                    </Paragraph>
+                  )}
+                </YStack>
               </YStack>
             )}
           </SchemaForm>
