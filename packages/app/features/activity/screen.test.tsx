06b23d70cd516c85ce7f997860921de0a85a26db
musidlo <musidlo@gmail.com>
2025-04-15 15:18:21 +0200

    Added back search on activity on mobile

--- packages/app/features/activity/screen.test.tsx
+++ packages/app/features/activity/screen.test.tsx
@@ -1,12 +1,45 @@
-import { config, TamaguiProvider } from '@my/ui'
-import { act, render, screen } from '@testing-library/react-native'
+import { expect } from '@jest/globals'
+import { TamaguiProvider, View as MockView, config } from '@my/ui'
+import { act, render, screen, fireEvent, waitFor } from '@testing-library/react-native'
 import { ActivityScreen } from './screen'
 import { useSearchResultHref } from 'app/utils/useSearchResultHref'
+import { useSupabase } from 'app/utils/supabase/useSupabase'
+import { useRootScreenParams } from 'app/routers/params'
 
 jest.unmock('app/provider/tag-search')
 jest.mock('app/utils/useSearchResultHref')
-
 jest.mock('app/features/activity/utils/useActivityFeed')
+jest.mock('app/routers/params')
+jest.mock('app/utils/supabase/useSupabase')
+
+jest.mock('expo-router', () => ({
+  usePathname: jest.fn(),
+}))
+
+jest.mock('solito/link', () => ({
+  Link: ({ children, href }: { children: React.ReactNode; href: string }) => (
+    <MockView dataSet={{ href }}>{children}</MockView>
+  ),
+  useLink: jest.fn().mockReturnValue({ href: '/profile/123', onPress: jest.fn() }),
+}))
+
+jest.mock('app/provider/coins', () => ({
+  useCoins: jest.fn().mockReturnValue({
+    coins: [
+      {
+        label: 'USDC',
+        token: '0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913',
+        balance: 250000n,
+      },
+      {
+        label: 'SEND',
+        token: '0xEab49138BA2Ea6dd776220fE26b7b8E446638956',
+        balance: 250000n,
+      },
+    ],
+    totalPrice: 5000000n,
+  }),
+}))
 
 jest.mock('app/utils/useSwapRouters', () => ({
   useSwapRouters: jest.fn().mockReturnValue({ data: [] }),
@@ -20,11 +53,32 @@ describe('ActivityScreen', () => {
   beforeEach(() => {
     jest.useFakeTimers()
     jest.setSystemTime(new Date('2025-01-19T12:00:00Z'))
+    // @ts-expect-error mock
+    useSearchResultHref.mockImplementation((item) => {
+      return `/profile/${item.send_id}`
+    })
   })
 
   afterEach(() => jest.useRealTimers())
 
   it('renders activity screen', async () => {
+    ;(useSupabase as jest.Mock).mockReturnValue({
+      rpc: jest.fn().mockReturnValue({
+        abortSignal: jest.fn().mockReturnValue({
+          data: [
+            {
+              send_id_matches: [],
+              tag_matches: [],
+              phone_matches: [],
+              address_matches: [],
+            },
+          ],
+          error: null,
+        }),
+      }),
+    })
+    ;(useRootScreenParams as jest.Mock).mockReturnValue([{}, jest.fn()])
+
     render(
       <TamaguiProvider defaultTheme={'dark'} config={config}>
         <ActivityScreen />
@@ -45,5 +99,47 @@ describe('ActivityScreen', () => {
     expect(screen.getByText('Send Account Signing Key Removed')).toBeOnTheScreen()
     expect(screen.getByText('Send Token Upgrade')).toBeOnTheScreen()
     expect(screen).toMatchSnapshot()
+  })
+
+  it('returns the correct search results', async () => {
+    ;(useSupabase as jest.Mock).mockReturnValue({
+      rpc: jest.fn().mockReturnValue({
+        abortSignal: jest.fn().mockReturnValue({
+          data: [
+            {
+              send_id_matches: [],
+              tag_matches: [
+                {
+                  send_id: 3665,
+                  tag_name: 'test',
+                  avatar_url: 'https://avatars.githubusercontent.com/u/123',
+                },
+              ],
+              phone_matches: [],
+              address_matches: [],
+            },
+          ],
+          error: null,
+        }),
+      }),
+    })
+    ;(useRootScreenParams as jest.Mock).mockReturnValue([{ search: 'test' }, jest.fn()])
+
+    render(
+      <TamaguiProvider defaultTheme={'dark'} config={config}>
+        <ActivityScreen />
+      </TamaguiProvider>
+    )
+    const searchInput = screen.getByPlaceholderText('Search')
+    fireEvent.changeText(searchInput, 'test')
+    await act(async () => {
+      jest.advanceTimersByTime(2000)
+      jest.runAllTimers()
+    })
+    await waitFor(() => screen.findByTestId('tag-search-3665'))
+
+    expect(searchInput.props.value).toBe('test')
+    const searchResults = await screen.findByTestId('tag-search-3665')
+    expect(searchResults).toHaveTextContent('??test/test')
   })
 })
