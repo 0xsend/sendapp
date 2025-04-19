fef576458c9e60f2f88f04334e391872f8fa8b47
youngkidwarrior <victor@she.energy>
2025-04-19 02:21:34 -0700

    Refactor HomeQuickActions

--- packages/app/features/home/HomeQuickActions.tsx
+++ packages/app/features/home/HomeQuickActions.tsx
@@ -1,17 +1,78 @@
-import { Button, LinkableButton, Theme, XStack, YStack, type XStackProps } from '@my/ui'
-import { IconArrowUp, IconPlus, IconSwap } from 'app/components/icons'
+import {
+  Button,
+  LinkableButton,
+  type LinkableButtonProps,
+  Theme,
+  XStack,
+  YStack,
+  type XStackProps,
+} from '@my/ui'
+import { IconArrowUp, IconPlus, IconStacks, IconSwap } from 'app/components/icons'
 import { useHoverStyles } from 'app/utils/useHoverStyles'
 import { sendCoin, usdcCoin } from 'app/data/coins'
 import { useCoinFromTokenParam } from 'app/utils/useCoinFromTokenParam'
-import { useIsSendingUnlocked } from 'app/utils/useIsSendingUnlocked'
 
 type HomeQuickActionsProps = XStackProps
 
-export const HomeQuickActions = ({ ...props }: HomeQuickActionsProps) => {
-  const { coin } = useCoinFromTokenParam()
-  const hoverStyles = useHoverStyles()
-  const { isSendingUnlocked } = useIsSendingUnlocked()
+export const HomeQuickActions = ({ children, ...props }: HomeQuickActionsProps) => {
+  return (
+    <XStack w={'100%'} gap={'$3'} $gtLg={{ gap: '$5' }} {...props}>
+      {children}
+    </XStack>
+  )
+}
 
+const QuickActionButton = ({ href, children }: LinkableButtonProps) => {
+  const hoverStyles = useHoverStyles()
+  return (
+    <LinkableButton
+      href={href}
+      f={1}
+      height={'auto'}
+      hoverStyle={hoverStyles}
+      focusStyle={hoverStyles}
+      w="100%"
+    >
+      {children}
+    </LinkableButton>
+  )
+}
+
+const Send = () => {
+  const { coin } = useCoinFromTokenParam()
+  const getSendUrl = () => {
+    if (!coin) {
+      return '/send'
+    }
+
+    return `/send?sendToken=${coin.token}`
+  }
+  return (
+    <QuickActionButton href={getSendUrl()}>
+      <YStack
+        testID={'send-quick-action'}
+        gap="$2"
+        jc={'space-between'}
+        ai="center"
+        px="$4"
+        py="$3.5"
+        $gtSm={{ py: '$4' }}
+      >
+        <IconArrowUp
+          size={'$1.5'}
+          $theme-dark={{ color: '$primary' }}
+          $theme-light={{ color: '$color12' }}
+        />
+        <Button.Text fontSize={'$5'} px="$2" ta="center" w="100%">
+          Send
+        </Button.Text>
+      </YStack>
+    </QuickActionButton>
+  )
+}
+
+const Trade = () => {
+  const { coin } = useCoinFromTokenParam()
   const getTradeUrl = () => {
     if (!coin) {
       return '/trade'
@@ -23,89 +84,59 @@ export const HomeQuickActions = ({ ...props }: HomeQuickActionsProps) => {
 
     return `/trade?inToken=${coin.token}`
   }
-
-  const getSendUrl = () => {
-    if (!coin) {
-      return '/send'
-    }
-
-    return `/send?sendToken=${coin.token}`
-  }
-
   return (
-    <XStack w={'100%'} gap={'$3'} $gtLg={{ gap: '$5' }} {...props}>
-      <LinkableButton
-        href="/deposit"
-        f={1}
-        height={'auto'}
-        hoverStyle={hoverStyles}
-        focusStyle={hoverStyles}
-        ta="center"
-        w="100%"
-      >
-        <YStack gap="$2" jc={'space-between'} ai="center" px="$4" py="$3.5" $gtSm={{ py: '$4' }}>
-          <IconPlus
-            size={'$1.5'}
+    <QuickActionButton href={getTradeUrl()}>
+      <YStack gap="$2" jc={'space-between'} ai="center" p="$4" height={'auto'}>
+        <Theme name="green">
+          <IconSwap
+            size={'$1'}
             $theme-dark={{ color: '$primary' }}
             $theme-light={{ color: '$color12' }}
           />
-          <Button.Text fontSize={'$5'} px="$2">
-            Deposit
-          </Button.Text>
-        </YStack>
-      </LinkableButton>
-      {isSendingUnlocked && (
-        <LinkableButton
-          href={getSendUrl()}
-          f={1}
-          height={'auto'}
-          hoverStyle={hoverStyles}
-          focusStyle={hoverStyles}
-          ta="center"
-          w="100%"
-        >
-          <YStack
-            testID={'send-quick-action'}
-            gap="$2"
-            jc={'space-between'}
-            ai="center"
-            px="$4"
-            py="$3.5"
-            $gtSm={{ py: '$4' }}
-          >
-            <IconArrowUp
-              size={'$1.5'}
-              $theme-dark={{ color: '$primary' }}
-              $theme-light={{ color: '$color12' }}
-            />
-            <Button.Text fontSize={'$5'} px="$2" ta="center" w="100%">
-              Send
-            </Button.Text>
-          </YStack>
-        </LinkableButton>
-      )}
-      <LinkableButton
-        href={getTradeUrl()}
-        f={1}
-        height={'auto'}
-        hoverStyle={hoverStyles}
-        focusStyle={hoverStyles}
-        ta="center"
-        w="100%"
-      >
-        <YStack gap="$2" jc={'space-between'} ai="center" p="$4" height={'auto'}>
-          <Theme name="green">
-            <IconSwap
-              size={'$1'}
-              $theme-dark={{ color: '$primary' }}
-              $theme-light={{ color: '$color12' }}
-            />
-          </Theme>
-          <Button.Text fontSize={'$5'} px="$2">
-            Trade
-          </Button.Text>
-        </YStack>
-      </LinkableButton>
-    </XStack>
+        </Theme>
+        <Button.Text fontSize={'$5'} px="$2">
+          Trade
+        </Button.Text>
+      </YStack>
+    </QuickActionButton>
   )
 }
+
+const Deposit = () => {
+  return (
+    <QuickActionButton href={'/deposit'}>
+      <YStack gap="$2" jc={'space-between'} ai="center" px="$4" py="$3.5" $gtSm={{ py: '$4' }}>
+        <IconPlus
+          size={'$1.5'}
+          $theme-dark={{ color: '$primary' }}
+          $theme-light={{ color: '$color12' }}
+        />
+        <Button.Text fontSize={'$5'} px="$2">
+          Deposit
+        </Button.Text>
+      </YStack>
+    </QuickActionButton>
+  )
+}
+
+const Earn = () => {
+  return (
+    <QuickActionButton href={'/earn'}>
+      <YStack gap="$2" jc={'space-between'} ai="center" px="$4" py="$3.5" $gtSm={{ py: '$4' }}>
+        <IconStacks
+          size={'$1.5'}
+          $theme-dark={{ color: '$primary' }}
+          $theme-light={{ color: '$color12' }}
+        />
+        <Button.Text fontSize={'$5'} px="$2">
+          Earn
+        </Button.Text>
+      </YStack>
+    </QuickActionButton>
+  )
+}
+
+HomeQuickActions.Deposit = Deposit
+HomeQuickActions.Send = Send
+HomeQuickActions.Trade = Trade
+HomeQuickActions.Earn = Earn
