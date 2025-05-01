11b85110184df26b41f8eb604b54ab2c63e5b8cc
Big Boss <bigboss@metalrodeo.xyz>
2025-04-30 22:24:29 -0500

    chore: update assetBundlePatterns and add appleTeamId and permissions in app.json for improved configuration and setup

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Send.app development environment";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     foundry-overlay.url = "github:0xbigboss/foundry-overlay";
@@ -38,27 +38,49 @@
     flake-utils.lib.eachSystem systems (
       system: let
         pkgs = import nixpkgs {inherit overlays system;};
-        nodejs = pkgs.nodejs_20;
-        yarn = pkgs.yarn.override {inherit nodejs;};
       in {
         formatter = pkgs.alejandra;
 
         devShells.default = pkgs.mkShell {
-          nativeBuildInputs = [
-            pkgs.foundry
-            nodejs
-            yarn
-            pkgs.python310
-            pkgs.gnused
-            pkgs.postgresql_15
+          nativeBuildInputs =
+            [
+              pkgs.foundry
+              pkgs.python310
+              pkgs.gnused
+              pkgs.postgresql_15
 
-            pkgs.unstable.jq
-            pkgs.unstable.yj
-            pkgs.unstable.caddy
-            pkgs.unstable.bun
-            pkgs.unstable.tilt
-            pkgs.unstable.temporal-cli
-          ];
+              pkgs.unstable.fnm
+              pkgs.unstable.jq
+              pkgs.unstable.yj
+              pkgs.unstable.caddy
+              pkgs.unstable.bun
+              pkgs.unstable.tilt
+              pkgs.unstable.temporal-cli
+            ]
+            # macOS-specific tools
+            ++ (pkgs.lib.optionals pkgs.stdenv.isDarwin [
+              pkgs.darwin.apple_sdk.frameworks.CoreServices
+              pkgs.darwin.apple_sdk.frameworks.CoreFoundation
+            ]);
+
+          shellHook = ''
+            ${
+              if pkgs.stdenv.isDarwin
+              then ''
+                # Use the host's Xcode installation
+                export DEVELOPER_DIR=$(xcrun xcode-select -p)
+                export SDKROOT=$(xcrun --sdk macosx --show-sdk-path)
+
+                # Prioritize host tools over Nix
+                export PATH=/usr/bin:$DEVELOPER_DIR/usr/bin:$PATH
+              ''
+              else ''
+                # silence is golden
+              ''
+            }
+            eval "$(fnm env --use-on-cd --shell bash)"
+            echo "Welcome to the Send.app development environment!"
+          '';
         };
 
         # For compatibility with older versions of the `nix` binary
