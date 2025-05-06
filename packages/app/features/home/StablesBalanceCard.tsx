365a7215ab239aa76af0ac11f76720da0bba5082
youngkidwarrior <victor@she.energy>
2025-05-05 20:03:38 -0700

    Show $ sign for stables

--- packages/app/features/home/StablesBalanceCard.tsx
+++ packages/app/features/home/StablesBalanceCard.tsx
@@ -132,7 +132,7 @@ export const StablesBalanceCard = () => {
                           toggleIsPriceHidden()
                         }}
                       >
-                        ${formattedBalance}
+                        ${formattedUSDBalance}
                       </BigHeading>
                     </>
                   )
