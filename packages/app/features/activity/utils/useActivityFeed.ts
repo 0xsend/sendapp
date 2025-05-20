7f14590f4fe1321b51b730374979409da6f8ff72
youngkidwarrior <victor@she.energy>
2025-05-20 04:07:05 -0700

    Improve look of route change bar

--- packages/app/features/activity/utils/useActivityFeed.ts
+++ packages/app/features/activity/utils/useActivityFeed.ts
@@ -34,7 +34,6 @@ export function useActivityFeed({
 > {
   const supabase = useSupabase()
   const addressBook = useAddressBook()
-  console.log(addressBook)
   const enabled = useMemo(() => addressBook.isError || addressBook.isSuccess, [addressBook])
   const queryKey = useMemo(
     () => ['activity_feed', { addressBook, supabase, pageSize }] as const,
