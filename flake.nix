e11d0576867038adb4a584fb612c35db0ac62427
Big Boss <bigboss@metalrodeo.xyz>
2025-05-28 22:24:00 -0500

    chore: update nixpkgs version in flake.lock and flake.nix from 24.11 to 25.05 for compatibility and improvements

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Send.app development environment";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     foundry-overlay.url = "github:0xbigboss/foundry-overlay";
