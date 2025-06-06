7e2639d891e9d163fe7e50a0fa5a765299c2f37e
musidlo <musidlo@gmail.com>
2025-06-06 17:00:34 +0200

    Native: refactor stack imports

--- apps/expo/app/account/sendtag/add.tsx
+++ apps/expo/app/account/sendtag/add.tsx
@@ -1,11 +1,11 @@
-import { Stack as StackRouter } from 'expo-router/build/layouts/Stack'
+import { Stack } from 'expo-router/build/layouts/Stack'
 import { ScreenContainer } from 'apps-expo/components/layout/ScreenContainer'
 import { Paragraph } from '@my/ui'
 
 export default function AddSendtagsScreen() {
   return (
     <>
-      <StackRouter.Screen
+      <Stack.Screen
         options={{
           title: 'Register Sendtags',
         }}
--- apps/expo/app/profile/[sendid].tsx
+++ apps/expo/app/profile/[sendid].tsx
@@ -1,11 +1,11 @@
 import { ScreenContainer } from 'apps-expo/components/layout/ScreenContainer'
-import { Stack as StackRouter } from 'expo-router'
+import { Stack } from 'expo-router'
 import { Paragraph } from '@my/ui'
 
 export default function ProfileScreen() {
   return (
     <>
-      <StackRouter.Screen
+      <Stack.Screen
         options={{
           title: 'History',
         }}
