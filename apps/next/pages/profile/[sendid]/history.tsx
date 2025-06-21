5439aaa48e6b07ffcea40999e65aa710866abbcc
youngkidwarrior <victor@she.energy>
2025-06-20 21:50:32 -0700

    feat(profile): add profile history view with new routing structure

---
+++ apps/next/pages/profile/[sendid]/history.tsx
@@ -0,0 +1,71 @@
+import { HomeLayout } from 'app/features/home/layout.web'
+import Head from 'next/head'
+import { createPagesServerClient } from '@supabase/auth-helpers-nextjs'
+import type { NextPageWithLayout } from '../../_app'
+import type { GetServerSideProps, GetServerSidePropsContext } from 'next'
+import type { Database } from '@my/supabase/database.types'
+import { userOnboarded } from 'utils/userOnboarded'
+import { createSupabaseAdminClient } from 'app/utils/supabase/admin'
+import { ProfileHistoryScreen } from 'app/features/profile/history/screen'
+
+export const Page: NextPageWithLayout = () => {
+  return (
+    <>
+      <Head>
+        <title>Send | Profile History</title>
+      </Head>
+      <ProfileHistoryScreen />
+    </>
+  )
+}
+
+// Profile page is not protected, but we need to look up the user profile by tag in case we have to show a 404
+export const getServerSideProps = (async (ctx: GetServerSidePropsContext) => {
+  const { sendid: sendidParam } = ctx.params ?? {}
+  const sendid = Number(sendidParam)
+
+  if (Number.isNaN(sendid)) {
+    return {
+      notFound: true,
+    }
+  }
+
+  const supabase = createPagesServerClient<Database>(ctx)
+  const {
+    data: { session },
+  } = await supabase.auth.getSession()
+
+  if (session) {
+    // not anonymous user
+    // check if user is onboarded
+    const needsOnboarding = await userOnboarded(supabase, ctx)
+    if (needsOnboarding) return needsOnboarding
+  }
+
+  // check if profile exists
+  const supabaseAdmin = createSupabaseAdminClient()
+  const { data: profile, error } = await supabaseAdmin
+    .rpc('profile_lookup', { lookup_type: 'sendid', identifier: sendid.toString() })
+    .maybeSingle()
+
+  if (error) {
+    console.error('Error fetching profile from sendid', error)
+    throw error
+  }
+
+  if (profile === null || (!profile.is_public && !session)) {
+    // no profile or profile is private and user is not logged in
+    // return 404
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: {},
+  }
+}) satisfies GetServerSideProps
+
+Page.getLayout = (children) => <HomeLayout fullHeight>{children}</HomeLayout>
+
+export default Page
