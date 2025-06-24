1c7fa5fd506f938b09eb77c825cb6850ba56e040
youngkidwarrior <victor@she.energy>
2025-06-24 09:29:28 -0700

    feat(pages): update tag pages with improved routing and UI

--- apps/next/pages/profile/[sendid]/index.tsx
+++ apps/next/pages/profile/[sendid]/index.tsx
@@ -1,13 +1,12 @@
 import { ProfileScreen } from 'app/features/profile/screen'
-import { HomeLayout } from 'app/features/home/layout.web'
 import Head from 'next/head'
 import { createPagesServerClient } from '@supabase/auth-helpers-nextjs'
-import type { NextPageWithLayout } from '../_app'
+import type { NextPageWithLayout } from '../../_app'
 import type { GetServerSideProps, GetServerSidePropsContext } from 'next'
 import type { Database } from '@my/supabase/database.types'
 import { userOnboarded } from 'utils/userOnboarded'
 import { createSupabaseAdminClient } from 'app/utils/supabase/admin'
-import { TopNav } from 'app/components/TopNav'
+import { ProfileLayout } from 'app/features/profile/layout.web'
 
 export const Page: NextPageWithLayout = () => {
   return (
@@ -67,10 +66,6 @@ export const getServerSideProps = (async (ctx: GetServerSidePropsContext) => {
   }
 }) satisfies GetServerSideProps
 
-Page.getLayout = (children) => (
-  <HomeLayout TopNav={<TopNav header="History" backFunction="router" />} fullHeight>
-    {children}
-  </HomeLayout>
-)
+Page.getLayout = (children) => <ProfileLayout fullHeight>{children}</ProfileLayout>
 
 export default Page
