9fb173ec136b20dabcd2e37558c854b23097df7b
youngkidwarrior <victor@she.energy>
2025-06-24 09:27:07 -0700

    feat(icons): add Discord, Spotify, and YouTube social media icons

---
+++ packages/app/components/icons/IconYoutube.tsx
@@ -0,0 +1,33 @@
+import type { ColorTokens } from '@my/ui'
+import { type IconProps, themed } from '@tamagui/helpers-icon'
+import { memo } from 'react'
+import { Path, Svg } from 'react-native-svg'
+
+const aspectRatio = 256 / 180
+
+const Youtube = (props) => {
+  const { size, color, ...rest } = props
+
+  const height = size / aspectRatio
+  const width = size
+
+  return (
+    <Svg
+      color={color as ColorTokens | undefined}
+      width={width}
+      height={height}
+      viewBox="0 0 256 180"
+      {...rest}
+    >
+      <Path
+        d="M250.346 28.075A32.18 32.18 0 0 0 227.69 5.418C207.824 0 127.87 0 127.87 0S47.912.164 28.046 5.582A32.18 32.18 0 0 0 5.39 28.24c-6.009 35.298-8.34 89.084.165 122.97a32.18 32.18 0 0 0 22.656 22.657c19.866 5.418 99.822 5.418 99.822 5.418s79.955 0 99.82-5.418a32.18 32.18 0 0 0 22.657-22.657c6.338-35.348 8.291-89.1-.164-123.134Z"
+        fill="red"
+      />
+      <Path fill="currentColor" d="m102.421 128.06 66.328-38.418-66.328-38.418z" />
+    </Svg>
+  )
+}
+
+const IconYoutube = memo<IconProps>(themed(Youtube))
+
+export { IconYoutube }
