071c0b06189755eff32b782aaea473d36152c430
youngkidwarrior <victor@she.energy>
2025-06-24 09:29:28 -0700

    feat(pages): update tag pages with improved routing and UI

---
+++ apps/next/pages/[tag]/index.tsx
@@ -0,0 +1,91 @@
+import { ProfileScreen } from 'app/features/profile/screen'
+import Head from 'next/head'
+import { createPagesServerClient } from '@supabase/auth-helpers-nextjs'
+import type { NextPageWithLayout } from '../_app'
+import type { GetServerSideProps, GetServerSidePropsContext } from 'next'
+import type { Database } from '@my/supabase/database.types'
+import { userOnboarded } from 'utils/userOnboarded'
+import { SendtagSchema } from 'app/utils/zod/sendtag'
+import { assert } from 'app/utils/assert'
+import { createSupabaseAdminClient } from 'app/utils/supabase/admin'
+import { TopNav } from 'app/components/TopNav'
+import { HomeLayout } from 'app/features/home/layout.web'
+
+export const Page: NextPageWithLayout<{ sendid: number | null }> = ({ sendid }) => {
+  return (
+    <>
+      <Head>
+        <title>Send | Profile</title>
+      </Head>
+      <ProfileScreen sendid={sendid} />
+    </>
+  )
+}
+
+// Profile page is not protected, but we need to look up the user profile by tag in case we have to show a 404
+export const getServerSideProps = (async (ctx: GetServerSidePropsContext) => {
+  const { tag: tagParam } = ctx.params ?? {}
+
+  // ensure identifier is valid before proceeding
+  const result = SendtagSchema.safeParse({
+    name: tagParam?.toString().match(/^@/) ? tagParam.toString().slice(1) : tagParam,
+  })
+
+  if (!result.success) {
+    return {
+      notFound: true,
+    }
+  }
+
+  const { name: tag } = result.data
+
+  assert(!!tag, 'tag is required')
+  assert(typeof tag === 'string', 'Identifier tag must be a string')
+
+  const supabase = createPagesServerClient<Database>(ctx)
+  const {
+    data: { session },
+  } = await supabase.auth.getSession()
+
+  if (session) {
+    // not anonymous user / check if user is onboarded
+    const needsOnboarding = await userOnboarded(supabase, ctx)
+    if (needsOnboarding) return needsOnboarding
+  }
+
+  // check if profile exists
+  const supabaseAdmin = createSupabaseAdminClient()
+  const { data: profile, error } = await supabaseAdmin
+    .rpc('profile_lookup', {
+      lookup_type: 'tag',
+      identifier: tag,
+    })
+    .maybeSingle()
+
+  if (error) {
+    console.error('Error fetching profile from tag', error)
+    throw error
+  }
+
+  if (profile === null || (!profile.is_public && !session)) {
+    // no profile or profile is private and user is not logged in
+    // return 404
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: {
+      sendid: profile.sendid,
+    },
+  }
+}) satisfies GetServerSideProps
+
+Page.getLayout = (children) => (
+  <HomeLayout TopNav={<TopNav backFunction="router" />} fullHeight>
+    {children}
+  </HomeLayout>
+)
+
+export default Page
