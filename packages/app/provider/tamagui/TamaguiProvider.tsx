bb2586a1fa362d932b9571a37986fe94907ed102
Mikołaj Musidłowski <38129890+musidlo@users.noreply.github.com>
2025-07-24 12:59:20 +0200

    Native: disable font scaling (#1764)

--- packages/app/provider/tamagui/TamaguiProvider.tsx
+++ packages/app/provider/tamagui/TamaguiProvider.tsx
@@ -1,15 +1,13 @@
 import { TamaguiProvider as TamaguiProviderOG } from '@my/ui'
-import { Platform, Text, TextInput } from 'react-native'
+import { Text, TextInput } from 'react-native'
 import config from '../../tamagui.config'
 import { useRootTheme } from '../theme/UniversalThemeProvider'
 import { useMemo } from 'react'
 
-if (Platform.OS !== 'web') {
-  // @ts-expect-error some hacky way to disable font scaling, our UI is not ready for this, couldn't find any better way to disable it
-  Text.defaultProps = { ...(Text.defaultProps || {}), allowFontScaling: false }
-  // @ts-expect-error same for inputs
-  TextInput.defaultProps = { ...(TextInput.defaultProps || {}), allowFontScaling: false }
-}
+// @ts-expect-error some hacky way to disable font scaling, our UI is not ready for this, couldn't find any better way to disable it
+Text.defaultProps = { ...(Text.defaultProps || {}), allowFontScaling: false }
+// @ts-expect-error same for inputs
+TextInput.defaultProps = { ...(TextInput.defaultProps || {}), allowFontScaling: false }
 
 export const TamaguiProvider = ({ children }: { children: React.ReactNode }) => {
   const [rootTheme] = useRootTheme()
