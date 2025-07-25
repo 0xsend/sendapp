5baff2a60d5df9ec6609228d6f1f2d7a9325aab9
musidlo <musidlo@gmail.com>
2025-07-25 20:16:55 +0200

    Native: production build

--- packages/app/utils/getRpId.ts
+++ packages/app/utils/getRpId.ts
@@ -2,10 +2,9 @@ import { applicationId } from 'expo-application'
 import { Platform } from 'react-native'
 
 export function getRpId() {
-  const bundleId = applicationId?.split('.').reverse().join('.')
   return Platform.select({
     web: undefined,
-    ios: bundleId,
-    android: bundleId?.replaceAll('_', '-'),
+    ios: applicationId,
+    android: applicationId, // TODO this values for sure is wrong, gonna fix it when working on android
   })
 }
