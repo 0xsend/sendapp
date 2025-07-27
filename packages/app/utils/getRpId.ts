df6de364b77799f34c147d3e8cbfef42c5327a31
youngkidwarrior <victor@she.energy>
2025-07-27 02:30:33 -0700

    Fix getRpId types

--- packages/app/utils/getRpId.ts
+++ packages/app/utils/getRpId.ts
@@ -4,7 +4,7 @@ import { Platform } from 'react-native'
 export function getRpId() {
   return Platform.select({
     web: undefined,
-    ios: applicationId,
-    android: applicationId, // TODO this values for sure is wrong, gonna fix it when working on android
+    ios: applicationId ?? undefined,
+    android: applicationId ?? undefined, // TODO this values for sure is wrong, gonna fix it when working on android
   })
 }
