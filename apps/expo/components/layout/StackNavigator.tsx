cf3b58a670784bc78fb76d0a1493155fc81d0ecb
musidlo <musidlo@gmail.com>
2025-07-29 12:02:17 +0200

    Stack navigation headers

--- apps/expo/components/layout/StackNavigator.tsx
+++ apps/expo/components/layout/StackNavigator.tsx
@@ -1,10 +1,13 @@
-import { Stack } from 'expo-router'
+import { Stack, useRouter } from 'expo-router'
 import { config, Paragraph, useTheme, XStack } from '@my/ui'
 import { useIsDarkTheme } from 'apps-expo/utils/layout/useIsDarkTheme'
+import { Pressable } from 'react-native'
+import { IconArrowLeft } from 'app/components/icons'
 
 export default function StackNavigator() {
   const theme = useTheme()
   const isDark = useIsDarkTheme()
+  const router = useRouter()
 
   return (
     <Stack
@@ -14,9 +17,24 @@ export default function StackNavigator() {
           backgroundColor: theme.background.val,
         },
         headerTintColor: isDark ? config.tokens.color.primary.val : theme.color12.val,
+        headerLeft: () => (
+          <Pressable onPress={() => router.back()}>
+            <IconArrowLeft
+              size={'$1.5'}
+              color={isDark ? config.tokens.color.primary.val : theme.color12.val}
+            />
+          </Pressable>
+        ),
+        headerTitleAlign: 'left',
         headerTitle: ({ children }) => (
-          <XStack f={1} ai="center" jc="flex-start">
-            <Paragraph fontWeight={'300'} fontSize={'$8'} col="$color10" lineHeight={32}>
+          <XStack flex={1} ai="center" jc="flex-start">
+            <Paragraph
+              fontWeight="500"
+              fontSize="$8"
+              color="$color12"
+              lineHeight={32}
+              numberOfLines={1}
+            >
               {children}
             </Paragraph>
           </XStack>
