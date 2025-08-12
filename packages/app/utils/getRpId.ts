50add04a5b0006fe64c20736101fcbb835ff69d5
musidlo <musidlo@gmail.com>
2025-08-12 11:58:34 +0200

    Fixed local build

--- packages/app/utils/getRpId.ts
+++ packages/app/utils/getRpId.ts
@@ -2,6 +2,8 @@ import { applicationId } from 'expo-application'
 import { Platform } from 'react-native'
 
 export function getRpId() {
+  console.log('applicationId', applicationId)
+
   return Platform.select({
     web: undefined,
     ios: applicationId ?? undefined,
