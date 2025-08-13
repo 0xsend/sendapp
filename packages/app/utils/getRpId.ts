02be33ed83a7c2092c08d1e37dfbdfbbf3ee0e72
Mikołaj Musidłowski <38129890+musidlo@users.noreply.github.com>
2025-08-13 10:20:42 +0200

    Native: made rp id static (#1830)

--- packages/app/utils/getRpId.ts
+++ packages/app/utils/getRpId.ts
@@ -1,10 +1,9 @@
-import { applicationId } from 'expo-application'
 import { Platform } from 'react-native'
 
 export function getRpId() {
   return Platform.select({
     web: undefined,
-    ios: applicationId ?? undefined,
-    android: applicationId ?? undefined, // TODO this values for sure is wrong, gonna fix it when working on android
+    ios: 'send.app',
+    android: 'send.app',
   })
 }
