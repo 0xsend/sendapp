31e7641e8a649c663a9655a3de7cafee988f4efa
Mikołaj Musidłowski <38129890+musidlo@users.noreply.github.com>
2025-09-04 12:39:27 +0200

    Native: fixed profile send button navigation crushing android app (#1901)

---
+++ packages/app/features/profile/utils/ProfileSendButton.tsx
@@ -0,0 +1,28 @@
+import { Button, LinkableButton, useThemeName } from '@my/ui'
+import { IconArrowUp } from 'app/components/icons'
+
+export default function ProfileSendButton({ sendId }: { sendId?: number | null }) {
+  const isDark = useThemeName()?.startsWith('dark')
+
+  return (
+    <LinkableButton
+      href={{
+        pathname: '/send',
+        query: { recipient: sendId, idType: 'sendid' },
+      }}
+      borderRadius={'$4'}
+      jc="center"
+      ai="center"
+      position="relative"
+      bc={isDark ? 'rgba(255, 255, 255, 0.1)' : 'rgba(0, 0, 0, 0.1)'}
+      f={1}
+    >
+      <Button.Icon>
+        <IconArrowUp size={'$1'} color={isDark ? '$primary' : '$color12'} />
+      </Button.Icon>
+      <Button.Text color="$color12" fontSize={'$4'} fontWeight={'400'} textAlign="center">
+        Send
+      </Button.Text>
+    </LinkableButton>
+  )
+}
