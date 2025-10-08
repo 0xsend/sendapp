6e944c8a9dbc8b98114e39cac2288143a6289f39
Erick <eho@kolwaii.com>
2025-10-08 12:04:23 -0500

    update

--- packages/workflows/src/deposit-workflow/activities.ts
+++ packages/workflows/src/deposit-workflow/activities.ts
@@ -9,16 +9,9 @@ import {
 } from 'app/utils/decodeSendEarnDepositUserOp'
 import { hexToBytea } from 'app/utils/hexToBytea'
 import { createSupabaseAdminClient } from 'app/utils/supabase/admin'
-import type {
-  UserOperation,
-  WaitForUserOperationReceiptParameters,
-  GetUserOperationReceiptReturnType,
-} from 'permissionless'
-import { isAddressEqual, zeroAddress, type Address, type Hex } from 'viem'
-import { byteaToHex } from 'app/utils/byteaToHex'
-import superjson from 'superjson'
-import type { PgBytea } from '@my/supabase/database.types'
-import { decodeExecuteBatchCalldata } from 'app/utils/decode-calldata'
+import type { UserOperation } from 'permissionless'
+import { isAddressEqual, zeroAddress, type Address } from 'viem'
+import { createUserOpActivities, type UserOpActivities } from './wagmi'
 
 import {
   getUserIdFromAddress,
@@ -28,13 +21,6 @@ import {
   type TemporalDepositInsert,
   type TemporalDepositUpdate,
 } from './supabase'
-import {
-  getBaseBlockNumber,
-  sendUserOperation,
-  simulateUserOperation,
-  waitForTransactionReceipt,
-  waitForUserOperationReceipt,
-} from './wagmi'
 
 type UpsertTemporalDepositActivityParams = {
   workflow_id: string
@@ -43,19 +29,7 @@ type UpsertTemporalDepositActivityParams = {
   block_num: bigint
 }
 
-type DepositActivities = {
-  simulateUserOperationActivity: (userOp: UserOperation<'v0.7'>) => Promise<void>
-  decodeExecuteBatchCallDataActivity: (data: Hex) => Promise<{
-    [K in keyof ReturnType<typeof decodeExecuteBatchCalldata>]: NonNullable<
-      ReturnType<typeof decodeExecuteBatchCalldata>[K]
-    >
-  }>
-  getBaseBlockNumberActivity: () => Promise<bigint>
-  sendUserOpActivity: (userOp: UserOperation<'v0.7'>) => Promise<PgBytea>
-  waitForTransactionReceiptActivity: (hash: PgBytea) => Promise<GetUserOperationReceiptReturnType>
-  waitForUserOperationReceiptActivity: (
-    params: WaitForUserOperationReceiptParameters
-  ) => Promise<GetUserOperationReceiptReturnType>
+type DepositActivities = UserOpActivities & {
   upsertTemporalDepositActivity: (
     params: UpsertTemporalDepositActivityParams
   ) => Promise<TemporalDeposit>
@@ -80,103 +54,12 @@ export const createDepositActivities = (
 ): DepositActivities => {
   bootstrap(env)
   return {
-    async simulateUserOperationActivity(userOp) {
-      try {
-        await simulateUserOperation(userOp)
-      } catch (error) {
-        log.error('Failed to simulate user operation', { error })
-        throw ApplicationFailure.nonRetryable(
-          'Failed to simulate user operation',
-          error.code,
-          error.details
-        )
-      }
-    },
-    async decodeExecuteBatchCallDataActivity(data) {
-      try {
-        const { args, functionName } = decodeExecuteBatchCalldata(data)
-        if (!args || !functionName) {
-          throw ApplicationFailure.nonRetryable('Failed to decode executeBatch user op')
-        }
-
-        return { functionName, args }
-      } catch (error) {
-        log.error('Failed to decode executeBatch user op', { error })
-        throw ApplicationFailure.nonRetryable(error.message, error.code, error.details)
-      }
-    },
-    async getBaseBlockNumberActivity() {
-      try {
-        return await getBaseBlockNumber()
-      } catch (error) {
-        log.error('Failed to get block number', { code: error.code, error })
-        throw ApplicationFailure.retryable('Failed to get block number')
-      }
-    },
-    async sendUserOpActivity(userOp) {
-      try {
-        const hash = await sendUserOperation(userOp)
-        const hashBytea = hexToBytea(hash)
-        return hashBytea
-      } catch (error) {
-        log.error('sendUserOpActivity failed', { error })
-        // Throw non retryable error for now
-        // This should retry a few times
-        throw ApplicationFailure.nonRetryable(
-          'Failed to send user operation',
-          error.code,
-          error.details
-        )
-      }
-    },
-    /**
-     * Waits for the transaction receipt for the given hash.
-     * @deprecated - use waitForUserOperationReceiptActivity instead
-     */
-    async waitForTransactionReceiptActivity(hash) {
-      try {
-        const hexHash = byteaToHex(hash)
-        const bundlerReceipt = await waitForTransactionReceipt(hexHash)
-        log.info('waitForTransactionReceiptActivity', {
-          bundlerReceipt: superjson.stringify(bundlerReceipt),
-        })
-        if (!bundlerReceipt.success) {
-          throw ApplicationFailure.nonRetryable(
-            `Transaction failed: ${bundlerReceipt.receipt.transactionHash}`
-          )
-        }
-        return bundlerReceipt
-      } catch (error) {
-        log.error('waitForTransactionReceipt failed', { error })
-        throw ApplicationFailure.nonRetryable(error.message, error.code, error.details)
-      }
-    },
-
-    /**
-     * Waits for the user operation receipt for the given hash.
-     */
-    async waitForUserOperationReceiptActivity(params: WaitForUserOperationReceiptParameters) {
-      try {
-        const bundlerReceipt = await waitForUserOperationReceipt(params)
-        log.info('waitForUserOperationReceiptActivity', {
-          bundlerReceipt,
-        })
-        if (!bundlerReceipt.success) {
-          throw ApplicationFailure.nonRetryable(
-            `Transaction failed: ${bundlerReceipt.receipt.transactionHash}`
-          )
-        }
-        return bundlerReceipt
-      } catch (error) {
-        log.error('waitForUserOperationReceipt failed', { error })
-        throw ApplicationFailure.nonRetryable(error.message, error.code, error.details)
-      }
-    },
     upsertTemporalDepositActivity,
     decodeDepositUserOpActivity,
     updateTemporalDepositActivity,
     verifyDepositIndexedActivity,
     upsertReferralRelationshipActivity,
+    ...createUserOpActivities(env),
   }
 }
 
--- packages/workflows/src/deposit-workflow/wagmi.ts
+++ packages/workflows/src/deposit-workflow/wagmi.ts
@@ -1,36 +1,133 @@
+import type { PgBytea } from '@my/supabase/database.types'
+import { ApplicationFailure, log } from '@temporalio/activity'
+import { byteaToHex } from 'app/utils/byteaToHex'
+import { decodeExecuteBatchCalldata } from 'app/utils/decode-calldata'
+import { hexToBytea } from 'app/utils/hexToBytea'
 import type {
-  UserOperation,
   GetUserOperationReceiptReturnType,
+  UserOperation,
   WaitForUserOperationReceiptParameters,
 } from 'permissionless'
+import superjson from 'superjson'
+import type { Hex } from 'viem'
 import { baseMainnetClient, cdpBundlerClient, entryPointAddress } from '@my/wagmi'
+import { bootstrap } from '../utils'
 
-export async function simulateUserOperation(userOp: UserOperation<'v0.7'>) {
-  return await baseMainnetClient.call({
-    account: entryPointAddress[baseMainnetClient.chain.id],
-    to: userOp.sender,
-    data: userOp.callData,
-  })
+export type UserOpActivities = {
+  simulateUserOperationActivity: (userOp: UserOperation<'v0.7'>) => Promise<void>
+  decodeExecuteBatchCallDataActivity: (data: Hex) => Promise<{
+    [K in keyof ReturnType<typeof decodeExecuteBatchCalldata>]: NonNullable<
+      ReturnType<typeof decodeExecuteBatchCalldata>[K]
+    >
+  }>
+  getBaseBlockNumberActivity: () => Promise<bigint>
+  sendUserOpActivity: (userOp: UserOperation<'v0.7'>) => Promise<PgBytea>
+  waitForTransactionReceiptActivity: (hash: PgBytea) => Promise<GetUserOperationReceiptReturnType>
+  waitForUserOperationReceiptActivity: (
+    params: WaitForUserOperationReceiptParameters
+  ) => Promise<GetUserOperationReceiptReturnType>
 }
 
-export async function sendUserOperation(userOp: UserOperation<'v0.7'>) {
-  return await cdpBundlerClient.sendUserOperation({
-    userOperation: userOp,
-  })
-}
+export const createUserOpActivities = (
+  env: Record<string, string | undefined>
+): UserOpActivities => {
+  bootstrap(env)
+  return {
+    async simulateUserOperationActivity(userOp) {
+      try {
+        await baseMainnetClient.call({
+          account: entryPointAddress[baseMainnetClient.chain.id],
+          to: userOp.sender,
+          data: userOp.callData,
+        })
+      } catch (error) {
+        log.error('Failed to simulate user operation', { error })
+        throw ApplicationFailure.nonRetryable(
+          'Failed to simulate user operation',
+          error.code,
+          error.details
+        )
+      }
+    },
+    async decodeExecuteBatchCallDataActivity(data) {
+      try {
+        const { args, functionName } = decodeExecuteBatchCalldata(data)
+        if (!args || !functionName) {
+          throw ApplicationFailure.nonRetryable('Failed to decode executeBatch user op')
+        }
 
-export async function waitForTransactionReceipt(
-  hash: `0x${string}`
-): Promise<GetUserOperationReceiptReturnType> {
-  return await cdpBundlerClient.waitForUserOperationReceipt({ hash })
-}
+        return { functionName, args }
+      } catch (error) {
+        log.error('Failed to decode executeBatch user op', { error })
+        throw ApplicationFailure.nonRetryable(error.message, error.code, error.details)
+      }
+    },
+    async getBaseBlockNumberActivity() {
+      try {
+        return await baseMainnetClient.getBlockNumber()
+      } catch (error) {
+        log.error('Failed to get block number', { code: error.code, error })
+        throw ApplicationFailure.retryable('Failed to get block number')
+      }
+    },
+    async sendUserOpActivity(userOp) {
+      try {
+        const hash = await cdpBundlerClient.sendUserOperation({
+          userOperation: userOp,
+        })
+        const hashBytea = hexToBytea(hash)
+        return hashBytea
+      } catch (error) {
+        log.error('sendUserOpActivity failed', { error })
+        throw ApplicationFailure.nonRetryable(
+          'Failed to send user operation',
+          error.code,
+          error.details
+        )
+      }
+    },
+    /**
+     * Waits for the transaction receipt for the given hash.
+     * @deprecated - use waitForUserOperationReceiptActivity instead
+     */
+    async waitForTransactionReceiptActivity(hash) {
+      try {
+        const hexHash = byteaToHex(hash)
+        const bundlerReceipt = await cdpBundlerClient.waitForUserOperationReceipt({ hash: hexHash })
+        log.info('waitForTransactionReceiptActivity', {
+          bundlerReceipt: superjson.stringify(bundlerReceipt),
+        })
+        if (!bundlerReceipt.success) {
+          throw ApplicationFailure.nonRetryable(
+            `Transaction failed: ${bundlerReceipt.receipt.transactionHash}`
+          )
+        }
+        return bundlerReceipt
+      } catch (error) {
+        log.error('waitForTransactionReceipt failed', { error })
+        throw ApplicationFailure.nonRetryable(error.message, error.code, error.details)
+      }
+    },
 
-export async function waitForUserOperationReceipt(
-  params: WaitForUserOperationReceiptParameters
-): Promise<GetUserOperationReceiptReturnType> {
-  return await cdpBundlerClient.waitForUserOperationReceipt(params)
-}
-
-export async function getBaseBlockNumber() {
-  return await baseMainnetClient.getBlockNumber()
+    /**
+     * Waits for the user operation receipt for the given hash.
+     */
+    async waitForUserOperationReceiptActivity(params: WaitForUserOperationReceiptParameters) {
+      try {
+        const bundlerReceipt = await cdpBundlerClient.waitForUserOperationReceipt(params)
+        log.info('waitForUserOperationReceiptActivity', {
+          bundlerReceipt,
+        })
+        if (!bundlerReceipt.success) {
+          throw ApplicationFailure.nonRetryable(
+            `Transaction failed: ${bundlerReceipt.receipt.transactionHash}`
+          )
+        }
+        return bundlerReceipt
+      } catch (error) {
+        log.error('waitForUserOperationReceipt failed', { error })
+        throw ApplicationFailure.nonRetryable(error.message, error.code, error.details)
+      }
+    },
+  }
 }
