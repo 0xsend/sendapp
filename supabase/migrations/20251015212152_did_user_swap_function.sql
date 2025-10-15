dd3536bc6c7fcf49c629936b716b0fd365f7a3cb
musidlo <musidlo@gmail.com>
2025-10-15 23:34:17 +0200

    Migration formatting

---
+++ supabase/migrations/20251015212152_did_user_swap_function.sql
@@ -0,0 +1,43 @@
+set check_function_bodies = off;
+
+CREATE OR REPLACE FUNCTION public.did_user_swap()
+ RETURNS boolean
+ LANGUAGE plpgsql
+AS $function$
+BEGIN
+    RETURN EXISTS (
+        SELECT 1
+        FROM activity_feed af
+        WHERE (
+            EXISTS (
+              SELECT 1 FROM liquidity_pools lp
+              WHERE (af.data->>'f')::bytea = lp.pool_addr
+            )
+            OR EXISTS (
+              SELECT 1 FROM swap_routers sr
+              WHERE (af.data->>'f')::bytea = sr.router_addr
+            )
+            OR EXISTS (
+              SELECT 1 FROM liquidity_pools lp
+              WHERE (af.data->>'t')::bytea = lp.pool_addr
+            )
+            OR EXISTS (
+              SELECT 1 FROM swap_routers sr
+              WHERE (af.data->>'t')::bytea = sr.router_addr
+            )
+        )
+        LIMIT 1
+    );
+END;
+$function$
+;
+
+REVOKE ALL ON FUNCTION "public"."did_user_swap"() FROM PUBLIC;
+
+GRANT ALL ON FUNCTION "public"."did_user_swap"() TO "anon";
+
+GRANT ALL ON FUNCTION "public"."did_user_swap"() TO "authenticated";
+
+GRANT ALL ON FUNCTION "public"."did_user_swap"() TO "service_role";
+
+
