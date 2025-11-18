356333307bdba956edf3f0c01877117a2988ac77
pathoam <pathoam@pm.me>
2025-11-18 11:09:25 -0700

    next deposit pages changes

--- apps/next/pages/deposit/debit-card.tsx
+++ apps/next/pages/deposit/debit-card.tsx
@@ -1,20 +1,33 @@
+import type { ReactNode } from 'react'
 import { HomeLayout } from 'app/features/home/layout.web'
 import { TopNav } from 'app/components/TopNav'
 import { NextSeo } from 'next-seo'
 import type { NextPageWithLayout } from '../_app'
 import { DepositCoinbaseScreen } from 'app/features/deposit/DepositCoinbase/screen'
+import { useTranslation } from 'react-i18next'
 
 export const Page: NextPageWithLayout = () => {
+  const { t } = useTranslation('navigation')
+  const debitCardTitle = t('stack.deposit.debitCard')
+
   return (
     <>
-      <NextSeo title="Send | Debit Card Deposit" />
+      <NextSeo title={`Send | ${debitCardTitle}`} />
       <DepositCoinbaseScreen defaultPaymentMethod="CARD" />
     </>
   )
 }
 
-Page.getLayout = (children) => (
-  <HomeLayout TopNav={<TopNav header="Debit Card" backFunction="pop" />}>{children}</HomeLayout>
-)
+function DebitCardLayout({ children }: { children: ReactNode }) {
+  const { t } = useTranslation('navigation')
+
+  return (
+    <HomeLayout TopNav={<TopNav header={t('stack.deposit.debitCard')} backFunction="pop" />}>
+      {children}
+    </HomeLayout>
+  )
+}
+
+Page.getLayout = (children) => <DebitCardLayout>{children}</DebitCardLayout>
 
 export default Page
