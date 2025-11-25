c421ba329cdc373880a317c8fff79b0c6a752afa
Ehsan sarshar <ehsan.herai@gmail.com>
2025-11-25 21:01:11 +0400

    fix(send): use Tamagui bottom sheet instead of gorhom for transaction detail

--- packages/app/features/send/components/SendChat/Transaction.tsx
+++ packages/app/features/send/components/SendChat/Transaction.tsx
@@ -10,6 +10,7 @@ import {
   SizableText,
   useTheme,
   useMedia,
+  Sheet,
 } from '@my/ui'
 import BottomSheet from '@gorhom/bottom-sheet'
 import { BottomSheetView } from '@gorhom/bottom-sheet'
@@ -107,7 +108,7 @@ const TransactionContent = ({
       </Button>
 
       {!isFailed ? (
-        <YStack gap="$4">
+        <YStack pb="$4" gap="$4">
           <XStack ai="center" gap="$3">
             <SizableText size="$9" fontWeight="600" fontFamily="$mono" color="$color12">
               {numericAmount}
@@ -148,77 +149,45 @@ const TransactionContent = ({
 }
 
 export const Transaction = ({ open, onClose: onCloseProp, transaction }: TransactionProps) => {
-  const bottomSheetRef = useRef<BottomSheet>(null)
-
-  useEffect(() => {
-    if (open) {
-      bottomSheetRef.current?.expand()
-    } else {
-      bottomSheetRef.current?.close()
-    }
-  }, [open])
-
-  const { gtLg } = useMedia()
-
-  const theme = useTheme()
-
   const onClose = () => {
-    bottomSheetRef.current?.close()
-    setTimeout(() => {
-      onCloseProp()
-    }, 200)
+    onCloseProp()
+  }
+
+  const transRef = useRef<Activity | undefined>(undefined)
+
+  if (!transRef.current) {
+    transRef.current = transaction
   }
 
   return (
-    <Portal zIndex={200}>
-      <BottomSheet
-        index={-1}
-        ref={bottomSheetRef}
-        snapPoints={[230]}
-        animationConfigs={{
-          stiffness: 1000,
-          damping: 80,
-        }}
-        enablePanDownToClose
-        handleComponent={null}
-        backgroundStyle={{
-          borderRadius: 0,
-          backgroundColor: theme.color1.val,
-        }}
-        containerStyle={
-          gtLg
-            ? {
-                maxWidth: 698,
-                margin: 47,
-                marginLeft: 'auto',
-                borderRadius: 20,
-              }
-            : undefined
+    <Sheet
+      open={open && !!transaction}
+      onOpenChange={(open) => {
+        if (!open) {
+          setTimeout(() => {
+            onClose()
+          }, 200)
         }
-      >
-        {transaction && (
-          <BottomSheetView id="transaction-content">
-            <TransactionContent transaction={transaction} onClose={onClose} />
-          </BottomSheetView>
+      }}
+      forceRemoveScrollEnabled={open}
+      modal
+      snapPoints={[250]}
+      snapPointsMode="constant"
+      dismissOnSnapToBottom
+      zIndex={100_000}
+      animation="responsive"
+    >
+      <Sheet.Frame bg="$color1" id="transaction-content" maxWidth={720} m={35} ml="auto">
+        {transRef.current && (
+          <TransactionContent transaction={transRef.current} onClose={onCloseProp} />
         )}
-      </BottomSheet>
-      <AnimatePresence>
-        {open && (
-          <View
-            pe="auto"
-            role="button"
-            aria-label="Close transaction"
-            animation="200ms"
-            enterStyle={{ opacity: 0 }}
-            exitStyle={{ opacity: 0 }}
-            bg="$shadowColor"
-            pos="absolute"
-            inset={0}
-            onPress={onClose}
-            zi={-1}
-          />
-        )}
-      </AnimatePresence>
-    </Portal>
+      </Sheet.Frame>
+      <Sheet.Overlay
+        animation="100ms"
+        bg="$shadowColor"
+        enterStyle={{ o: 0 }}
+        exitStyle={{ o: 0 }}
+      />
+    </Sheet>
   )
 }
