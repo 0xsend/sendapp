a6f7285d289a012461ddd241eed4117e37684cd0
patrick <pathoam@pm.me>
2025-11-26 09:32:24 -0700

    fix rewards activity titles (#2250)

--- apps/next/pages/rewards/index.tsx
+++ apps/next/pages/rewards/index.tsx
@@ -1,25 +1,36 @@
+import type { ReactNode } from 'react'
 import { ActivityRewardsScreen } from 'app/features/rewards/activity/screen'
 import { NextSeo } from 'next-seo'
 import type { NextPageWithLayout } from 'next-app/pages/_app'
 import { HomeLayout } from 'app/features/home/layout.web'
 import { TopNav } from 'app/components/TopNav'
 import { MobileButtonRowLayout } from 'app/components/MobileButtonRowLayout'
+import { useTranslation } from 'react-i18next'
 
 export const Page: NextPageWithLayout = () => {
+  const { t } = useTranslation('navigation')
+  const rewardsTitle = t('stack.rewards.root')
+
   return (
     <>
-      <NextSeo title="Send | Rewards" />
+      <NextSeo title={`Send | ${rewardsTitle}`} />
       <ActivityRewardsScreen />
     </>
   )
 }
 
-Page.getLayout = (children) => (
-  <MobileButtonRowLayout.ActivityRewards>
-    <HomeLayout TopNav={<TopNav header="Activity Rewards" backFunction={'pop'} />}>
-      {children}
-    </HomeLayout>
-  </MobileButtonRowLayout.ActivityRewards>
-)
+function RewardsLayout({ children }: { children: ReactNode }) {
+  const { t } = useTranslation('navigation')
+
+  return (
+    <MobileButtonRowLayout.ActivityRewards>
+      <HomeLayout TopNav={<TopNav header={t('stack.rewards.root')} backFunction={'pop'} />}>
+        {children}
+      </HomeLayout>
+    </MobileButtonRowLayout.ActivityRewards>
+  )
+}
+
+Page.getLayout = (children) => <RewardsLayout>{children}</RewardsLayout>
 
 export default Page
