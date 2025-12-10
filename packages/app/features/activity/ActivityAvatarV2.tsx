0633aecdf2e667fe983487c7019e5123f7868551
Ehsan sarshar <ehsan.herai@gmail.com>
2025-12-11 01:58:48 +0400

    feat: create improved version of ActivityAvatar and TokenActivityRow

---
+++ packages/app/features/activity/ActivityAvatarV2.tsx
@@ -0,0 +1,263 @@
+// optimized version of ActivityAvatar for the TokenActivityRowV2.tsx
+import {
+  Avatar,
+  LinkableAvatar,
+  type LinkableAvatarProps,
+  Spinner,
+  useThemeName,
+  XStack,
+} from '@my/ui'
+import { Minus, Plus, ArrowDown, ArrowUp } from '@tamagui/lucide-icons'
+import { AvatarSendEarnDeposit } from 'app/components/avatars'
+import { AvatarSendEarnWithdraw } from 'app/components/avatars/AvatarSendEarnWithdraw'
+import { IconUpgrade, IconBadgeCheckSolid2 } from 'app/components/icons'
+import { IconCoin } from 'app/components/icons/IconCoin'
+import { allCoinsDict } from 'app/data/coins'
+import { ContractLabels } from 'app/data/contract-labels'
+import {
+  counterpart,
+  isActivitySwapTransfer,
+  isSendPotTicketPurchase,
+  isSendPotWin,
+  isSwapBuyTransfer,
+} from 'app/utils/activity'
+import type { useAddressBook } from 'app/utils/useAddressBook'
+import type { useLiquidityPools } from 'app/utils/useLiquidityPools'
+import { useSwapRouters } from 'app/utils/useSwapRouters'
+import { IconSendPotTicket } from 'app/components/icons/IconSendPotTicket'
+import {
+  type Activity,
+  isSendAccountReceiveEvent,
+  isSendAccountTransfersEvent,
+  isSendEarnDepositEvent,
+  isSendEarnEvent,
+  isSendEarnWithdrawEvent,
+  isSendTokenUpgradeEvent,
+} from 'app/utils/zod/activity'
+import { Platform } from 'react-native'
+
+interface ActivityAvatarProps extends Omit<LinkableAvatarProps, 'children' | 'href'> {
+  activity: Activity
+  swapRouters: ReturnType<typeof useSwapRouters>['data']
+  liquidityPools: ReturnType<typeof useLiquidityPools>['data']
+  addressBook: ReturnType<typeof useAddressBook>
+}
+export function ActivityAvatar({
+  activity,
+  swapRouters,
+  liquidityPools,
+  addressBook,
+  ...props
+}: ActivityAvatarProps) {
+  const user = counterpart(activity)
+  const { from_user, to_user, data } = activity
+  const isERC20Transfer = isSendAccountTransfersEvent(activity)
+  const isETHReceive = isSendAccountReceiveEvent(activity)
+  const theme = useThemeName()
+  const isDark = theme.includes('dark')
+
+  if (isSendPotTicketPurchase(activity) || isSendPotWin(activity)) {
+    return (
+      <XStack w="$4.5" h={'$4.5'} br="$4" bc={'$olive'}>
+        <IconSendPotTicket color={'$color2'} />
+      </XStack>
+    )
+  }
+
+  if (isActivitySwapTransfer(activity, swapRouters, liquidityPools)) {
+    return <TradeActivityAvatar activity={activity} />
+  }
+
+  if (user) {
+    return (
+      <XStack
+        onPress={(e) => {
+          e.stopPropagation()
+        }}
+        position={'relative'}
+      >
+        <LinkableAvatar
+          size="$5"
+          gap="$2"
+          href={`/profile/${user.send_id}`}
+          circular={true}
+          {...props}
+        >
+          {(() => {
+            switch (true) {
+              case !user.avatar_url:
+                return Platform.OS === 'android' ? (
+                  <UserImage user={user} />
+                ) : (
+                  <Avatar.Image src={undefined} />
+                )
+              case Boolean(to_user?.send_id) && Boolean(from_user?.send_id):
+                return <Avatar.Image src={user.avatar_url} />
+              case isERC20Transfer || isETHReceive:
+                return (
+                  <IconCoin
+                    symbol={
+                      allCoinsDict[data?.coin?.token as keyof typeof allCoinsDict]?.symbol ?? ''
+                    }
+                  />
+                )
+              default:
+                return Platform.OS === 'android' && !user?.avatar_url ? (
+                  <UserImage user={user} />
+                ) : (
+                  <Avatar.Image src={user?.avatar_url ?? undefined} />
+                )
+            }
+          })()}
+
+          <Avatar.Fallback jc="center" bc="$olive">
+            <Avatar size="$5" {...props}>
+              <UserImage user={user} />
+            </Avatar>
+          </Avatar.Fallback>
+        </LinkableAvatar>
+        {user.is_verified && (
+          <XStack zi={100} pos="absolute" bottom={0} right={0} x="$0.5" y="$0.5">
+            <XStack pos="absolute" elevation={'$1'} scale={0.5} br={1000} inset={0} />
+            <IconBadgeCheckSolid2
+              size="$1"
+              scale={0.7}
+              color="$neon8"
+              $theme-dark={{ color: '$neon7' }}
+              // @ts-expect-error - checkColor is not typed
+              checkColor={isDark ? '#082B1B' : '#fff'}
+            />
+          </XStack>
+        )}
+      </XStack>
+    )
+  }
+
+  if (isSendTokenUpgradeEvent(activity)) {
+    return (
+      <Avatar size="$4.5" br="$4" gap="$2" {...props}>
+        <IconUpgrade size="$4.5" br="$4" gap="$2" />
+      </Avatar>
+    )
+  }
+
+  if (isSendEarnEvent(activity)) {
+    if (isSendEarnDepositEvent(activity)) {
+      return <AvatarSendEarnDeposit {...props} />
+    }
+    if (isSendEarnWithdrawEvent(activity)) {
+      return <AvatarSendEarnWithdraw {...props} />
+    }
+  }
+
+  if (isERC20Transfer) {
+    // is transfer, but an unknown user
+    const address = from_user?.id ? activity.data.t : activity.data.f
+    const name = addressBook?.data?.[address] ?? address
+
+    if (name === ContractLabels.SendEarn) {
+      if (from_user?.id) {
+        return <AvatarSendEarnDeposit {...props} />
+      }
+      if (to_user?.id) {
+        return <AvatarSendEarnWithdraw {...props} />
+      }
+    }
+
+    if (addressBook.isLoading) {
+      return (
+        <Avatar size="$4.5" br="$4" gap="$2" {...props}>
+          <Spinner size="small" />
+        </Avatar>
+      )
+    }
+
+    return (
+      <Avatar size="$4.5" br="$4" gap="$2" {...props}>
+        <Avatar.Image
+          src={`https://ui-avatars.com/api/?name=${name}&size=256&format=png&background=86ad7f`}
+        />
+        <Avatar.Fallback jc="center" bc="$olive">
+          <Avatar size="$4.5" br="$4" {...props}>
+            <Avatar.Image
+              src={`https://ui-avatars.com/api/?name=${name}&size=256&format=png&background=86ad7f`}
+            />
+          </Avatar>
+        </Avatar.Fallback>
+      </Avatar>
+    )
+  }
+
+  // @todo make this an icon instead of a fallback TODO
+  return (
+    <Avatar size="$4.5" br="$4" gap="$2" {...props}>
+      <Avatar.Image
+        src={'https://ui-avatars.com/api/?name=TODO&size=256&format=png&background=86ad7f'}
+      />
+      <Avatar.Fallback jc="center" bc="$olive">
+        <Avatar size="$4.5" br="$4" {...props}>
+          <Avatar.Image
+            src={'https://ui-avatars.com/api/?name=TODO&size=256&format=png&background=86ad7f'}
+          />
+        </Avatar>
+      </Avatar.Fallback>
+    </Avatar>
+  )
+}
+
+const TradeActivityAvatar = ({ activity }: { activity: Activity }) => {
+  const { data: swapRouters } = useSwapRouters()
+  const isBuyTransfer = isSwapBuyTransfer(activity, swapRouters)
+  const Icon = isBuyTransfer ? Plus : Minus
+
+  return (
+    <XStack w="$5" h={'$5'} br="$4" ai={'center'} jc={'center'} position={'relative'}>
+      <IconCoin symbol={activity.data?.coin?.symbol ?? ''} size={'$5'} />
+      <XStack
+        position={'absolute'}
+        top={0}
+        right={0}
+        transform={'translate(5px, -5px) scale(0.85)'}
+        bc={isBuyTransfer ? '$olive' : '$error'}
+        borderRadius={999}
+        borderWidth={2}
+        borderColor={'$color1'}
+      >
+        <Icon size={'$1'} />
+      </XStack>
+    </XStack>
+  )
+}
+
+const TransferDirectionIndicator = ({ activity }: { activity: Activity }) => {
+  const { to_user } = activity
+
+  return (
+    <XStack
+      position={'absolute'}
+      bottom={0}
+      right={0}
+      transform={'translate(5px, 5px) scale(0.85)'}
+      bc={to_user?.id ? '$olive' : '$error'}
+      borderRadius={999}
+      borderWidth={2}
+      borderColor={'$color1'}
+    >
+      {to_user?.id ? (
+        <ArrowDown size={'$1'} color={'$white'} />
+      ) : (
+        <ArrowUp size={'$1'} color={'$white'} />
+      )}
+    </XStack>
+  )
+}
+
+const UserImage = ({ user }: { user?: Activity['from_user'] | Activity['to_user'] }) => {
+  return (
+    <Avatar.Image
+      src={`https://ui-avatars.com/api/?name=${
+        user?.name ?? user?.main_tag_name ?? user?.send_id
+      }&size=256&format=png&background=86ad7f`}
+    />
+  )
+}
---
+++ packages/app/features/home/TokenActivityRowV2.tsx
@@ -0,0 +1,181 @@
+// optimized version of TokenActivityRow for the RecentActivityFeed.tsx
+import { Text, XStack, YStack } from '@my/ui'
+import { Paragraph, useEvent } from 'tamagui'
+import {
+  useDateFromActivity,
+  useEventNameFromActivity,
+  useSubtextFromActivity,
+} from 'app/utils/activity'
+import { useAmountFromActivity } from 'app/utils/activity-hooks'
+import {
+  type Activity,
+  isSendAccountReceiveEvent,
+  isSendAccountTransfersEvent,
+} from 'app/utils/zod/activity'
+import { ActivityAvatar } from '../activity/ActivityAvatarV2'
+
+import { useHoverStyles } from 'app/utils/useHoverStyles'
+import type { useUser } from 'app/utils/useUser'
+import {
+  isTemporalEthTransfersEvent,
+  isTemporalTokenTransfersEvent,
+} from 'app/utils/zod/activity/TemporalTransfersEventSchema'
+import type { ReactNode } from 'react'
+import { memo, useCallback } from 'react'
+import type { useSendScreenParams } from 'app/routers/params'
+import type { useSwapRouters } from 'app/utils/useSwapRouters'
+import type { useLiquidityPools } from 'app/utils/useLiquidityPools'
+import type { useAddressBook } from 'app/utils/useAddressBook'
+
+interface TokenActivityRowV2Props {
+  profile: ReturnType<typeof useUser>['profile']
+  activity: Activity
+  onPress?: (activity: Activity) => void
+  sendParamsRef: React.RefObject<ReturnType<typeof useSendScreenParams>>
+  swapRouters: ReturnType<typeof useSwapRouters>['data']
+  liquidityPools: ReturnType<typeof useLiquidityPools>['data']
+  addressBook: ReturnType<typeof useAddressBook>
+}
+
+export const TokenActivityRow = ({
+  profile,
+  activity,
+  onPress,
+  sendParamsRef,
+  swapRouters,
+  liquidityPools,
+  addressBook,
+}: TokenActivityRowV2Props) => {
+  const { from_user, to_user } = activity
+  const amount = useAmountFromActivity(activity, swapRouters, liquidityPools)
+  const date = useDateFromActivity({ activity })
+  const eventName = useEventNameFromActivity({ activity, swapRouters, liquidityPools })
+  const subtext = useSubtextFromActivity({ activity, swapRouters, liquidityPools })
+  const isERC20Transfer = isSendAccountTransfersEvent(activity)
+  const isETHReceive = isSendAccountReceiveEvent(activity)
+  const isTemporalTransfer =
+    isTemporalEthTransfersEvent(activity) || isTemporalTokenTransfersEvent(activity)
+  const isUserTransfer =
+    (isERC20Transfer || isETHReceive || isTemporalTransfer) &&
+    Boolean(to_user?.send_id) &&
+    Boolean(from_user?.send_id)
+
+  const [sendParams, setSendParams] = sendParamsRef.current ?? []
+
+  const handlePress = useEvent(() => {
+    if (onPress) {
+      if (isUserTransfer) {
+        setSendParams?.({
+          ...sendParams,
+          recipient:
+            profile?.send_id === from_user?.send_id
+              ? to_user?.send_id?.toString()
+              : from_user?.send_id?.toString(),
+          idType: 'sendid',
+          m: 1,
+        })
+        return
+      }
+      onPress(activity)
+    }
+  })
+
+  return (
+    <TokenActivityRowContent
+      amount={amount}
+      date={date}
+      eventName={eventName}
+      subtext={subtext}
+      isUserTransfer={isUserTransfer}
+      activity={activity}
+      onPress={onPress ? handlePress : undefined}
+      swapRouters={swapRouters}
+      liquidityPools={liquidityPools}
+      addressBook={addressBook}
+    />
+  )
+}
+
+TokenActivityRow.displayName = 'TokenActivityRow'
+
+const TokenActivityRowContent = memo(
+  ({
+    amount,
+    date,
+    eventName,
+    subtext,
+    isUserTransfer,
+    activity,
+    onPress,
+    swapRouters,
+    liquidityPools,
+    addressBook,
+  }: {
+    amount: ReactNode
+    date: ReactNode
+    eventName: string
+    subtext: string | null
+    isUserTransfer: boolean
+    activity: Activity
+    onPress?: () => void
+    swapRouters: ReturnType<typeof useSwapRouters>['data']
+    liquidityPools: ReturnType<typeof useLiquidityPools>['data']
+    addressBook: ReturnType<typeof useAddressBook>
+  }) => {
+    const hoverStyles = useHoverStyles()
+
+    return (
+      <XStack
+        width="100%"
+        height={102}
+        ai="center"
+        jc="space-between"
+        gap="$4"
+        p="$3.5"
+        br="$4"
+        borderWidth={1}
+        borderColor={'$color1'}
+        cursor={onPress ? 'pointer' : 'default'}
+        testID={'TokenActivityRow'}
+        hoverStyle={onPress ? hoverStyles : null}
+        onPress={onPress}
+      >
+        <XStack gap="$3.5" width={'100%'} f={1} alignItems={'flex-start'}>
+          <ActivityAvatar
+            activity={activity}
+            addressBook={addressBook}
+            swapRouters={swapRouters}
+            liquidityPools={liquidityPools}
+          />
+          <YStack width={'100%'} f={1} height={'auto'} overflow="hidden" gap={'$1'}>
+            <XStack jc="space-between" gap="$1.5" width={'100%'}>
+              <Text color="$color12" fontSize="$5" fontWeight={'500'}>
+                {isUserTransfer ? subtext : eventName}
+              </Text>
+              <Text>&nbsp;</Text>
+              <Text color="$color12" fontSize="$5" fontWeight={'500'} ta="right">
+                {amount}
+              </Text>
+            </XStack>
+            <Paragraph
+              color={'$color10'}
+              size={'$4'}
+              maxWidth={'100%'}
+              overflow={'hidden'}
+              textOverflow={'ellipsis'}
+              numberOfLines={2}
+              lineHeight={18}
+            >
+              {isUserTransfer ? eventName : subtext}
+            </Paragraph>
+            <Paragraph color={'$color10'} size={'$3'} flexShrink={0} display={'flex'} opacity={0.6}>
+              {date}
+            </Paragraph>
+          </YStack>
+        </XStack>
+      </XStack>
+    )
+  }
+)
+
+TokenActivityRowContent.displayName = 'TokenActivityRowContent'
