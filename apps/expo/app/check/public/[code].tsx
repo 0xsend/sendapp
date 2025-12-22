2f48462654d5e6de9d0ad50d82bfa715cc95b4df
Erick Ho <eho@send.it>
2025-12-21 19:22:37 -0600

    add native screens

---
+++ apps/expo/app/check/public/[code].tsx
@@ -0,0 +1,22 @@
+import { ScreenContainer } from 'apps-expo/components/layout/ScreenContainer'
+import { Stack, useLocalSearchParams } from 'expo-router'
+import { CheckPublicPreviewScreen } from 'app/features/check/claim/public-preview'
+import { useTranslation } from 'react-i18next'
+
+export default function Screen() {
+  const { t } = useTranslation('navigation')
+  const { code } = useLocalSearchParams<{ code: string }>()
+
+  return (
+    <>
+      <Stack.Screen
+        options={{
+          title: t('stack.check.claim'),
+        }}
+      />
+      <ScreenContainer>
+        <CheckPublicPreviewScreen checkCode={code ?? ''} />
+      </ScreenContainer>
+    </>
+  )
+}
