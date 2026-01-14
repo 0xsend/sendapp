5bdfb3cc2d967913b0a707c94e464b948d9916ab
Mikołaj Musidłowski <38129890+musidlo@users.noreply.github.com>
2026-01-14 17:49:35 +0100

    [SEND-752] Send chat opening undefined after using native back button (#2534)

--- apps/expo/app/(tabs)/send/index.tsx
+++ apps/expo/app/(tabs)/send/index.tsx
@@ -1,7 +1,10 @@
 import { SendScreen as SendMainScreen } from 'app/features/send/screen'
 import { TabScreenContainer } from 'apps-expo/components/layout/TabScreenContainer'
+import { useClearSendParamsOnBlur } from 'apps-expo/utils/useClearSendParamsOnBlur'
 
 export default function SendScreen() {
+  useClearSendParamsOnBlur()
+
   return (
     <TabScreenContainer>
       <SendMainScreen />
