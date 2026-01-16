6896fc0a5b88ca46444f6f73a1b3a02b81800f33
Ehsan sarshar <ehsan.herai@gmail.com>
2026-01-16 19:41:46 +0400

    feat(AnimatedLetterText): disable blur for lowerend devices

--- packages/ui/src/components/AnimatedLetterText.web.tsx
+++ packages/ui/src/components/AnimatedLetterText.web.tsx
@@ -1,4 +1,4 @@
-import type { ViewProps } from '@tamagui/core'
+import { useMedia, type ViewProps } from '@tamagui/core'
 import { useMemo } from 'react'
 import { motion, AnimatePresence } from 'motion/react'
 
@@ -6,7 +6,41 @@ interface AnimatedLetterTextProps extends ViewProps {
   children: string
 }
 
-function createEachLetter(letter: string, text: string, index: number) {
+const transition = {
+  layout: { type: 'spring', stiffness: 1400, damping: 140 },
+} as const
+
+const letterStyle = {
+  fontSize: 23,
+  fontWeight: 500,
+  color: 'var(--color12)',
+  fontFamily: 'var(--f-family)',
+}
+
+const initial = {
+  opacity: 0,
+  scale: 0.6,
+  filter: 'blur(2px)',
+} as const
+const animate = {
+  opacity: 1,
+  scale: 1,
+  filter: 'blur(0px)',
+} as const
+const exit = initial
+
+// light version for low end devices
+const initialNoBlur = {
+  opacity: 0,
+  scale: 0.6,
+} as const
+const animateNoBlur = {
+  opacity: 1,
+  scale: 1,
+} as const
+const exitNoBlur = initialNoBlur
+
+function createEachLetter(letter: string, text: string, index: number, isLowEndDevice: boolean) {
   const lowerLetter = letter.toLowerCase()
   const lowerText = text.toLowerCase()
   const howManySameLettersBefore = lowerText.slice(0, index).split(lowerLetter).length - 1
@@ -16,18 +50,11 @@ function createEachLetter(letter: string, text: string, index: number) {
       className="font_heading"
       key={`${lowerLetter}-${howManySameLettersBefore}`}
       layoutId={`${lowerLetter}-${howManySameLettersBefore}`}
-      initial={{ opacity: 0, scale: 0.6, filter: 'blur(2px)' }}
-      animate={{ opacity: 1, scale: 1, filter: 'blur(0px)' }}
-      exit={{ opacity: 0, scale: 0.6, filter: 'blur(2px)' }}
-      transition={{
-        layout: { type: 'spring', stiffness: 1400, damping: 140 },
-      }}
-      style={{
-        fontSize: 23,
-        fontWeight: 500,
-        color: 'var(--color12)',
-        fontFamily: 'var(--f-family)',
-      }}
+      initial={isLowEndDevice ? initialNoBlur : initial}
+      animate={isLowEndDevice ? animateNoBlur : animate}
+      exit={isLowEndDevice ? exitNoBlur : exit}
+      transition={transition}
+      style={letterStyle}
     >
       {letter}
     </motion.span>
@@ -35,9 +62,10 @@ function createEachLetter(letter: string, text: string, index: number) {
 }
 
 export function AnimatedLetterText({ children }: AnimatedLetterTextProps) {
+  const { sm } = useMedia()
   const letterItems = useMemo(() => {
-    return children.split('').map((letter, index) => createEachLetter(letter, children, index))
-  }, [children])
+    return children.split('').map((letter, index) => createEachLetter(letter, children, index, sm))
+  }, [children, sm])
 
   return (
     <div style={{ display: 'inline-flex' }}>
